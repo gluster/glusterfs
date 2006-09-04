9af27d4ea2f2612fd83a660e246787a025c87afb
Vikas Gorur <vikas@80x25.org>
2006-09-04 12:12:25 +0000

    added GPL notice to all files

--- glusterfsd/src/glusterfsd-fops.c
+++ glusterfsd/src/glusterfsd-fops.c
@@ -1,3 +1,41 @@
+/*
+  (C) 2006 Gluster core team <http://www.gluster.org/>
+  
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License as
+  published by the Free Software Foundation; either version 2 of
+  the License, or (at your option) any later version.
+    
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+    
+  You should have received a copy of the GNU General Public
+  License along with this program; if not, write to the Free
+  Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA 02110-1301 USA
+*/ 
+
+/*
+  (C) 2006 Gluster core team <http://www.gluster.org/>
+  
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License as
+  published by the Free Software Foundation; either version 2 of
+  the License, or (at your option) any later version.
+    
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+    
+  You should have received a copy of the GNU General Public
+  License along with this program; if not, write to the Free
+  Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA 02110-1301 USA
+*/ 
+
 
 #include "glusterfsd.h"
 #include <time.h>
@@ -712,7 +750,6 @@ glusterfsd_getattr (struct sock_private *sock_priv)
 			       data_to_bin (dict_get (dict, "PATH")),
 			       &stbuf);
 
-  printf ("return = (%d), errno = (%d)\n", ret, errno);
   dict_del (dict, "PATH");
 
   // convert stat structure to ASCII values (solving endian problem)
