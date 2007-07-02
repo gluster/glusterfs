608212217e4c8a90b295c219f9a6d8a3d2df72b5
Amar Tumballi <amar@80x25.org>
2007-07-02 18:21:12 +0000

    proper fixes in server_inode_pruning (by Avati)

--- xlators/protocol/server/src/server-protocol.c
+++ xlators/protocol/server/src/server-protocol.c
@@ -676,12 +676,13 @@ server_inode_prune (call_frame_t *frame,
       
       /* use bound_xl from the original frame, since copy_frame() does not preserve state */
       inode_curr->ref++; // :(
+      list_del_init (&inode_curr->list);
       STACK_WIND (inode_prune_frame,
 		  server_inode_prune_cbk,
 		  bound_xl,
 		  bound_xl->fops->forget,
 		  inode_curr);
-      //inode_unref (inode_curr);	
+      inode_unref (inode_curr);	
     }
   }
   return 0;
