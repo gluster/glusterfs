c72132fa90de30210b1993371b0e3ac5f7eb1df8
Anand V. Avati <avati@80x25.org>
2007-08-03 08:41:44 +0000

    dict_ref in call stub checking for refs, and not for count

--- libglusterfs/src/call-stub.c
+++ libglusterfs/src/call-stub.c
@@ -1047,7 +1047,7 @@ fop_writev_stub (call_frame_t *frame,
   stub->args.writev.count = count;
   stub->args.writev.off = off;
 
-  if (count)
+  if (frame->root->req_refs)
     dict_ref (frame->root->req_refs);
 
   return stub;
@@ -1798,7 +1798,7 @@ call_resume_wind (call_stub_t *stub)
 			    stub->args.writev.count,
 			    stub->args.writev.off);
       freee (stub->args.writev.vector);
-      if (stub->args.writev.count)
+      if (refs)
 	dict_unref (refs);
       break;
     }
