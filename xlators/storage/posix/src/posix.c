468d50ded6e4fcfff490d4911ba7cd389b58769f
Anand V. Avati <avati@80x25.org>
2007-10-03 06:52:25 +0000

    fix in storage/posix to calculate to usec from nsec

--- xlators/storage/posix/src/posix.c
+++ xlators/storage/posix/src/posix.c
@@ -705,9 +705,9 @@ posix_utimens (call_frame_t *frame,
    * Done: Check if this is correct */
 
   tv[0].tv_sec = ts[0].tv_sec;
-  tv[0].tv_usec = ts[0].tv_nsec * 1000;
+  tv[0].tv_usec = ts[0].tv_nsec / 1000;
   tv[1].tv_sec = ts[1].tv_sec;
-  tv[1].tv_usec = ts[1].tv_nsec * 1000;
+  tv[1].tv_usec = ts[1].tv_nsec / 1000;
 
   SET_FS_UID_GID (frame->root->uid, frame->root->gid);
     
