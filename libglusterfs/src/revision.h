c753a0f31d2f84682475c24761df46940ba7c536
Vikas Gorur <vikas@80x25.org>
2008-08-20 07:53:19 +0000

    op_ret value was always zero on success in posix_readv, changed to number of bytes written

--- libglusterfs/src/revision.h
+++ libglusterfs/src/revision.h
@@ -1,1 +1,1 @@
-#define GLUSTERFS_REPOSITORY_REVISION "glusterfs--mainline--3.0--patch-287"
+#define GLUSTERFS_REPOSITORY_REVISION "glusterfs--mainline--3.0--patch-289"
--- xlators/storage/posix/src/posix.c
+++ xlators/storage/posix/src/posix.c
@@ -78,6 +78,7 @@
                 gf_log (args);                                          \
         }
 
+
 int32_t
 posix_lookup (call_frame_t *frame, xlator_t *this,
               loc_t *loc, int32_t need_xattr)
@@ -1532,8 +1533,6 @@ posix_readv (call_frame_t *frame, xlator_t *this,
                         "fstat failed: %s", strerror (op_errno));
                 goto out;
         }
-
-        op_ret = 0;
 
  out:
         if (op_ret == -1) {
