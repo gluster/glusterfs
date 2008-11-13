9ee13fec6e83458d26ba1dbcdf81a568afa6c9d2
Harshavardhana (y4m4) <harsha@gluster.com>
2008-11-13 18:05:15 +0000

    fixed a typo

--- libglusterfs/src/compat.h
+++ libglusterfs/src/compat.h
@@ -161,7 +161,7 @@ enum {
 # endif
 
 #define llistxattr(path,key,size)               listxattr(path,key,size,XATTR_NOFOLLOW)
-#define lgetxattr(path,key,value,size)          getxattrf(path,key,value,size,0,XATTR_NOFOLLOW)
+#define lgetxattr(path,key,value,size)          getxattr(path,key,value,size,0,XATTR_NOFOLLOW)
 #define lsetxattr(path,key,value,size,flags)    setxattr(path,key,value,size,0,flags|XATTR_NOFOLLOW)
 #define lremovexattr(path,key)                  removexattr(path,key,XATTR_NOFOLLOW)
 #define fgetxattr(path,key,value,size)          fgetxattr(path,key,value,size,0,0)
