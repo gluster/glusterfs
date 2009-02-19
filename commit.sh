0bb3a56f246082967f7a89a7ea51638babf514d1
Anand V. Avati <avati@amp.gluster.com>
2009-02-19 02:04:17 -0800

    updated commit.sh
    changed commit.sh for git usage
    
    Signed-off-by: Anand V. Avati <avati@dev.gluster.com>

--- commit.sh
+++ commit.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
 export EDITOR="emacs"
-#TLA_REVISION=$(expr 1 + $(cat ./libglusterfs/src/revision.h | cut -f 8 -d '-' | sed -e 's/"//'))
-#sed -i "s/AC_INIT.*/AC_INIT([glusterfs],[2.0.0tla${TLA_REVISION}],[gluster-users@gluster.org])/g" ./configure.ac
-tla commit --write-revision ./libglusterfs/src/revision.h:'#define GLUSTERFS_REPOSITORY_REVISION "%s"' "$@"
+git commit -a -e "$@"
