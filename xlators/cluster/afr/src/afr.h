a70589a44cdbefa4dabd31664af9eb530f623d99
Vikas Gorur <vikas@gluster.com>
2009-04-02 08:30:32 -0700

    Defined afr_inode_ctx_t structure.
    Notification of a split-brain situation, which was earlier signalled
    by the mere presence of inode context is now signalled by
    the 'split_brain' member in the structure.
    
    Signed-off-by: Anand V. Avati <avati@amp.gluster.com>

--- xlators/cluster/afr/src/afr.h
+++ xlators/cluster/afr/src/afr.h
@@ -389,6 +389,23 @@ typedef struct _afr_local {
 	afr_self_heal_t self_heal;
 } afr_local_t;
 
+
+typedef struct {
+        /*
+          split-brain situation in which afr
+          can do nothing
+        */
+        gf_boolean_t split_brain;
+
+        /*
+           subvolume from which all reads should
+           happen for this inode
+        */
+        int read_child;
+
+} afr_inode_ctx_t;
+
+
 /* try alloc and if it fails, goto label */
 #define ALLOC_OR_GOTO(var, type, label) do {			\
 		var = CALLOC (sizeof (type), 1);		\
