99bf8da4764f425ac4022c9854acbd4247b6fdfc
Anand V. Avati <avati@amp.gluster.com>
2009-05-22 02:10:23 +0000

    THIS: set appropriately in call stub resumes
    Signed-off-by: Anand V. Avati <avati@dev.gluster.com>

--- libglusterfs/src/call-stub.c
+++ libglusterfs/src/call-stub.c
@@ -4154,15 +4154,22 @@ out:
 void
 call_resume (call_stub_t *stub)
 {
+        xlator_t *old_THIS = NULL;
+
 	errno = EINVAL;
 	GF_VALIDATE_OR_GOTO ("call-stub", stub, out);
 
 	list_del_init (&stub->list);
 
-	if (stub->wind)
-		call_resume_wind (stub);
-	else
-		call_resume_unwind (stub);
+        old_THIS = THIS;
+        THIS = stub->frame->this;
+        {
+                if (stub->wind)
+                        call_resume_wind (stub);
+                else
+                        call_resume_unwind (stub);
+        }
+        THIS = old_THIS;
 
 	call_stub_destroy (stub);
 out:
