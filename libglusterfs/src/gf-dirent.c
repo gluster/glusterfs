3365abb9d9147fa978f4f9151d97636781948980
Shehjar Tikoo <shehjart@gluster.com>
2009-06-03 01:05:34 +0000

    libglusterfs: Add empty and NULL dirents list checks
    These checks are needed in case a higher layer intends to
    delink the dirent list and passes a NULL pointer to
    fop_readdir_cbk_stub for the entries parameter.
    
    Consequently, the gf_dirent_free must guard against an empty list
    because the stub that is passed to it mgiht have an empty
    dirent list.
    
    Signed-off-by: Anand V. Avati <avati@dev.gluster.com>

--- libglusterfs/src/gf-dirent.c
+++ libglusterfs/src/gf-dirent.c
@@ -76,6 +76,12 @@ gf_dirent_free (gf_dirent_t *entries)
 	gf_dirent_t *entry = NULL;
 	gf_dirent_t *tmp = NULL;
 
+        if (!entries)
+                return;
+
+        if (list_empty (&entries->list))
+                return;
+
 	list_for_each_entry_safe (entry, tmp, &entries->list, list) {
 		list_del (&entry->list);
 		FREE (entry);
