3b99c6d82f9259db54f2d2d0a2520f88e7b12843
Raghavendra G <raghavendra@zresearch.com>
2009-06-10 07:02:27 +0000

    booster: fix memory corruption in booster_cleanup
    - calling glusterfs_umount_all leads to memory corruption.
        hence commenting out for timebeing.
    
    Signed-off-by: Anand V. Avati <avati@dev.gluster.com>

--- booster/src/booster.c
+++ booster/src/booster.c
@@ -1099,10 +1099,16 @@ booster_cleanup (void)
 	free (booster_glfs_fdtable);
 	booster_glfs_fdtable = NULL;
 
-        /* FIXME: there may be issues during execution of fini of individual
-           xlators due to inconsistent lock states.
-        */
-        glusterfs_umount_all ();
+        /*
+         * FIXME: there may be issues during execution of fini of individual
+         * xlators due to inconsistent lock states.
+         */
+        
+        /* 
+         * as above FIXME says, glusterfs_umount_all indeed leads to memory 
+         * corruption, hence commenting out
+         */
+        /* glusterfs_umount_all (); */
 	glusterfs_reset ();
 }
 
