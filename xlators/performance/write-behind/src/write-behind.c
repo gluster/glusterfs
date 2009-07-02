1a79ea17f77a99471205e8ddb10a94dce80eb7b2
Basavanagowda Kanur <gowda@gluster.com>
2009-07-02 01:55:29 +0000

    performance/write-behind - bug fix in open() and create().
    open() & create() calls should reset frame->local to NULL.
    
    bz# 104
    
    Signed-off-by: Anand V. Avati <avati@dev.gluster.com>

--- xlators/performance/write-behind/src/write-behind.c
+++ xlators/performance/write-behind/src/write-behind.c
@@ -924,6 +924,8 @@ wb_open_cbk (call_frame_t *frame, void *cookie, xlator_t *this, int32_t op_ret,
 
                 LOCK_INIT (&file->lock);
         }
+        
+        frame->local = NULL;
 
         STACK_UNWIND (frame, op_ret, op_errno, fd);
         return 0;
@@ -977,7 +979,9 @@ wb_create_cbk (call_frame_t *frame, void *cookie, xlator_t *this,
 
                 LOCK_INIT (&file->lock);
         }
-
+        
+        frame->local = NULL;
+        
         STACK_UNWIND (frame, op_ret, op_errno, fd, inode, buf);
         return 0;
 }
