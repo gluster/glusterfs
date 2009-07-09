c744ff99b5e72f7b6ee1909b90649333b6ffc98d
Raghavendra G <raghavendra@zresearch.com>
2009-07-09 00:26:58 +0000

    quick-read: implement qr_fchown
    Signed-off-by: Anand V. Avati <avati@dev.gluster.com>

--- xlators/performance/quick-read/src/quick-read.c
+++ xlators/performance/quick-read/src/quick-read.c
@@ -1090,6 +1090,100 @@ out:
 }
 
 
+static int32_t
+qr_fchown_cbk (call_frame_t *frame, void *cookie, xlator_t *this,
+               int32_t op_ret, int32_t op_errno, struct stat *buf)
+{
+	STACK_UNWIND (frame, op_ret, op_errno, buf);
+	return 0;
+}
+
+
+int32_t
+qr_fchown_helper (call_frame_t *frame, xlator_t *this, fd_t *fd, uid_t uid,
+                  gid_t gid)
+{
+        STACK_WIND (frame, qr_fchown_cbk, FIRST_CHILD (this),
+                    FIRST_CHILD (this)->fops->fchown, fd, uid, gid);
+        return 0;
+}
+
+
+int32_t
+qr_fchown (call_frame_t *frame, xlator_t *this,	fd_t *fd, uid_t uid, gid_t gid)
+{
+        uint64_t     value = 0;
+        int          flags = 0;
+        call_stub_t *stub = NULL;  
+        char        *path = NULL;
+        loc_t        loc = {0, };
+        qr_fd_ctx_t *qr_fd_ctx = NULL;
+        int32_t      ret = -1, op_ret = -1, op_errno = -1;
+        char         need_open = 0, can_wind = 0, need_unwind = 0;
+
+        ret = fd_ctx_get (fd, this, &value);
+        if (ret == 0) {
+                qr_fd_ctx = (qr_fd_ctx_t *)(long) value;
+        }
+
+        if (qr_fd_ctx) {
+                LOCK (&qr_fd_ctx->lock);
+                {
+                        path = qr_fd_ctx->path;
+                        flags = qr_fd_ctx->flags;
+
+                        if (!(qr_fd_ctx->opened
+                              || qr_fd_ctx->open_in_transit)) {
+                                need_open = 1;
+                                qr_fd_ctx->open_in_transit = 1;
+                        }
+
+                        if (qr_fd_ctx->opened) {
+                                can_wind = 1;
+                        } else {
+                                stub = fop_fchown_stub (frame, qr_fchown_helper,
+                                                        fd, uid, gid);
+                                if (stub == NULL) {
+                                        op_ret = -1;
+                                        op_errno = ENOMEM;
+                                        need_unwind = 1;
+                                        qr_fd_ctx->open_in_transit = 0;
+                                        goto unlock;
+                                }
+
+                                list_add_tail (&stub->list,
+                                               &qr_fd_ctx->waiting_ops);
+                        }
+                }
+        unlock:
+                UNLOCK (&qr_fd_ctx->lock);
+        } else {
+                can_wind = 1;
+        }
+
+out:
+        if (need_unwind) {
+                STACK_UNWIND (frame, op_ret, op_errno, NULL);
+        } else if (can_wind) {
+                STACK_WIND (frame, qr_fchown_cbk, FIRST_CHILD (this),
+                            FIRST_CHILD (this)->fops->fchown, fd, uid, gid);
+        } else if (need_open) {
+                op_ret = qr_loc_fill (&loc, fd->inode, path);
+                if (op_ret == -1) {
+                        qr_resume_pending_ops (qr_fd_ctx);
+                        goto out;
+                }
+
+                STACK_WIND (frame, qr_open_cbk, FIRST_CHILD(this),
+                            FIRST_CHILD(this)->fops->open, &loc, flags, fd);
+
+                qr_loc_wipe (&loc);
+        }
+        
+        return 0;
+}
+
+
 int32_t 
 init (xlator_t *this)
 {
@@ -1167,6 +1261,7 @@ struct xlator_fops fops = {
         .readv       = qr_readv,
         .writev      = qr_writev,
         .fstat       = qr_fstat,
+        .fchown      = qr_fchown,
 };
 
 
