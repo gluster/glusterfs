d02b9245c4c5d5b770500efdd1819efebdbf9233
Vijay Bellur <vijay@gluster.com>
2009-10-05 23:59:13 +0000

    Changed occurrences of Z Research to Gluster.
    Signed-off-by: Anand V. Avati <avati@dev.gluster.com>

--- xlators/cluster/dht/src/dht-common.c
+++ xlators/cluster/dht/src/dht-common.c
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2009-2009 Z RESEARCH, Inc. <http://www.zresearch.com>
+   Copyright (c) 2009-2009 Gluster, Inc. <http://www.gluster.com>
    This file is part of GlusterFS.
 
    GlusterFS is free software; you can redistribute it and/or modify
