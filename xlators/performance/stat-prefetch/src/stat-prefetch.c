dd5e622be1dc6450c66ebca991081304f6254889
Raghavendra G <raghavendra@gluster.com>
2009-10-08 06:21:31 +0000

    performance/stat-prefetch: lookup path in mkdir if it is not already looked up.
    Signed-off-by: Anand V. Avati <avati@dev.gluster.com>
    
    BUG: 221 (stat prefetch implementation)
    URL: http://bugs.gluster.com/cgi-bin/bugzilla3/show_bug.cgi?id=221

--- xlators/performance/stat-prefetch/src/stat-prefetch.c
+++ xlators/performance/stat-prefetch/src/stat-prefetch.c
@@ -1181,35 +1181,98 @@ sp_new_entry_cbk (call_frame_t *frame, void *cookie, xlator_t *this,
 
 
 int32_t
+sp_mkdir_helper (call_frame_t *frame, xlator_t *this, loc_t *loc, mode_t mode)
+{
+        uint64_t        value     = 0;
+        sp_inode_ctx_t *inode_ctx = NULL;
+        int32_t         ret       = 0, op_ret = -1, op_errno = -1;
+         
+        ret = inode_ctx_get (loc->inode, this, &value);
+        if (ret == -1) {
+                gf_log (this->name, GF_LOG_DEBUG, "context not set in inode "
+                        "(%p)", loc->inode);
+                op_errno = EINVAL;
+                goto unwind;
+        }
+
+        inode_ctx = (sp_inode_ctx_t *)(long) value;
+        GF_VALIDATE_OR_GOTO_WITH_ERROR (this->name, inode_ctx, unwind, op_errno,
+                                        EINVAL);
+
+        LOCK (&inode_ctx->lock);
+        {
+                op_ret = inode_ctx->op_ret;
+                op_errno = inode_ctx->op_errno;
+        }
+        UNLOCK (&inode_ctx->lock);
+
+        if (((op_ret == -1) && (op_errno != ENOENT))
+            || (op_ret == 0)) {
+                if (op_ret == 0) {
+                        op_ret = -1;
+                        op_errno = EEXIST;
+                }
+
+                goto unwind;
+        }
+
+        STACK_WIND (frame, sp_new_entry_cbk, FIRST_CHILD(this),
+                    FIRST_CHILD(this)->fops->mkdir, loc, mode);
+
+        return 0;
+
+unwind:
+        SP_STACK_UNWIND (frame, -1, op_errno, NULL, NULL, NULL);
+        return 0;
+}
+
+
+int32_t
 sp_mkdir (call_frame_t *frame, xlator_t *this, loc_t *loc, mode_t mode)
 {
-        int32_t     ret = 0, op_errno = EINVAL;
+        int32_t         ret          = -1, op_errno = -1;
+        call_stub_t    *stub         = NULL;
+        char            can_wind     = 0, need_lookup = 0, need_unwind = 1;
 
-        GF_VALIDATE_OR_GOTO_WITH_ERROR (this->name, loc, unwind, op_errno,
+        GF_VALIDATE_OR_GOTO_WITH_ERROR (this->name, loc, out, op_errno,
                                         EINVAL);
-        GF_VALIDATE_OR_GOTO_WITH_ERROR (this->name, loc->parent, unwind,
+        GF_VALIDATE_OR_GOTO_WITH_ERROR (this->name, loc->parent, out,
                                         op_errno, EINVAL);
-        GF_VALIDATE_OR_GOTO_WITH_ERROR (this->name, loc->path, unwind, op_errno,
+        GF_VALIDATE_OR_GOTO_WITH_ERROR (this->name, loc->path, out, op_errno,
                                         EINVAL);
-        GF_VALIDATE_OR_GOTO_WITH_ERROR (this->name, loc->name, unwind, op_errno,
+        GF_VALIDATE_OR_GOTO_WITH_ERROR (this->name, loc->name, out, op_errno,
                                         EINVAL);
-        GF_VALIDATE_OR_GOTO_WITH_ERROR (this->name, loc->inode, unwind,
+        GF_VALIDATE_OR_GOTO_WITH_ERROR (this->name, loc->inode, out,
                                         op_errno, EINVAL);
 
         ret = sp_cache_remove_parent_entry (frame, this, (char *)loc->path);
         if (ret == -1) {
                 op_errno = ENOMEM;
                 gf_log (this->name, GF_LOG_ERROR, "out of memory");
-                goto unwind;
+                goto out;
         }
 
-	STACK_WIND (frame, sp_new_entry_cbk, FIRST_CHILD(this),
-                    FIRST_CHILD(this)->fops->mkdir, loc, mode);
+        stub = fop_mkdir_stub (frame, sp_mkdir_helper, loc, mode);
+        if (stub == NULL) {
+                op_errno = ENOMEM;
+                gf_log (this->name, GF_LOG_ERROR, "out of memory");
+                goto out;
+        }
 
-        return 0;
+        sp_process_inode_ctx (frame, this, loc, stub, &need_unwind,
+                              &need_lookup, &can_wind, &op_errno);
 
-unwind:
-        SP_STACK_UNWIND (frame, -1, op_errno, loc->inode, NULL);
+out:
+        if (need_unwind) {
+                SP_STACK_UNWIND (frame, -1, op_errno, NULL, NULL, NULL);
+        } else if (need_lookup) {
+                STACK_WIND (frame, sp_lookup_cbk, FIRST_CHILD(this),
+                            FIRST_CHILD(this)->fops->lookup, loc, NULL);
+        } else if (can_wind) {
+                STACK_WIND (frame, sp_new_entry_cbk, FIRST_CHILD(this),
+                            FIRST_CHILD(this)->fops->mkdir, loc, mode);
+        }
+         
         return 0;
 }
 
