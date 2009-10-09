b2f4e1794bc8acbb553b63f26989cb60a5a4eae8
Raghavendra G <raghavendra@gluster.com>
2009-10-09 12:40:19 +0000

    performance/quick-read: change the default option values in quick-read.
    Signed-off-by: Anand V. Avati <avati@dev.gluster.com>
    
    BUG: 273 (Code review and optimize quick-read)
    URL: http://bugs.gluster.com/cgi-bin/bugzilla3/show_bug.cgi?id=273

--- xlators/performance/quick-read/src/quick-read.c
+++ xlators/performance/quick-read/src/quick-read.c
@@ -2159,6 +2159,7 @@ init (xlator_t *this)
                 goto out;
         }
 
+        conf->max_file_size = 65536;
         ret = dict_get_str (this->options, "max-file-size", 
                             &str);
         if (ret == 0) {
@@ -2173,7 +2174,7 @@ init (xlator_t *this)
                 }
         }
 
-        conf->cache_timeout = -1;
+        conf->cache_timeout = 1;
         ret = dict_get_str (this->options, "cache-timeout", &str);
         if (ret == 0) {
                 ret = gf_string2uint_base10 (str, 
