0c4b5814e7402658493f1e16ef7fa9a8fa8b4be5
Amar Tumballi <amar@gluster.com>
2009-11-28 03:09:02 +0000

    bring new MOP called NOTIFY.
    which can be used as a bridge between client and server processes,
    when needed.
    
    This mop is needed now, so that many features in 3.0.0 can come in
    with this without bumping protocol version.
    
    Signed-off-by: Amar Tumballi <amar@gluster.com>
    Signed-off-by: Anand V. Avati <avati@dev.gluster.com>
    
    BUG: 201 (server should get notified about client 'umount')
    URL: http://bugs.gluster.com/cgi-bin/bugzilla3/show_bug.cgi?id=201

--- libglusterfs/src/glusterfs.h
+++ libglusterfs/src/glusterfs.h
@@ -135,9 +135,10 @@ typedef enum {
         GF_MOP_STATS,
         GF_MOP_SETSPEC,
         GF_MOP_GETSPEC,
-	GF_MOP_PING,
+	GF_MOP_PING,      /* 5 */
         GF_MOP_LOG,
-        GF_MOP_MAXVALUE   /* 5 */
+        GF_MOP_NOTIFY,
+        GF_MOP_MAXVALUE   /* 8 */
 } glusterfs_mop_t;
 
 typedef enum {
