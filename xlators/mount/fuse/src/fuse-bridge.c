54046015d3c732b48304c9a5ecf7a7f8ff94c90e
Pavan Vilas Sondur <pavan@gluster.com>
2009-12-03 14:51:12 +0000

    mount/fuse: Print correct log message if encountered with errno - EAGAIN.
    Signed-off-by: Pavan Vilas Sondur <pavan@gluster.com>
    Signed-off-by: Anand V. Avati <avati@dev.gluster.com>
    
    BUG: 336 (Use lock owner field from fuse in locks)
    URL: http://bugs.gluster.com/cgi-bin/bugzilla3/show_bug.cgi?id=336

--- xlators/mount/fuse/src/fuse-bridge.c
+++ xlators/mount/fuse/src/fuse-bridge.c
@@ -2861,6 +2861,15 @@ fuse_setlk_cbk (call_frame_t *frame, void *cookie, xlator_t *this,
                                         "'features/posix-locks' on server side "
                                         "will add SETLK support.");
                         }
+                } else if (op_errno == EAGAIN) {
+                        gf_log ("glusterfs-fuse", GF_LOG_DEBUG,
+                                "Returning EAGAIN Flock: "
+                                "start=%llu, len=%llu, pid=%llu, lk-owner=%llu",
+                                (unsigned long long) lock->l_start,
+                                (unsigned long long) lock->l_len,
+                                (unsigned long long) lock->l_pid,
+                                (unsigned long long) frame->root->lk_owner);
+
                 } else  {
                         gf_log ("glusterfs-fuse", GF_LOG_WARNING,
                                 "%"PRIu64": ERR => -1 (%s)",
