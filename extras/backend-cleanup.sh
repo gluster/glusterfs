421f3e0f79455af41401cacbf511d9c80e817605
Pavan Sondur <pavan@gluster.com>
2010-01-19 08:11:05 +0000

    extras: Add defrag scripts to the repository
    Signed-off-by: Pavan Vilas Sondur <pavan@gluster.com>
    Signed-off-by: Anand V. Avati <avati@dev.gluster.com>
    
    BUG: 478 (Add defrag scripts into glusterfs)
    URL: http://bugs.gluster.com/cgi-bin/bugzilla3/show_bug.cgi?id=478

---
+++ extras/backend-cleanup.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# This script can be used to cleanup the 'cluster/distribute' translator's 
+# stale link files. One may choose to run this only when number of subvolumes
+# to distribute volume gets increased (or decreased) 
+# 
+# This script has to be run on the servers, which are exporting the data to 
+# GlusterFS
+#
+# (c) 2009 Gluster Inc <http://www.gluster.com/> 
+
+set -e
+
+# Change the below variable as per the setup.
+export_directory="/export/glusterfs"
+
+clean_dir()
+{
+    # Clean the 'link' files on backend
+    find "${export_directory}" -type f -perm +01000 -exec rm -v '{}' \; 
+}
+
+main()
+{
+    clean_dir ;
+}
+
+main "$@"
