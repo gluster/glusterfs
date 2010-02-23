gluster/glusterfs

commit 512dcad823feb199d78f5b66a09bc404ccb899e8

distribute: Support readdirp fop
Signed-off-by: Anand V. Avati <avati@dev.gluster.com>

BUG: 292 (Separate readdirp functionality from readdir fop)
URL: http://bugs.gluster.com/cgi-bin/bugzilla3/show_bug.cgi?id=292

xlators/cluster/dht/src/dht-common.c
@@ -2024,6 +2024,91 @@ err:
 
 
 int
+dht_readdirp_cbk (call_frame_t *frame, void *cookie, xlator_t *this, int op_ret,
+                  int op_errno, gf_dirent_t *orig_entries)
+{
+	dht_local_t  *local = NULL;
+	gf_dirent_t   entries;
+	gf_dirent_t  *orig_entry = NULL;
+	gf_dirent_t  *entry = NULL;
+	call_frame_t *prev = NULL;
+	xlator_t     *next_subvol = NULL;
+        off_t         next_offset = 0;
+	int           count = 0;
+
+
+	INIT_LIST_HEAD (&entries.list);
+	prev = cookie;
+	local = frame->local;
+
+	if (op_ret < 0)
+		goto done;
+
+	list_for_each_entry (orig_entry, (&orig_entries->list), list) {
+                next_offset = orig_entry->d_off;
+
+                if (check_is_linkfile (NULL, (&orig_entry->d_stat), NULL)
+                    || (check_is_dir (NULL, (&orig_entry->d_stat), NULL)
+                        && (prev->this != dht_first_up_subvol (this)))) {
+                        continue;
+                }
+
+                entry = gf_dirent_for_name (orig_entry->d_name);
+                if (!entry) {
+                        gf_log (this->name, GF_LOG_ERROR,
+                                "Out of memory");
+                        goto unwind;
+                }
+
+                dht_itransform (this, prev->this, orig_entry->d_ino,
+                                &entry->d_ino);
+                dht_itransform (this, prev->this, orig_entry->d_off,
+                                &entry->d_off);
+
+                entry->d_stat.st_ino = entry->d_ino;
+                entry->d_type = orig_entry->d_type;
+                entry->d_len  = orig_entry->d_len;
+
+                list_add_tail (&entry->list, &entries.list);
+                count++;
+	}
+	op_ret = count;
+
+done:
+	if (count == 0) {
+                /* non-zero next_offset means that
+                   EOF is not yet hit on the current subvol
+                */
+                if (next_offset == 0) {
+                        next_subvol = dht_subvol_next (this, prev->this);
+                } else {
+                        next_subvol = prev->this;
+                }
+
+		if (!next_subvol) {
+			goto unwind;
+		}
+
+		STACK_WIND (frame, dht_readdirp_cbk,
+			    next_subvol, next_subvol->fops->readdirp,
+			    local->fd, local->size, next_offset);
+		return 0;
+	}
+
+unwind:
+	if (op_ret < 0)
+		op_ret = 0;
+
+	DHT_STACK_UNWIND (frame, op_ret, op_errno, &entries);
+
+	gf_dirent_free (&entries);
+
+        return 0;
+}
+
+
+
+int
 dht_readdir_cbk (call_frame_t *frame, void *cookie, xlator_t *this,
 		 int op_ret, int op_errno, gf_dirent_t *orig_entries)
 {
@@ -2107,8 +2192,8 @@ unwind:
 
 
 int
-dht_readdir (call_frame_t *frame, xlator_t *this,
-	     fd_t *fd, size_t size, off_t yoff)
+dht_do_readdir (call_frame_t *frame, xlator_t *this, fd_t *fd, size_t size,
+                off_t yoff, int whichop)
 {
 	dht_local_t  *local  = NULL;
 	dht_conf_t   *conf = NULL;
@@ -2137,9 +2222,12 @@ dht_readdir (call_frame_t *frame, xlator_t *this,
 	dht_deitransform (this, yoff, &xvol, (uint64_t *)&xoff);
 
 	/* TODO: do proper readdir */
-	STACK_WIND (frame, dht_readdir_cbk,
-		    xvol, xvol->fops->readdir,
-		    fd, size, xoff);
+        if (whichop == GF_FOP_READDIR)
+                STACK_WIND (frame, dht_readdir_cbk, xvol, xvol->fops->readdir,
+                            fd, size, xoff);
+        else
+                STACK_WIND (frame, dht_readdirp_cbk, xvol, xvol->fops->readdirp,
+                            fd, size, xoff);
 
 	return 0;
 
@@ -2149,6 +2237,24 @@ err:
 
 	return 0;
 }
+
+
+int
+dht_readdir (call_frame_t *frame, xlator_t *this, fd_t *fd, size_t size,
+             off_t yoff)
+{
+        dht_do_readdir (frame, this, fd, size, yoff, GF_FOP_READDIR);
+        return 0;
+}
+
+int
+dht_readdirp (call_frame_t *frame, xlator_t *this, fd_t *fd, size_t size,
+              off_t yoff)
+{
+        dht_do_readdir (frame, this, fd, size, yoff, GF_FOP_READDIRP);
+        return 0;
+}
+
 
 
 int

xlators/cluster/dht/src/dht.c
@@ -370,6 +370,7 @@ struct xlator_fops fops = {
 	.lk          = dht_lk,
 	.opendir     = dht_opendir,
 	.readdir     = dht_readdir,
+	.readdirp    = dht_readdirp,
 	.fsyncdir    = dht_fsyncdir,
 	.symlink     = dht_symlink,
 	.unlink      = dht_unlink,

commit d180f3ec6c641b9708085ffe65fa4d93500bee84

dht: fix READDIR semantics which got broken in 512dcad823feb199d78f5b66a09bc404ccb899e8
Signed-off-by: Anand V. Avati <avati@blackhole.gluster.com>
Signed-off-by: Anand V. Avati <avati@dev.gluster.com>

BUG: 584 (automatically configure 'lookup-unhashed' option)
URL: http://bugs.gluster.com/cgi-bin/bugzilla3/show_bug.cgi?id=584

xlators/cluster/dht/src/dht-common.c
@@ -2121,9 +2121,9 @@ dht_readdirp_cbk (call_frame_t *frame, void *cookie, xlator_t *this, int op_ret,
 		goto done;
 
         if (!local->layout)
-                local->layout = layout = dht_layout_get (this, local->fd->inode);
-        else
-                layout = local->layout;
+                local->layout = dht_layout_get (this, local->fd->inode);
+
+        layout = local->layout;
 
 	list_for_each_entry (orig_entry, (&orig_entries->list), list) {
                 next_offset = orig_entry->d_off;
@@ -2225,46 +2225,36 @@ dht_readdir_cbk (call_frame_t *frame, void *cookie, xlator_t *this,
 	if (op_ret < 0)
 		goto done;
 
-        layout = dht_layout_get (this, local->fd->inode);
+        if (!local->layout)
+                local->layout = dht_layout_get (this, local->fd->inode);
+
+        layout = local->layout;
 
 	list_for_each_entry (orig_entry, (&orig_entries->list), list) {
                 next_offset = orig_entry->d_off;
 
-                if (check_is_linkfile (NULL, (&orig_entry->d_stat), NULL)
-                    || (check_is_dir (NULL, (&orig_entry->d_stat), NULL)
-                        && (prev->this != dht_first_up_subvol (this)))) {
-                        continue;
-                }
+                subvol = dht_layout_search (this, layout, orig_entry->d_name);
 
-                entry = gf_dirent_for_name (orig_entry->d_name);
-                if (!entry) {
-                        gf_log (this->name, GF_LOG_ERROR,
-                                "Out of memory");
-                        goto unwind;
-                }
-
-                /* Do this if conf->search_unhashed is set to "auto" */
-                if (conf->search_unhashed == GF_DHT_LOOKUP_UNHASHED_AUTO) {
-                        subvol = dht_layout_search (this, layout,
-                                                    orig_entry->d_name);
-                        if (!subvol || (subvol != prev->this)) {
-                                /* TODO: Count the number of entries which need
-                                   linkfile to prove its existance in fs */
-                                layout->search_unhashed++;
+                if (!subvol || (subvol != prev->this)) {
+                        entry = gf_dirent_for_name (orig_entry->d_name);
+                        if (!entry) {
+                                gf_log (this->name, GF_LOG_ERROR,
+                                        "memory allocation failed :(");
+                                goto unwind;
                         }
+
+                        dht_itransform (this, prev->this, orig_entry->d_ino,
+                                        &entry->d_ino);
+                        dht_itransform (this, prev->this, orig_entry->d_off,
+                                        &entry->d_off);
+
+                        entry->d_type = orig_entry->d_type;
+                        entry->d_len  = orig_entry->d_len;
+
+                        list_add_tail (&entry->list, &entries.list);
+                        count++;
                 }
-
-                dht_itransform (this, prev->this, orig_entry->d_ino,
-                                &entry->d_ino);
-                dht_itransform (this, prev->this, orig_entry->d_off,
-                                &entry->d_off);
-
-                entry->d_type = orig_entry->d_type;
-                entry->d_len  = orig_entry->d_len;
-
-                list_add_tail (&entry->list, &entries.list);
-                count++;
-	}
+        }
 	op_ret = count;
 
 done: