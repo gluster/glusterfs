e3ad79741be19d190a1722a27473531edd1498bf
Vikas Gorur <vikas@gluster.com>
2010-05-03 18:10:46 +0000

    cluster/afr: Unwind lk using struct flock from a successful server
    Signed-off-by: Vikas Gorur <vikas@gluster.com>
    Signed-off-by: Anand V. Avati <avati@dev.gluster.com>
    
    BUG: 882 (directory traversing problem (client crash))
    URL: http://bugs.gluster.com/cgi-bin/bugzilla3/show_bug.cgi?id=882

--- xlators/cluster/afr/src/afr.c
+++ xlators/cluster/afr/src/afr.c
@@ -2434,6 +2434,7 @@ afr_lk_cbk (call_frame_t *frame, void *cookie, xlator_t *this,
 		local->op_ret        = 0;
 		local->op_errno      = 0;
 		local->cont.lk.locked_nodes[child_index] = 1;
+		local->cont.lk.flock = *lock;
 	}
 
 	child_index++;
@@ -2452,7 +2453,7 @@ afr_lk_cbk (call_frame_t *frame, void *cookie, xlator_t *this,
 		/* locking has succeeded on all nodes that are up */
 		
 		AFR_STACK_UNWIND (lk, frame, local->op_ret, local->op_errno,
-                                  lock);
+                                  &local->cont.lk.flock);
 	}
 
 	return 0;
