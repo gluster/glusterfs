fac3ff8bfb3958a3bdc34dc9bff7cb281597e40f
Pavan Sondur <pavan@gluster.com>
2010-07-25 18:13:08 +0000

    syncop: initial implementation
    Resending Avati's syncop patch with a few bug fixes.
    
    (please do not skip the IMPORTANT NOTES section)
    
    * Framework for SYNChronous OPerations
    --------------------------------------
    
    This patch provides a framework for performing synchronous operations
    over the underlying actual asynchronous GlusterFS FOPS.
    
    * Use cases
    -----------
    
    1. Convenient implementation of crawler thread in replicate/pump
    
    2. Convenient implementation of high level control flow in DVM
    
    * Background
    ------------
    
    All (almost) threads in GlusterFS are hosts for executing aysnchronous
    file operations using the STACK_WIND and STACK_UNWIND primitives - as calls
    and callbacks.
    
    While the STACK_WIND and STACK_UNWIND macros provide high control for
    efficiently implementing file operations in a clustered/parallel environment,
    there are tasks where the nature of the task itself is sequential and
    the execution performance of the task is not critical. In these cases the
    complexity to implement the task with STACK_WIND/STACK_UNWIND based operations
    as calls and callbacks is an overkill.
    
    * Introduction
    ---------------
    
    syncop: are wrappers around the STACK_WIND/STACK_UNWIND based asynchronous fops.
    
    synctask: a sequential task (a C function) which uses syncops.
    
    syncenv: an environement to schedule and execute synctasks.
    
    The synchronicity is implemented via ucontext.h based continuations.
    
    Execution of synchronous tasks is possible only in a synchronous environment.
    Therefore, the first step  is to create such an environment -
    
            struct syncenv *env = syncenv_new (0);
    
    This creates a synchronous environment, with a thread (scheduler) to host the
    synchronous tasks. Creation of this environment is generally to be done at the
    time of process initialization. Next is to spawn a synchronous task in this
    environment -
    
            int slow_self_heal (void *data);
            int completion_func (int ret, void *data);
    
            ret = synctask_new (env, slow_self_heal, completion_func, data);
    
    Here slow_self_heal is a task which is implemented using synchronous operations.
    When slow_self_heal() completes, completion_func() is called with the first
    parameter as the return value of slow_self_heal(). Both these functions get
    the @data argument as the same value passed to synctask_new().
    
            int
            slow_self_heal (void *data)
            {
                xlator_t *child = FIRST_CHILD (THIS);
                fd_t     *dir = NULL;
    
                ...
    
                dir = syncop_opendir (child, loc);
                entry = syncop_readdir (dir);
    
                ...
    
                return ret;
            }
    
    * IMPORTANT NOTES
    -----------------
    
    - calling syncops in code executing outside the synchronous environment will
      very likely cause and undesired blocking of the executing thread leading to
      deadlocks!!
      The synchronous environment is a special thread where such sleeps are safe,
      and these sleeps result in the scheduler to 'swap in' other synctasks.
    
    - syncops can put the task to sleep. DO NOT issue syncops while holding mutexes.
      This is very similar to the blunder of holding a mutex and doing STACK_WIND.
    
    - It works best when synctasks use only syncops. If a call_frame is created and
      STACK_WIND'ed, the callback would very likely happen in a thread outside the
      synchronous enviroment, at an undefined time - as expected. So note that the
      synchronous environment does not tame the notorious behaviour of STACK_WIND.
    
    Signed-off-by: Anand V. Avati <avati@blackhole.gluster.com>
    Signed-off-by: Anand V. Avati <avati@dev.gluster.com>
    
    BUG: 971 (dynamic volume management)
    URL: http://bugs.gluster.com/cgi-bin/bugzilla3/show_bug.cgi?id=971

---
+++ libglusterfs/src/syncop.h
@@ -0,0 +1,164 @@
+/*
+   Copyright (c) 2010 Gluster, Inc. <http://www.gluster.com>
+   This file is part of GlusterFS.
+
+   GlusterFS is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published
+   by the Free Software Foundation; either version 3 of the License,
+   or (at your option) any later version.
+
+   GlusterFS is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see
+   <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _SYNCOP_H
+#define _SYNCOP_H
+
+#ifndef _CONFIG_H
+#define _CONFIG_H
+#include "config.h"
+#endif
+
+#include "xlator.h"
+#include <sys/time.h>
+#include <pthread.h>
+#include <ucontext.h>
+
+
+struct synctask;
+struct syncenv;
+
+
+typedef int (*synctask_cbk_t) (int ret, void *opaque);
+
+typedef int (*synctask_fn_t) (void *opaque);
+
+
+/* for one sequential execution of @syncfn */
+struct synctask {
+        struct list_head    all_tasks;
+        struct syncenv     *env;
+        xlator_t           *xl;
+        synctask_cbk_t      synccbk;
+        synctask_fn_t       syncfn;
+        void               *opaque;
+        void               *stack;
+        int                 complete;
+
+        ucontext_t          ctx;
+};
+
+/* hosts the scheduler thread and framework for executing synctasks */
+struct syncenv {
+        pthread_t           processor;
+        struct synctask    *current;
+
+        struct list_head    runq;
+        struct list_head    waitq;
+
+        pthread_mutex_t     mutex;
+        pthread_cond_t      cond;
+
+        ucontext_t          sched;
+        size_t              stacksize;
+};
+
+
+struct syncargs {
+        int                 op_ret;
+        int                 op_errno;
+        struct iatt         iatt1;
+        struct iatt         iatt2;
+        dict_t             *xattr;
+
+        /* do not touch */
+        pthread_mutex_t     mutex;
+        char                complete;
+        pthread_cond_t      cond;
+        struct synctask    *task;
+};
+
+
+#define __yawn(args) do {                                               \
+        struct synctask *task = NULL;                                   \
+                                                                        \
+        task = synctask_get ();                                         \
+        if (task) {                                                     \
+                args->task = task;                                      \
+                synctask_yawn (task);                                   \
+        } else {                                                        \
+                pthread_mutex_init (&args->mutex, NULL);                \
+                pthread_cond_init (&args->cond, NULL);                  \
+        }                                                               \
+} while (0)
+
+
+#define __yield(args) do {                                              \
+        if (args->task) {                                               \
+                synctask_yield (args->task);                            \
+        } else {                                                        \
+                pthread_mutex_lock (&args->mutex);                      \
+                {                                                       \
+                        while (!args->complete)                         \
+                                pthread_cond_wait (&args->cond,         \
+                                                   &args->mutex);       \
+                }                                                       \
+                pthread_mutex_unlock (&args->mutex);                    \
+                                                                        \
+                pthread_mutex_destroy (&args->mutex);                   \
+                pthread_cond_destroy (&args->cond);                     \
+        }                                                               \
+} while (0)
+
+
+#define __wake(args) do {                                               \
+        if (args->task) {                                               \
+                synctask_wake (args->task);                             \
+        } else {                                                        \
+                pthread_mutex_lock (&args->mutex);                      \
+                {                                                       \
+                        args->complete = 1;                             \
+                        pthread_cond_broadcast (&args->cond);           \
+                }                                                       \
+                pthread_mutex_unlock (&args->mutex);                    \
+        }                                                               \
+} while (0)
+
+
+#define SYNCOP(subvol, stb, cbk, op, params ...) do {                   \
+        call_frame_t    *frame = NULL;                                  \
+                                                                        \
+        frame = create_frame (THIS, THIS->ctx->pool);                   \
+                                                                        \
+        __yawn (stb);                                                   \
+        STACK_WIND_COOKIE (frame, (void *)stb, cbk, subvol, op, params);\
+        __yield (stb);                                                  \
+} while (0)
+
+
+#define SYNCENV_DEFAULT_STACKSIZE (16 * 1024)
+
+struct syncenv * syncenv_new ();
+void syncenv_destroy (struct syncenv *);
+
+int synctask_new (struct syncenv *, synctask_fn_t, synctask_cbk_t, void *);
+void synctask_zzzz (struct synctask *task);
+void synctask_yawn (struct synctask *task);
+void synctask_wake (struct synctask *task);
+void synctask_yield (struct synctask *task);
+
+int syncop_lookup (xlator_t *subvol, loc_t *loc, dict_t *xattr_req,
+                   /* out */
+                   struct iatt *iatt, dict_t **xattr_rsp, struct iatt *parent);
+
+int syncop_setattr (xlator_t *subvol, loc_t *loc, struct iatt *iatt, int valid,
+                    /* out */
+                    struct iatt *preop, struct iatt *postop);
+
+#endif /* _SYNCOP_H */
