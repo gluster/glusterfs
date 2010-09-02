fed472201fdf17ef57ea9b823b9215a0b3f558ec
Amar Tumballi <amar@gluster.com>
2010-09-02 07:26:54 +0000

    cluster/distribute: add option 'use-readdirp' to force readdirp
    Signed-off-by: Amar Tumballi <amar@gluster.com>
    Signed-off-by: Vijay Bellur <vijay@dev.gluster.com>
    
    BUG: 1508 (add-brick works but files are not distributed to newly added server)
    URL: http://bugs.gluster.com/cgi-bin/bugzilla3/show_bug.cgi?id=1508

--- xlators/cluster/dht/src/dht-common.c
+++ xlators/cluster/dht/src/dht-common.c
@@ -2644,6 +2644,9 @@ dht_readdir (call_frame_t *frame, xlator_t *this, fd_t *fd, size_t size,
                 }
         }
 
+        if (conf->use_readdirp)
+                op = GF_FOP_READDIRP;
+
         dht_do_readdir (frame, this, fd, size, yoff, op);
         return 0;
 }
