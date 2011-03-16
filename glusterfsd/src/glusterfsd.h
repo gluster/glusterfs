01d06b22c406acbbdfb222cd9d9c8596adadd66c
Amar Tumballi <amar@gluster.com>
2011-03-16 09:39:15 +0000

    glusterfsd: whitespace cleanup
    Signed-off-by: Amar Tumballi <amar@gluster.com>
    Signed-off-by: Vijay Bellur <vijay@dev.gluster.com>
    
    BUG: 2346 (Log message enhancements in GlusterFS - phase 1)
    URL: http://bugs.gluster.com/cgi-bin/bugzilla3/show_bug.cgi?id=2346

--- glusterfsd/src/glusterfsd.h
+++ glusterfsd/src/glusterfsd.h
@@ -1,20 +1,20 @@
 /*
-   Copyright (c) 2006-2010 Gluster, Inc. <http://www.gluster.com>
-   This file is part of GlusterFS.
+  Copyright (c) 2006-2010 Gluster, Inc. <http://www.gluster.com>
+  This file is part of GlusterFS.
 
-   GlusterFS is free software; you can redistribute it and/or modify
-   it under the terms of the GNU Affero General Public License as published
-   by the Free Software Foundation; either version 3 of the License,
-   or (at your option) any later version.
+  GlusterFS is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published
+  by the Free Software Foundation; either version 3 of the License,
+  or (at your option) any later version.
 
-   GlusterFS is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Affero General Public License for more details.
+  GlusterFS is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Affero General Public License for more details.
 
-   You should have received a copy of the GNU Affero General Public License
-   along with this program.  If not, see
-   <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see
+  <http://www.gnu.org/licenses/>.
 */
 
 #ifndef __GLUSTERFSD_H__
@@ -47,26 +47,26 @@
 #define ENABLE_DEBUG_MODE         1
 
 enum argp_option_keys {
-	ARGP_VOLFILE_SERVER_KEY = 's',
-	ARGP_VOLUME_FILE_KEY = 'f',
-	ARGP_LOG_LEVEL_KEY = 'L',
-	ARGP_LOG_FILE_KEY = 'l',
-	ARGP_VOLFILE_SERVER_PORT_KEY = 131,
-	ARGP_VOLFILE_SERVER_TRANSPORT_KEY = 132,
-	ARGP_PID_FILE_KEY = 'p',
+        ARGP_VOLFILE_SERVER_KEY = 's',
+        ARGP_VOLUME_FILE_KEY = 'f',
+        ARGP_LOG_LEVEL_KEY = 'L',
+        ARGP_LOG_FILE_KEY = 'l',
+        ARGP_VOLFILE_SERVER_PORT_KEY = 131,
+        ARGP_VOLFILE_SERVER_TRANSPORT_KEY = 132,
+        ARGP_PID_FILE_KEY = 'p',
         ARGP_SOCK_FILE_KEY = 'S',
-	ARGP_NO_DAEMON_KEY = 'N',
-	ARGP_RUN_ID_KEY = 'r',
-	ARGP_DEBUG_KEY = 133,
-	ARGP_ENTRY_TIMEOUT_KEY = 135,
-	ARGP_ATTRIBUTE_TIMEOUT_KEY = 136,
-	ARGP_VOLUME_NAME_KEY = 137,
-	ARGP_XLATOR_OPTION_KEY = 138,
-	ARGP_DIRECT_IO_MODE_KEY = 139,
+        ARGP_NO_DAEMON_KEY = 'N',
+        ARGP_RUN_ID_KEY = 'r',
+        ARGP_DEBUG_KEY = 133,
+        ARGP_ENTRY_TIMEOUT_KEY = 135,
+        ARGP_ATTRIBUTE_TIMEOUT_KEY = 136,
+        ARGP_VOLUME_NAME_KEY = 137,
+        ARGP_XLATOR_OPTION_KEY = 138,
+        ARGP_DIRECT_IO_MODE_KEY = 139,
 #ifdef GF_DARWIN_HOST_OS
-	ARGP_NON_LOCAL_KEY = 140,
+        ARGP_NON_LOCAL_KEY = 140,
 #endif /* DARWIN */
-	ARGP_VOLFILE_ID_KEY = 143,
+        ARGP_VOLFILE_ID_KEY = 143,
         ARGP_VOLFILE_CHECK_KEY = 144,
         ARGP_VOLFILE_MAX_FETCH_ATTEMPTS = 145,
         ARGP_LOG_SERVER_KEY = 146,
