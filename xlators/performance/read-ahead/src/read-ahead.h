d0b82760dc06b16f323d65506337c8373ce6ceb1
Raghavendra G <raghavendra@gluster.com>
2011-03-16 09:41:09 +0000

    performance/read-ahead: white space related changes to adhere to coding guidelines.
    Signed-off-by: Raghavendra G <raghavendra@gluster.com>
    Signed-off-by: Amar Tumballi <amar@gluster.com>
    Signed-off-by: Vijay Bellur <vijay@dev.gluster.com>
    
    BUG: 2346 (Log message enhancements in GlusterFS - phase 1)
    URL: http://bugs.gluster.com/cgi-bin/bugzilla3/show_bug.cgi?id=2346

--- xlators/performance/read-ahead/src/read-ahead.h
+++ xlators/performance/read-ahead/src/read-ahead.h
@@ -41,77 +41,77 @@ struct ra_waitq;
 
 
 struct ra_waitq {
-	struct ra_waitq *next;
-	void            *data;
+        struct ra_waitq *next;
+        void            *data;
 };
 
 
 struct ra_fill {
-	struct ra_fill *next;
-	struct ra_fill *prev;
-	off_t           offset;
-	size_t          size;
-	struct iovec   *vector;
-	int32_t         count;
+        struct ra_fill *next;
+        struct ra_fill *prev;
+        off_t           offset;
+        size_t          size;
+        struct iovec   *vector;
+        int32_t         count;
         struct iobref  *iobref;
 };
 
 
 struct ra_local {
-	mode_t            mode;
-	struct ra_fill    fill;
-	off_t             offset;
-	size_t            size;
-	int32_t           op_ret;
-	int32_t           op_errno;
-	off_t             pending_offset;
-	size_t            pending_size;
-	fd_t             *fd;
-	int32_t           wait_count;
-	pthread_mutex_t   local_lock;
+        mode_t            mode;
+        struct ra_fill    fill;
+        off_t             offset;
+        size_t            size;
+        int32_t           op_ret;
+        int32_t           op_errno;
+        off_t             pending_offset;
+        size_t            pending_size;
+        fd_t             *fd;
+        int32_t           wait_count;
+        pthread_mutex_t   local_lock;
 };
 
 
 struct ra_page {
-	struct ra_page   *next;
-	struct ra_page   *prev;
-	struct ra_file   *file;
-	char              dirty;
-	char              ready;
-	struct iovec     *vector;
-	int32_t           count;
-	off_t             offset;
-	size_t            size;
-	struct ra_waitq  *waitq;
+        struct ra_page   *next;
+        struct ra_page   *prev;
+        struct ra_file   *file;
+        char              dirty;
+        char              ready;
+        struct iovec     *vector;
+        int32_t           count;
+        off_t             offset;
+        size_t            size;
+        struct ra_waitq  *waitq;
         struct iobref    *iobref;
 };
 
 
 struct ra_file {
-	struct ra_file    *next;
-	struct ra_file    *prev;
-	struct ra_conf    *conf;
-	fd_t              *fd;
-	int                disabled;
-	size_t             expected;
-	struct ra_page     pages;
-	off_t              offset;
-	size_t             size;
-	int32_t            refcount;
-	pthread_mutex_t    file_lock;
-	struct iatt        stbuf;
-	uint64_t           page_size;
-	uint32_t           page_count;
+        struct ra_file    *next;
+        struct ra_file    *prev;
+        struct ra_conf    *conf;
+        fd_t              *fd;
+        int                disabled;
+        size_t             expected;
+        struct ra_page     pages;
+        off_t              offset;
+        size_t             size;
+        int32_t            refcount;
+        pthread_mutex_t    file_lock;
+        struct iatt        stbuf;
+        uint64_t           page_size;
+        uint32_t           page_count;
 };
 
 
 struct ra_conf {
-	uint64_t          page_size;
-	uint32_t          page_count;
-	void             *cache_block;
-	struct ra_file    files;
-	gf_boolean_t      force_atime_update;
-	pthread_mutex_t   conf_lock;
+        uint64_t          page_size;
+        uint32_t          page_count;
+        void             *cache_block;
+        struct ra_file    files;
+        gf_boolean_t      force_atime_update;
+        pthread_mutex_t   conf_lock;
 };
 
 
@@ -124,19 +124,19 @@ typedef struct ra_fill ra_fill_t;
 
 ra_page_t *
 ra_page_get (ra_file_t *file,
-	     off_t offset);
+             off_t offset);
 
 ra_page_t *
 ra_page_create (ra_file_t *file,
-		off_t offset);
+                off_t offset);
 
 void
 ra_page_fault (ra_file_t *file,
-	       call_frame_t *frame,
-	       off_t offset);
+               call_frame_t *frame,
+               off_t offset);
 void
 ra_wait_on_page (ra_page_t *page,
-		 call_frame_t *frame);
+                 call_frame_t *frame);
 
 ra_waitq_t *
 ra_page_wakeup (ra_page_t *page);
@@ -146,8 +146,8 @@ ra_page_flush (ra_page_t *page);
 
 ra_waitq_t *
 ra_page_error (ra_page_t *page,
-	       int32_t op_ret,
-	       int32_t op_errno);
+               int32_t op_ret,
+               int32_t op_errno);
 void
 ra_page_purge (ra_page_t *page);
 
@@ -156,7 +156,7 @@ ra_frame_return (call_frame_t *frame);
 
 void
 ra_frame_fill (ra_page_t *page,
-	       call_frame_t *frame);
+               call_frame_t *frame);
 
 void
 ra_file_destroy (ra_file_t *file);
@@ -164,36 +164,36 @@ ra_file_destroy (ra_file_t *file);
 static inline void
 ra_file_lock (ra_file_t *file)
 {
-	pthread_mutex_lock (&file->file_lock);
+        pthread_mutex_lock (&file->file_lock);
 }
 
 static inline void
 ra_file_unlock (ra_file_t *file)
 {
-	pthread_mutex_unlock (&file->file_lock);
+        pthread_mutex_unlock (&file->file_lock);
 }
 
 static inline void
 ra_conf_lock (ra_conf_t *conf)
 {
-	pthread_mutex_lock (&conf->conf_lock);
+        pthread_mutex_lock (&conf->conf_lock);
 }
 
 static inline void
 ra_conf_unlock (ra_conf_t *conf)
 {
-	pthread_mutex_unlock (&conf->conf_lock);
+        pthread_mutex_unlock (&conf->conf_lock);
 }
 static inline void
 ra_local_lock (ra_local_t *local)
 {
-	pthread_mutex_lock (&local->local_lock);
+        pthread_mutex_lock (&local->local_lock);
 }
 
 static inline void
 ra_local_unlock (ra_local_t *local)
 {
-	pthread_mutex_unlock (&local->local_lock);
+        pthread_mutex_unlock (&local->local_lock);
 }
 
 #endif /* __READ_AHEAD_H */
