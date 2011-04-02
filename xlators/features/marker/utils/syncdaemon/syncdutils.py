443c9ba6a00e0f3bb4069904accd614ae8d97436
Csaba Henk <csaba@gluster.com>
2011-04-02 19:40:48 +0000

    syncdaemon: provide transactional semantics to config file writing
    So updating the config file from multiple contexts won't mess it up.
    This prepares the next commit where we'll set options internaly (which lacks
    the serial nature of user actions).
    
    Signed-off-by: Csaba Henk <csaba@gluster.com>
    Signed-off-by: Vijay Bellur <vijay@dev.gluster.com>
    
    BUG: 2537 (gsync autorestart)
    URL: http://bugs.gluster.com/cgi-bin/bugzilla3/show_bug.cgi?id=2537

--- xlators/features/marker/utils/syncdaemon/syncdutils.py
+++ xlators/features/marker/utils/syncdaemon/syncdutils.py
@@ -1,3 +1,5 @@
+import os
+import fcntl
 try:
     # py 3
     from urllib import parse as urllib
@@ -9,3 +11,32 @@ def escape(s):
 
 def unescape(s):
     return urllib.unquote_plus(s)
+
+def update_file(path, updater, merger = lambda f: True):
+    """update a file in a transaction-like manner"""
+
+    fr = fw = None
+    try:
+        fd = os.open(path, os.O_CREAT|os.O_RDWR)
+        try:
+            fr = os.fdopen(fd, 'r+b')
+        except:
+            os.close(fd)
+            raise
+        fcntl.lockf(fr, fcntl.LOCK_EX)
+        merger(fr)
+
+        tmpp = path + '.tmp.' + str(os.getpid())
+        fd = os.open(tmpp, os.O_CREAT|os.O_EXCL|os.O_WRONLY)
+        try:
+            fw = os.fdopen(fd, 'wb', 0)
+        except:
+            os.close(fd)
+            raise
+        updater(fw)
+        os.fsync(fd)
+        os.rename(tmpp, path)
+    finally:
+        for fx in (fr, fw):
+            if fx:
+                fx.close()
