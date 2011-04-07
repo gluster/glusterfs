014508ed15caba48a6638eb48ab1f93ba06ee61e
Raghavendra G <raghavendra@gluster.com>
2011-04-07 02:51:50 +0000

    extras: scripts to cleanup xattrs stored by quota.
    Signed-off-by: Raghavendra G <raghavendra@gluster.com>
    Signed-off-by: Vijay Bellur <vijay@gluster.com>
    
    BUG: 2664 (Quota: recreating the volume on same bricks shows similar info as for earlier one using "list")
    URL: http://bugs.gluster.com/cgi-bin/bugzilla3/show_bug.cgi?id=2664

---
+++ extras/quota-remove-xattr.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This script is used to remove xattrs set by quota translator on a path.
+# It is generally invoked from quota-metadata-cleanup.sh, but can
+# also be used stand-alone.
+
+usage ()
+{
+    echo >&2 "usage: $0 <path>"
+}
+
+main ()
+{
+    [ $# -ne 1 ] && usage $0
+
+    XATTR_KEY_VALUE_PAIRS=`getfattr -d -m 'trusted.glusterfs.quota' $1 2>/dev/null | sed -e '/^# file/d'`
+
+    for i in $XATTR_KEY_VALUE_PAIRS; do
+        XATTR_KEY=`echo $i | sed -e 's/\([^=]*\).*/\1/g'`
+        setfattr -x $XATTR_KEY $1
+    done
+}
+
+main $@
