d0c16635cb62795a94a208406d19a83b5ebe605c
Raghavendra G <raghavendra@gluster.com>
2011-04-16 10:07:29 +0000

    extras/quota-xattr-cleanup: don't follow symbolic links.
    Signed-off-by: Raghavendra G <raghavendra@gluster.com>
    Signed-off-by: Anand Avati <avati@gluster.com>
    
    BUG: 2664 (Quota: recreating the volume on same bricks shows similar info as for earlier one using "list")
    URL: http://bugs.gluster.com/cgi-bin/bugzilla3/show_bug.cgi?id=2664

--- extras/quota-remove-xattr.sh
+++ extras/quota-remove-xattr.sh
@@ -13,11 +13,11 @@ main ()
 {
     [ $# -ne 1 ] && usage $0
 
-    XATTR_KEY_VALUE_PAIRS=`getfattr -d -m 'trusted.glusterfs.quota' $1 2>/dev/null | sed -e '/^# file/d'`
+    XATTR_KEY_VALUE_PAIRS=`getfattr -h -d -m 'trusted.glusterfs.quota' $1 2>/dev/null | sed -e '/^# file/d'`
 
     for i in $XATTR_KEY_VALUE_PAIRS; do
         XATTR_KEY=`echo $i | sed -e 's/\([^=]*\).*/\1/g'`
-        setfattr -x $XATTR_KEY $1
+        setfattr -h -x $XATTR_KEY $1
     done
 }
 
