d91f3981b0d80161e985fd1038c3524ac82d8d8a
Pranith Kumar K <pranithk@gluster.com>
2011-05-31 01:54:55 +0000

    cluster/dht: notify should succeed when waiting for all subvols first event
    Signed-off-by: Pranith Kumar K <pranithk@gluster.com>
    Signed-off-by: Anand Avati <avati@gluster.com>
    
    BUG: 2870 (Inconsistent xattr values when creating bricks)
    URL: http://bugs.gluster.com/cgi-bin/bugzilla3/show_bug.cgi?id=2870

--- xlators/cluster/dht/src/dht-common.c
+++ xlators/cluster/dht/src/dht-common.c
@@ -5054,6 +5054,7 @@ dht_notify (xlator_t *this, int event, void *data, ...)
 		subvol = data;
 
 		conf->gen++;
+                propagate = 1;
 
 		break;
 
@@ -5145,7 +5146,8 @@ dht_notify (xlator_t *this, int event, void *data, ...)
                 }
         }
 
-        if (propagate || event == GF_EVENT_CHILD_MODIFIED)
+        ret = 0;
+        if (propagate)
                 ret = default_notify (this, event, data);
 
 	return ret;
