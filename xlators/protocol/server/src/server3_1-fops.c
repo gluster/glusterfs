476d5ab635e1069246c21d65773e9f9c91852217
Raghavendra Bhat <raghavendrabhat@gluster.com>
2011-06-02 09:19:34 +0000

    protocol/server: logging changes
    Signed-off-by: Raghavendra Bhat <raghavendrabhat@gluster.com>
    Signed-off-by: Anand Avati <avati@gluster.com>
    
    BUG: 2982 ([glusterfs-3.2.1qa2]: logging changes is server protocol)
    URL: http://bugs.gluster.com/cgi-bin/bugzilla3/show_bug.cgi?id=2982

--- xlators/protocol/server/src/server3_1-fops.c
+++ xlators/protocol/server/src/server3_1-fops.c
@@ -334,7 +334,7 @@ server_entrylk_cbk (call_frame_t *frame, void *cookie, xlator_t *this,
                                        frame->root->lk_owner, GF_FOP_ENTRYLK);
         } else if ((op_errno != ENOSYS) && (op_errno != EAGAIN)) {
                 gf_log (this->name, GF_LOG_INFO,
-                        "%"PRId64": INODELK %s (%"PRId64") ==> %"PRId32" (%s)",
+                        "%"PRId64": ENTRYLK %s (%"PRId64") ==> %"PRId32" (%s)",
                         frame->root->unique, state->loc.path,
                         state->loc.inode ? state->loc.inode->ino : 0, op_ret,
                         strerror (op_errno));
