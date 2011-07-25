9bbf82c31c94bf28e6d82da424bad1e106f1bd6e
Vijay Bellur <vijay@gluster.com>
2011-07-25 15:31:21 +0530

    libglusterfs: Bring in os_daemon_* routines to replace modified FreeBSD code
    Change-Id: I41f4635b1b75adb6d22e2e325b99941f8d7a0b42
    BUG: 3206
    Reviewed-on: http://review.gluster.com/100
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Amar Tumballi <amar@gluster.com>

---
+++ libglusterfs/src/daemon.c
@@ -0,0 +1,68 @@
+/*
+  Copyright (c) 2011 Gluster, Inc. <http://www.gluster.com>
+  This file is part of GlusterFS.
+
+  GlusterFS is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published
+  by the Free Software Foundation; either version 3 of the License,
+  or (at your option) any later version.
+
+  GlusterFS is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see
+  <http://www.gnu.org/licenses/>.
+*/
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include "daemon.h"
+
+int
+os_daemon_return (int nochdir, int noclose)
+{
+	pid_t   pid  = -1;
+	int     ret  = -1;
+        FILE    *ptr = NULL;
+
+
+	ret = fork();
+	if (ret)
+		return ret;
+
+	pid = setsid();
+
+	if (pid == -1) {
+                ret = -1;
+		goto out;
+        }
+
+	if (!nochdir)
+		ret = chdir("/");
+
+        if (!noclose) {
+                ptr = freopen (DEVNULLPATH, "r", stdin);
+                ptr = freopen (DEVNULLPATH, "w", stdout);
+                ptr = freopen (DEVNULLPATH, "w", stderr);
+	}
+
+        ret = 0;
+out:
+	return ret;
+}
+
+int
+os_daemon (int nochdir, int noclose)
+{
+	int ret = -1;
+
+	ret = os_daemon_return (nochdir, noclose);
+	if (ret <= 0)
+		return ret;
+
+	_exit (0);
+}
