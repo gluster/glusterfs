8fc7c940fac95b869e156c1b8209f85d88e7d9b3
Sachidananda Urs <sac@gluster.com>
2011-07-29 16:26:03 +0530

    Remove redundant function `cli_path_strip_trailing_slashes'.
    Use cli_canonicalize_path instead, to remove trailing/duplicate slashes.
    
    Change-Id: Ie76267a4fa9ee9986cb36039afad4dfff830d5f8
    BUG: 3183
    Reviewed-on: http://review.gluster.com/124
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Anand Avati <avati@gluster.com>

--- cli/src/cli.h
+++ cli/src/cli.h
@@ -234,8 +234,6 @@ cli_cmd_broadcast_connected ();
 int
 cli_rpc_notify (struct rpc_clnt *rpc, void *mydata, rpc_clnt_event_t event,
                 void *data);
-void
-cli_path_strip_trailing_slashes (char *path);
 
 int
 cli_canonicalize_path (char *path);
