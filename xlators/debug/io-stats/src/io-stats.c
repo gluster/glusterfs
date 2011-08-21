aa07e855859d6a175fccb309ec3158e6ca894b0c
Vijay Bellur <vijay@gluster.com>
2011-08-21 16:34:19 +0530

    debug/io-stats: Handle log-level properly.
    Change-Id: I8e5b8841705b161068bdb2ccd2ac119d6b250c88
    BUG: 3415
    Reviewed-on: http://review.gluster.com/284
    Reviewed-by: Raghavendra Bhat <raghavendrabhat@gluster.com>
    Tested-by: Gluster Build System <jenkins@build.gluster.com>

--- xlators/debug/io-stats/src/io-stats.c
+++ xlators/debug/io-stats/src/io-stats.c
@@ -2335,6 +2335,8 @@ reconfigure (xlator_t *this, dict_t *options)
         int                 ret = -1;
         char               *sys_log_str = NULL;
         int                 sys_log_level = -1;
+        char               *log_str = NULL;
+        int                 log_level = -1;
 
         if (!this || !this->private)
                 goto out;
@@ -2354,6 +2356,12 @@ reconfigure (xlator_t *this, dict_t *options)
         if (sys_log_str) {
                 sys_log_level = glusterd_check_log_level (sys_log_str);
                 set_sys_log_level (sys_log_level);
+        }
+
+        GF_OPTION_RECONF ("log-level", log_str, options, str, out);
+        if (log_str) {
+                log_level = glusterd_check_log_level (log_str);
+                gf_log_set_loglevel (log_level);
         }
 
         ret = 0;
