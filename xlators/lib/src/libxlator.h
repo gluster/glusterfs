aaac9cf176b407f89a1e8015140916b3f9d3cddb
Vijay Bellur <vijay@gluster.com>
2012-03-01 00:17:21 +0530

    Refactor xlator/lib for better readability & maintainability.
    Change-Id: I6468462ae2bbc05c1dd6c2aea66b440aa7cfdd09
    BUG: 789078
    Signed-off-by: Vijay Bellur <vijay@gluster.com>
    Reviewed-on: http://review.gluster.com/2845
    Tested-by: Gluster Build System <jenkins@build.gluster.com>

--- xlators/lib/src/libxlator.h
+++ xlators/lib/src/libxlator.h
@@ -1,3 +1,21 @@
+/*Copyright (c) 2008-2012 Red Hat, Inc. <http://www.redhat.com>
+  This file is part of GlusterFS.
+
+  GlusterFS is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published
+  by the Free Software Foundation; either version 3 of the License,
+  or (at your option) any later version.
+
+  GlusterFS is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see
+  <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef _LIBXLATOR_H
 #define _LIBXLATOR_H
 
@@ -58,8 +76,10 @@ struct marker_str {
         char                  *vol_uuid;
 };
 
+typedef struct marker_str xl_marker_local_t;
+
 static inline gf_boolean_t
-marker_has_volinfo (struct marker_str *marker)
+marker_has_volinfo (xl_marker_local_t *marker)
 {
        if (marker->volmark)
                 return _gf_true;
