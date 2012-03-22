572a375b674bf9ba46c0715b3d0dd7ab165ce21e
Amar Tumballi <amarts@redhat.com>
2012-03-22 04:11:38 -0700

    Revert "core: enable process to return the appropriate error code"
    This reverts commit 16717f4df7b0fbd3c255f5faf8a2744444231bb7
    
    Change-Id: I65624db4d31e11e1bec66b07b352ce823ef069b9
    Reviewed-on: http://review.gluster.com/3001
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Vijay Bellur <vijay@gluster.com>

--- glusterfsd/src/glusterfsd-mgmt.c
+++ glusterfsd/src/glusterfsd-mgmt.c
@@ -54,7 +54,6 @@ int glusterfs_mgmt_pmap_signin (glusterfs_ctx_t *ctx);
 int glusterfs_volfile_fetch (glusterfs_ctx_t *ctx);
 int glusterfs_process_volfp (glusterfs_ctx_t *ctx, FILE *fp);
 int glusterfs_graph_unknown_options (glusterfs_graph_t *graph);
-int emancipate(glusterfs_ctx_t *ctx, int ret);
 
 int
 mgmt_cbk_spec (void *data)
@@ -1537,7 +1536,7 @@ mgmt_getspec_cbk (struct rpc_req *req, struct iovec *iov, int count,
         if (-1 == rsp.op_ret) {
                 gf_log (frame->this->name, GF_LOG_ERROR,
                         "failed to get the 'volume file' from server");
-                ret = rsp.op_errno;
+                ret = -1;
                 goto out;
         }
 
@@ -1598,8 +1597,6 @@ out:
         if (rsp.spec)
                 free (rsp.spec);
 
-        emancipate (ctx, ret);
-
         if (ret && ctx && !ctx->active) {
                 /* Do it only for the first time */
                 /* Failed to get the volume file, something wrong,
@@ -1643,8 +1640,6 @@ mgmt_rpc_notify (struct rpc_clnt *rpc, void *mydata, rpc_clnt_event_t event,
         cmd_args_t      *cmd_args = NULL;
         glusterfs_ctx_t *ctx = NULL;
         int              ret = 0;
-        int need_term = 0;
-        int emval = 0;
 
         this = mydata;
         ctx = this->ctx;
@@ -1659,39 +1654,28 @@ mgmt_rpc_notify (struct rpc_clnt *rpc, void *mydata, rpc_clnt_event_t event,
                         gf_log ("glusterfsd-mgmt", GF_LOG_INFO,
                                 "%d connect attempts left",
                                 cmd_args->max_connect_attempts);
-                        if (0 >= cmd_args->max_connect_attempts) {
-                                need_term = 1;
-                                emval = ENOTCONN;
-                        }
+                        if (0 >= cmd_args->max_connect_attempts)
+                                cleanup_and_exit (1);
                 }
                 break;
         case RPC_CLNT_CONNECT:
                 rpc_clnt_set_connected (&((struct rpc_clnt*)ctx->mgmt)->conn);
 
                 ret = glusterfs_volfile_fetch (ctx);
-                if (ret) {
-                        emval = ret;
-                        if (!ctx->active) {
-                                need_term = 1;
-                                gf_log ("glusterfsd-mgmt", GF_LOG_ERROR,
-                                        "failed to fetch volume file (key:%s)",
-                                        ctx->cmd_args.volfile_id);
-                                break;
-
-                        }
+                if (ret && ctx && (ctx->active == NULL)) {
+                        /* Do it only for the first time */
+                        /* Exit the process.. there is some wrong options */
+                        gf_log ("mgmt", GF_LOG_ERROR,
+                                "failed to fetch volume file (key:%s)",
+                                ctx->cmd_args.volfile_id);
+                        cleanup_and_exit (0);
                 }
 
                 if (is_mgmt_rpc_reconnect)
                         glusterfs_mgmt_pmap_signin (ctx);
-
                 break;
         default:
                 break;
-        }
-
-        if (need_term) {
-                emancipate (ctx, emval);
-                cleanup_and_exit (1);
         }
 
         return 0;
