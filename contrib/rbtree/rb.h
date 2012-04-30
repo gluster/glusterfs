67ceb347c83e3b994791feb58fc607475fa1177e
Kaleb KEITHLEY <kkeithle@redhat.com>
2012-04-30 09:36:38 -0400

    contrib/rbtree: additional license from Richard Fontana
    BUG: 807724
    Change-Id: I1e9a7aac7b535687b9e48bdb0e94c7f52dc6aad7
    Signed-off-by: Kaleb KEITHLEY <kkeithle@redhat.com>
    Reviewed-on: http://review.gluster.com/3252
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Anand Avati <avati@redhat.com>

--- contrib/rbtree/rb.h
+++ contrib/rbtree/rb.h
@@ -18,6 +18,13 @@
    License along with this library; if not, write to the Free Software
    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
    02110-1301 USA.
+
+   This code is also covered by the following earlier license notice:
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
 */
 
 #ifndef RB_H
