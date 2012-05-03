d3cc5862faa9ebda4d8d80f6b3e7a45c0df85c0c
Krishnan Parthasarathi <kp@gluster.com>
2012-05-03 16:29:06 +0530

    glusterd: Made dst brick's port info available to all peers
    Change-Id: I1f65743a31d95013fdf22cded91c314e9934a3a9
    BUG: 816915
    Signed-off-by: Krishnan Parthasarathi <kp@gluster.com>
    Reviewed-on: http://review.gluster.org/3275
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Vijay Bellur <vbellur@redhat.com>

--- xlators/mgmt/glusterd/src/glusterd-store.h
+++ xlators/mgmt/glusterd/src/glusterd-store.h
@@ -51,6 +51,7 @@ typedef enum glusterd_store_ver_ac_{
 #define GLUSTERD_STORE_KEY_RB_STATUS      "rb_status"
 #define GLUSTERD_STORE_KEY_RB_SRC_BRICK   "rb_src"
 #define GLUSTERD_STORE_KEY_RB_DST_BRICK   "rb_dst"
+#define GLUSTERD_STORE_KEY_RB_DST_PORT    "rb_port"
 #define GLUSTERD_STORE_KEY_VOL_DEFRAG     "rebalance_status"
 #define GLUSTERD_STORE_KEY_DEFRAG_OP      "rebalance_op"
 #define GLUSTERD_STORE_KEY_USERNAME       "username"
