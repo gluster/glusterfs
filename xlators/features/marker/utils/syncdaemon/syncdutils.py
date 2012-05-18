c40b73fc453caf123e806adebac6c69b003bc636
Csaba Henk <csaba@redhat.com>
2012-05-19 02:43:05 +0530

    geo-rep / gsyncd: further cleanup refinements
    - Regarding issue of leftover ssh control dirs:
    
      If master side worker is stuck in connection establishment
      phase, have the monitor kill it softly (ie. first by SIGTERM,
      to let it cleanup). This is trickier than sounds on first hearing,
      because if worker is stuck in waiting for a RePCe answer
      (in threading.Condition().wait()), then SIGTERM is ignored
      (more precisely, Python holds it back for the wait and resends it to
      itself when wait is over).
    
      So instead of signalling the worker only, we send TERM to the
      whole process group -- that brings down the ssh connection, which
      wakes up the waiting worker, which then can cleanup. Only problem
      is that monitor is also in the process group and it should not coomit
      a suicide. That is taken care by setting up a one-time SIGTERM
      handler in the monitor.
    
    - Regarding slave gsyncd stuck in chdir:
    
      Slave gsyncd is usually well behaved: if master does not send
      keepalives, it takes care to exit. However, if a hang occurs
      in early phase, when slave is to change to the gluster mountpoint,
      no timeout is set up for that (and unlike on master side, neither
      is there an external actor like the monitor to do that).
    
      So, to manage this scenario, we do the chdir in a (supposedly)
      short lived thread, and in the main thread we wait for the termination
      of this thread. If that does not happen within the time limit, main
      thread calls for cleanup and exit. (This logic explicitely takes the
      appropriate action in the cases when chdir succeeds or when hangs;
      but what about the remaining case, when chdir fails? Well in that case
      the chdir thread's exception handler will put the process to
      cleanup and exit route.)
    
    Change-Id: I6ad6faa9c7b1c37084d171d1e1a756abaff9eba8
    BUG: 786291
    Signed-off-by: Csaba Henk <csaba@redhat.com>
    Reviewed-on: http://review.gluster.com/3376
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Anand Avati <avati@redhat.com>

--- xlators/features/marker/utils/syncdaemon/syncdutils.py
+++ xlators/features/marker/utils/syncdaemon/syncdutils.py
@@ -7,7 +7,7 @@ import shutil
 import logging
 from threading import Lock, Thread as baseThread
 from errno import EACCES, EAGAIN, EPIPE, ENOTCONN, ECONNABORTED, EINTR, errorcode
-from signal import SIGTERM, SIGKILL
+from signal import signal, SIGTERM, SIGKILL
 from time import sleep
 import select as oselect
 from os import waitpid as owaitpid
@@ -271,3 +271,6 @@ def select(*a):
 
 def waitpid (*a):
     return eintr_wrap(owaitpid, OSError, *a)
+
+def set_term_handler(hook=lambda *a: finalize(*a, **{'exval': 1})):
+    signal(SIGTERM, hook)
