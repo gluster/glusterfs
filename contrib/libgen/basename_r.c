c97c3136bd2932705bf30b354417008b4e0eff2b
Emmanuel Dreyfus <manu@netbsd.org>
2012-05-25 06:39:48 +0200

    Switch to GNU basename_r() and dirname_r()
    BSD basename() and dirname() return static storage, hence they are not
    thread safe, while GNU flavors are. glusterfs assumes the GNU behavior.
    As a fix, we added a BSD-derived, thread-safe version of basename_r()
    and dirname_r() in contrib/libgen. BSD systems can use it instead of
    their built in versions.
    
    Unfortunately, there are more reliance on GNU behavior in glusterfs.
    There are place where free() is called on static storage returned by
    basename(), place where a second call to dirname() overwrite the
    string obtained once, and so forth.
    
    Each of these problems can be fixed, but it is likely to be a pandora
    box. We need to accept that glusterfs heavily rely on GNU behavior,
    and switch contrib/libgen's basename_r() and dirname_r() to the GNU
    version. This change does just that.
    
    BUG: 764655
    Change-Id: Id874b9c7aacd9aa3a7a4bd6a92a9633f5b2d6ac0
    Signed-off-by: Emmanuel Dreyfus <manu@netbsd.org>
    Reviewed-on: http://review.gluster.com/3430
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Anand Avati <avati@redhat.com>

--- contrib/libgen/basename_r.c
+++ contrib/libgen/basename_r.c
@@ -1,102 +1,36 @@
 /*
- * Copyright (c) 1997 Todd C. Miller <Todd.Miller@courtesan.com>
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
- * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
- * THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
- * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * borrowed from glibc-2.12.1/string/basename.c 
+ * Modified to return "." for NULL or "", as required for SUSv2.
  */
 
-/*
- * MT-SAFE by Kaleb S. KEITHLEY, Red Hat Inc., kkeithle@redhat.com
- */
+/* Return the name-within-directory of a file name.
+   Copyright (C) 1996,97,98,2002 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
 
-#if 0
-#ifndef lint
-static char rcsid[] = "$OpenBSD: basename.c,v 1.4 1999/05/30 17:10:30 espie Exp $";
-#endif /* not lint */
-#endif
-#if 0
-#include <sys/cdefs.h>
-__FBSDID("$FreeBSD: src/lib/libc/gen/basename.c,v 1.7.34.3.2.1 2010/12/21 17:09:25 kensmith Exp $");
-#endif
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
 
-#include <errno.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/param.h>
-#include <pthread.h>
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
 
-static pthread_key_t bname_key;
-
-static void
-make_bname_key(void)
-{
-      (void) pthread_key_create(&bname_key, free);
-}
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307 USA.  */
 
 char *
-basename_r(const char *path)
+basename_r (filename)
+     const char *filename;
 {
-	static pthread_once_t bname_key_once = PTHREAD_ONCE_INIT;
+  char *p;
 
-	const char *endp, *startp;
-	char *bname;
+  if ((filename == NULL) || (*filename == '\0'))
+    return ".";
 
-	(void) pthread_once(&bname_key_once, make_bname_key);
-
-	if ((bname = pthread_getspecific(bname_key)) == NULL) {
-		bname = (char *)malloc(MAXPATHLEN);
-		if (bname == NULL)
-			return(NULL);
-		(void) pthread_setspecific(bname_key, bname);
-	}
-
-	/* Empty or NULL string gets treated as "." */
-	if (path == NULL || *path == '\0') {
-		(void)strcpy(bname, ".");
-		return(bname);
-	}
-
-	/* Strip trailing slashes */
-	endp = path + strlen(path) - 1;
-	while (endp > path && *endp == '/')
-		endp--;
-
-	/* All slashes becomes "/" */
-	if (endp == path && *endp == '/') {
-		(void)strcpy(bname, "/");
-		return(bname);
-	}
-
-	/* Find the start of the base */
-	startp = endp;
-	while (startp > path && *(startp - 1) != '/')
-		startp--;
-
-	if (endp - startp + 2 > MAXPATHLEN) {
-		errno = ENAMETOOLONG;
-		return(NULL);
-	}
-	(void)strncpy(bname, startp, endp - startp + 1);
-	bname[endp - startp + 1] = '\0';
-	return(bname);
+  p = strrchr (filename, '/');
+  return p ? p + 1 : (char *) filename;
 }
