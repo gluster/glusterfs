493ef71222e780cb542c7edc5dfb7d0e3097c55a
Anand Avati <avati@redhat.com>
2012-05-29 22:30:53 -0700

    protocol: do not log getxattr/ENODATA as warning
    When SELinux is enabled, most of the files do not have labels and
    result is a ton of unnecessary logs
    
    Change-Id: I0e781e2fb6bcfb3fb12298175a41f7b981af9c39
    BUG: 811217
    Signed-off-by: Anand Avati <avati@redhat.com>
    Reviewed-on: http://review.gluster.com/3487
    Tested-by: Gluster Build System <jenkins@build.gluster.com>

--- xlators/protocol/client/src/client3_1-fops.c
+++ xlators/protocol/client/src/client3_1-fops.c
@@ -1049,7 +1049,7 @@ client3_1_getxattr_cbk (struct rpc_req *req, struct iovec *iov, int count,
 
 out:
         if (rsp.op_ret == -1) {
-                gf_log (this->name, ((op_errno == ENOTSUP) ?
+                gf_log (this->name, (((op_errno == ENOTSUP) || (op_errno == ENODATA)) ?
                                      GF_LOG_DEBUG : GF_LOG_WARNING),
                         "remote operation failed: %s. Path: %s (%s). Key: %s",
                         strerror (op_errno),
