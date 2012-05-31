05ad7e460c9f7ab7759313dfcd07ab37f9acd2b2
Mohammed Junaid <junaid@redhat.com>
2012-05-31 22:24:25 +0530

    swift: Updated the UFO admin guide for 3.3.
    Also, added a script to install the new rpm's.
    
    Change-Id: I66ba72822046d5930d021073a0cd918aafda6c24
    BUG: 811430
    Signed-off-by: Mohammed Junaid <junaid@redhat.com>
    Reviewed-on: http://review.gluster.com/3498
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Vijay Bellur <vijay@gluster.com>

---
+++ extras/rhel_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rpm -ivh http://download.gluster.com/pub/gluster/glusterfs/3.3/3.3.0/UFO/gluster-swift-1.4.8-4.el6.noarch.rpm
+rpm -ivh http://download.gluster.com/pub/gluster/glusterfs/3.3/3.3.0/UFO/gluster-swift-account-1.4.8-4.el6.noarch.rpm
+rpm -ivh http://download.gluster.com/pub/gluster/glusterfs/3.3/3.3.0/UFO/gluster-swift-container-1.4.8-4.el6.noarch.rpm
+rpm -ivh http://download.gluster.com/pub/gluster/glusterfs/3.3/3.3.0/UFO/gluster-swift-object-1.4.8-4.el6.noarch.rpm
+rpm -ivh http://download.gluster.com/pub/gluster/glusterfs/3.3/3.3.0/UFO/gluster-swift-proxy-1.4.8-4.el6.noarch.rpm
+rpm -ivh http://download.gluster.com/pub/gluster/glusterfs/3.3/3.3.0/UFO/gluster-swift-plugin-1.0-2.noarch.rpm
+rpm -ivh http://download.gluster.com/pub/gluster/glusterfs/3.3/3.3.0/UFO/gluster-swift-doc-1.4.8-4.el6.noarch.rpm
