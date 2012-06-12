85fdcbbcecf8f90aa4beae8a7267cea8e0212a9e
Raghavendra Bhat <raghavendra@redhat.com>
2012-06-12 19:17:41 +0530

    glusterd: copy /etc/glusterd to /var/lib and remove /etc/glusterd instead of moving
    * Change default directory from /etc/glusterd to /var/lib/glusterd
    
    Change-Id: Iefa82e4fdb94ce5a84dfb40855c80e16e9ffcc40
    BUG: 830218
    Signed-off-by: Raghavendra Bhat <raghavendra@redhat.com>
    Reviewed-on: http://review.gluster.com/3548
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Kaleb KEITHLEY <kkeithle@redhat.com>
    Reviewed-by: Anand Avati <avati@redhat.com>

--- xlators/mgmt/glusterd/src/glusterd.h
+++ xlators/mgmt/glusterd/src/glusterd.h
@@ -282,7 +282,7 @@ enum glusterd_vol_comp_status_ {
         GLUSTERD_VOL_COMP_RJT,
 };
 
-#define GLUSTERD_DEFAULT_WORKDIR "/etc/glusterd"
+#define GLUSTERD_DEFAULT_WORKDIR "/var/lib/glusterd"
 #define GLUSTERD_DEFAULT_PORT    GF_DEFAULT_BASE_PORT
 #define GLUSTERD_INFO_FILE      "glusterd.info"
 #define GLUSTERD_VOLUME_DIR_PREFIX "vols"
