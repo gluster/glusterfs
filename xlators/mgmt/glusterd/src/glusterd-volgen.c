96d87b1813d0fe80d7cff198e1b82a6695af159f
Krishnan Parthasarathi <kparthas@redhat.com>
2012-09-02 15:22:34 +0530

    storage/posix: Option to set brick(of a volume)'s root dir's uid/gid
    Change-Id: I529d4cd949477a436a5b571b69da9f1c8b33ee8f
    BUG: 858469
    Reviewed-on: https://code.engineering.redhat.com/gerrit/1863
    Reviewed-by: Vijay Bellur <vbellur@redhat.com>
    Tested-by: Vijay Bellur <vbellur@redhat.com>

--- xlators/mgmt/glusterd/src/glusterd-volgen.c
+++ xlators/mgmt/glusterd/src/glusterd-volgen.c
@@ -232,6 +232,8 @@ static struct volopt_map_entry glusterd_volopt_map[] = {
         {GLUSTERD_QUORUM_TYPE_KEY,               "mgmt/glusterd",             NULL,         "off", DOC, 0},
         {GLUSTERD_QUORUM_RATIO_KEY,              "mgmt/glusterd",             NULL,         "0", DOC, 0},
         {"storage.linux-aio",                    "storage/posix",             NULL, NULL, DOC, 0},
+        {"storage.owner-uid",                    "storage/posix",             "brick-uid", NULL, DOC, 0},
+        {"storage.owner-gid",                    "storage/posix",             "brick-gid", NULL, DOC, 0},
         {NULL,                                                                }
 };
 
