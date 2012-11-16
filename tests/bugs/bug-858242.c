088050b257ac9cbd2115587b0f7306760c44a4a3
Raghavendra G <raghavendra@gluster.com>
2012-11-16 12:06:36 +0530

    performance/io-cache: propagate errors while unwinding frame in                       read path.
    Change-Id: Ieb5d592a987e8681d5ec019da309f75e3b207580
    BUG: 858242
    Signed-off-by: Raghavendra G <raghavendra@gluster.com>
    Reviewed-on: http://review.gluster.org/4204
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Anand Avati <avati@redhat.com>

---
+++ tests/bugs/bug-858242.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int
+main (int argc, char *argv[])
+{
+        char        *filename = NULL, *volname = NULL, *cmd = NULL;
+        char  buffer[1024]    = {0, };
+        int          fd       = -1;
+        int          ret      = -1;
+        struct stat  statbuf  = {0, };
+
+        if (argc != 3) {
+                fprintf (stderr, "usage: %s <file-name> <volname>\n", argv[0]);
+                goto out;
+        }
+
+        filename = argv[1];
+        volname = argv[2];
+
+        fd = open (filename, O_RDWR | O_CREAT, 0);
+        if (fd < 0) {
+                fprintf (stderr, "open (%s) failed (%s)\n", filename,
+                         strerror (errno));
+                goto out;
+        }
+
+        ret = write (fd, "test-content", 12);
+        if (ret < 0) {
+                fprintf (stderr, "write failed (%s)", strerror (errno));
+                goto out;
+        }
+
+        ret = fsync (fd);
+        if (ret < 0) {
+                fprintf (stderr, "fsync failed (%s)", strerror (errno));
+                goto out;
+        }
+
+        ret = fstat (fd, &statbuf);
+        if (ret < 0) {
+                fprintf (stderr, "fstat failed (%s)", strerror (errno));
+                goto out;
+        }
+
+        ret = asprintf (&cmd, "gluster --mode=script volume stop %s force",
+                        volname);
+        if (ret < 0) {
+                fprintf (stderr, "cannot construct cli command string (%s)",
+                         strerror (errno));
+                goto out;
+        }
+
+        ret = system (cmd);
+        if (ret < 0) {
+                fprintf (stderr, "stopping volume (%s) failed", volname);
+                goto out;
+        }
+
+        ret = read (fd, buffer, 1024);
+        if (ret >= 0) {
+                fprintf (stderr, "read should've returned error, "
+                         "but is successful\n");
+                ret = -1;
+                goto out;
+        }
+
+        ret = 0;
+out:
+        return ret;
+}
