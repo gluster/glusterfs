13d44d000398fb4532bf5ed1ad7bb090e08e4813
Krishnan Parthasarathi <kparthas@redhat.com>
2013-03-01 16:18:26 +0530

    template: A minimal example xlator to build on
    This xlator is intended to save new GlusterFS developers
    from fighting it with autotools.
    
    How to add template xl onto the xlator graph?
    [..]
    
    volname-template
    type testing/features/template
    subvolumes volname-dht
    end-volume
    
    [..]
    
    Change-Id: If513284bb590be7c3b093054108dba2883064190
    BUG: 916985
    Signed-off-by: Krishnan Parthasarathi <kparthas@redhat.com>
    Reviewed-on: http://review.gluster.org/4604
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Vijay Bellur <vbellur@redhat.com>

---
+++ xlators/playground/template/src/template.h
@@ -0,0 +1,24 @@
+/*
+   Copyright (c) 2013 Red Hat, Inc. <http://www.redhat.com>
+   This file is part of GlusterFS.
+
+   This file is licensed to you under your choice of the GNU Lesser
+   General Public License, version 3 or any later version (LGPLv3 or
+   later), or the GNU General Public License, version 2 (GPLv2), in all
+   cases as published by the Free Software Foundation.
+*/
+#ifndef __TEMPLATE_H__
+#define __TEMPLATE_H__
+
+#ifndef _CONFIG_H
+#define _CONFIG_H
+#include "config.h"
+#endif
+
+#include "glusterfs.h"
+#include "logging.h"
+#include "dict.h"
+#include "xlator.h"
+#include "defaults.h"
+
+#endif /* __TEMPLATE_H__ */
