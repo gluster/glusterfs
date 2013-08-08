fdb3fb9a9eacc2bbb5de3d2a938564caafa2a9ba
Pranith Kumar K <pkarampu@redhat.com>
2013-08-08 15:22:10 +0530

    cluster/afr: Unwind frame on error in readdir[p]
    Change-Id: I5701bf115e0aa1adb4fb52f5418534910a2268d4
    BUG: 994959
    Signed-off-by: Pranith Kumar K <pkarampu@redhat.com>
    Reviewed-on: http://review.gluster.org/5531
    Reviewed-by: Vijay Bellur <vbellur@redhat.com>
    Tested-by: Gluster Build System <jenkins@build.gluster.com>

--- xlators/cluster/afr/src/afr-dir-read.c
+++ xlators/cluster/afr/src/afr-dir-read.c
@@ -410,9 +410,9 @@ afr_readdir_cbk (call_frame_t *frame, void *cookie,
 
         local = frame->local;
         afr_readdir_filter_trash_dir (entries, local->fd);
-        AFR_STACK_UNWIND (readdir, frame, op_ret, op_errno, entries, NULL);
 
 out:
+        AFR_STACK_UNWIND (readdir, frame, op_ret, op_errno, entries, NULL);
         return 0;
 }
 
@@ -429,9 +429,9 @@ afr_readdirp_cbk (call_frame_t *frame, void *cookie, xlator_t *this,
 
         local = frame->local;
         afr_readdir_filter_trash_dir (entries, local->fd);
-        AFR_STACK_UNWIND (readdirp, frame, op_ret, op_errno, entries, NULL);
 
 out:
+        AFR_STACK_UNWIND (readdirp, frame, op_ret, op_errno, entries, NULL);
         return 0;
 }
 
