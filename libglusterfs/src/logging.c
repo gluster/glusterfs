317f2656225905794a26c454e8c22f30166107f2
Bharata B Rao <bharata@linux.vnet.ibm.com>
2013-08-16 11:39:01 +0530

    log: Fix logging to stderr
    This fix enables gluster clients like QEMU using glfs_set_logging()
    with "-" as logfile to see client side errors on stderr.
    
    Change-Id: I35f7b65460d1c03f910ceb3bad0a65677317e702
    Signed-off-by: Bharata B Rao <bharata@linux.vnet.ibm.com>
    Reviewed-on: http://review.gluster.org/5637
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Anand Avati <avati@redhat.com>

--- libglusterfs/src/logging.c
+++ libglusterfs/src/logging.c
@@ -374,7 +374,7 @@ gf_log_init (void *data, const char *file, const char *ident)
 
         if (strcmp (file, "-") == 0) {
                 ctx->log.gf_log_logfile = stderr;
-
+                ctx->log.logfile = stderr;
                 return 0;
         }
 
