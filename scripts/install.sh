0b1d33356a4c97aa897018834f1212246e758c58
root <root@dhcp-0-88.blr.redhat.com>
2013-09-02 10:57:28 +0530

    minor changed to install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,32 +4,11 @@ cd ..
 sh autogen.sh
 echo "./autogen.sh completed..."
 
+sh configure --enable-debug
+echo "./configure completed"
 
-echo "Do you want to configure in debugging mode (y/n) ? "
-read debug
-if [ $debug == 'y' ]
-then
-        sh configure --enable-debug
-elif [ $debug == 'n' ]
-then
-        sh configure
-else
-        echo "wrong choice"  
-     #better loop this set of statements in case wrong choice is entered.
-fi
-
-
-echo "Do you want to run 'make' without optimization (y/n) ? "
-read opti
-if [ $opti == 'y' ]
-then
-        make -j4 CFLAGS="-ggdb -O0"
-elif [ $opti == 'n' ]
-then
-        make
-else
-        echo "wrong choice"
-fi
+make -j4 CFLAGS="-ggdb -O0"
+echo "make completed"
 
 sudo make install
-                 
+echo "installing make"                 
