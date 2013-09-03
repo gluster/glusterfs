ca9938e815939e79db878eb7644c2b9db43fb14a
root <root@dhcp-0-88.blr.redhat.com>
2013-09-03 10:06:06 +0530

    modified

--- scripts/send_mail.sh
+++ scripts/send_mail.sh
@@ -3,8 +3,9 @@ get_status=`tail -5 make_output.txt`
 echo $get_status > status.txt
 if grep -q 'ERROR\|error\|Error' status.txt
 then
-	echo "The build has been successful" | mail -s "Build Result" sachinspandits@gmail.com
+	echo "Build not successful"
 else
 	echo "Successful"
-	echo "The buils has been successful" | mail -s "Buuld Result" sachinspandits@gmail.com 
+	echo "The build has been successful" | mail -s "Build Result" sachinspandits@gmail.com 
+	echo "Mail has been sent to sachinspandits@gmail.com"
 fi
