39968c09626074b34b62541af5940f44ba70cc06
Varun Shastry <vshastry@redhat.com>
2013-10-22 16:12:58 +0530

    features/quota: Metadata cleanup
    Quota and marker uses 'trusted.glusterfs.quota*' and 'trusted.pgfid*' xattrs to
    store its configurations and accounting information and also to build the
    parent inode chain in case of absense of path.
    
    Problem:
    After disabling and then enabling quota back, the xattrs may contain stale data
    leading to impaired accounting and thus improper enforcement.
    
    Solution:
    Clean up all the quota related xattrs after quota disable.
    
    Marker xlator implements a virtual xattr to cleanup quota and pgfid xattrs. In
    this approach glusterd mounts an auxiliary mount and sends the below command to
    all the files by crawling the mountpoint.
    
     #setfattr -n "glusterfs.quota-xattr-cleanup" -v 1 <path/to/file>
    
    Credit:
    Krishnan Parthasarathi <kparthas@redhat.com>
    Varun Shastry          <vshastry@redhat.com>
    Change-Id: I9380eca58a285dc27dd572de1767aac8f2cd8049
    BUG: 969461
    Signed-off-by: Varun Shastry <vshastry@redhat.com>
    Reviewed-on: http://review.gluster.org/6369
    Reviewed-by: Raghavendra G <rgowdapp@redhat.com>
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Krishnan Parthasarathi <kparthas@redhat.com>
    Reviewed-by: Vijay Bellur <vbellur@redhat.com>

--- libglusterfs/src/glusterfs.h
+++ libglusterfs/src/glusterfs.h
@@ -87,6 +87,7 @@
 #define GF_XATTR_GET_REAL_FILENAME_KEY "glusterfs.get_real_filename:"
 #define GF_XATTR_USER_PATHINFO_KEY   "glusterfs.pathinfo"
 #define QUOTA_LIMIT_KEY "trusted.glusterfs.quota.limit-set"
+#define VIRTUAL_QUOTA_XATTR_CLEANUP_KEY "glusterfs.quota-xattr-cleanup"
 
 #define GF_READDIR_SKIP_DIRS       "readdir-filter-directories"
 
