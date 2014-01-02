859669759f7fa0f2114add13660ce3bf16c77f30
Brian Foster <bfoster@redhat.com>
2014-01-02 14:03:18 -0500

    extras/hook-scripts: SELinux brick file context management scripts
    The SELinux policy for gluster defines the glusterd_brick_t type to
    support server side SELinux (e.g., server side labels). Add
    convenience hook scripts that users/packagers can install to ensure
    that new bricks are labeled correctly.
    
    The volume create hook script adds a new SELinux file context for
    each brick path and runs a restorecon to label the brick. The
    volume delete hook removes the per-brick SELinux file context.
    
    Change-Id: I5f102db5382d813c4d822ff74e873a7a669b41db
    BUG: 1047975
    Signed-off-by: Brian Foster <bfoster@redhat.com>
    Signed-off-by: Niels de Vos <ndevos@redhat.com>
    Signed-off-by: Jiffin Tony Thottan <jthottan@redhat.com>
    Reviewed-on: https://review.gluster.org/6630
    Smoke: Gluster Build System <jenkins@build.gluster.org>
    NetBSD-regression: NetBSD Build System <jenkins@build.gluster.org>
    CentOS-regression: Gluster Build System <jenkins@build.gluster.org>
    Reviewed-by: Kaleb KEITHLEY <kkeithle@redhat.com>

---
+++ extras/hook-scripts/create/post/S10selinux-label-brick.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#
+# Install to hooks/<HOOKS_VER>/create/post
+#
+# Add an SELinux file context for each brick using the glusterd_brick_t type.
+# This ensures that the brick is relabeled correctly on an SELinux restart or
+# restore. Subsequently, run a restore on the brick path to set the selinux
+# labels.
+#
+###
+
+PROGNAME="Sselinux"
+OPTSPEC="volname:"
+VOL=
+
+function parse_args () {
+        ARGS=$(getopt -l $OPTSPEC  -name $PROGNAME $@)
+        eval set -- "$ARGS"
+
+        while true; do
+        case $1 in
+        --volname)
+         shift
+         VOL=$1
+         ;;
+        *)
+         shift
+         break
+         ;;
+        esac
+        shift
+        done
+}
+
+function set_brick_labels()
+{
+        volname=$1
+
+        # grab the path for each local brick
+        brickdirs=$(grep '^path=' /var/lib/glusterd/vols/${volname}/bricks/* | cut -d= -f 2)
+
+        for b in $brickdirs
+        do
+                # Add a file context for each brick path and associate with the
+                # glusterd_brick_t SELinux type.
+                semanage fcontext --add -t glusterd_brick_t -r s0 $b(/.*)?
+
+                # Set the labels on the new brick path.
+                restorecon -R $b
+        done
+}
+
+SELINUX_STATE=$(which getenforce && getenforce)
+[ "${SELINUX_STATE}" = 'Disabled' ] && exit 0
+
+parse_args $@
+[ -z "$VOL" ] && exit 1
+
+set_brick_labels $VOL
+
+exit 0
