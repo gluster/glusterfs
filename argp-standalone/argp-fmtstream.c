b2dfa011a3fdcb7d22764d143517d0fbd1c2a201
Emmanuel Dreyfus <manu@netbsd.org>
2014-01-22 14:47:23 +0100

    Fix build with c99 compilers
    BUG: 764655
    Change-Id: If5dfdc9c7427bd3d39d8da8f79e33ae2da6a3137
    Signed-off-by: Emmanuel Dreyfus <manu@netbsd.org>
    Reviewed-on: http://review.gluster.org/6034
    Reviewed-by: Harshavardhana <harsha@harshavardhana.net>
    Tested-by: Gluster Build System <jenkins@build.gluster.com>

--- argp-standalone/argp-fmtstream.c
+++ argp-standalone/argp-fmtstream.c
@@ -389,6 +389,7 @@ __argp_fmtstream_printf (struct argp_fmtstream *fs, const char *fmt, ...)
 weak_alias (__argp_fmtstream_printf, argp_fmtstream_printf)
 #endif
 
+#if __STDC_VERSION__ - 199900L < 1
 /* Duplicate the inline definitions in argp-fmtstream.h, for compilers
  * that don't do inlining. */
 size_t
@@ -471,5 +472,6 @@ __argp_fmtstream_point (argp_fmtstream_t __fs)
     __argp_fmtstream_update (__fs);
   return __fs->point_col >= 0 ? __fs->point_col : 0;
 }
+#endif /* __STDC_VERSION__ - 199900L < 1 */
 
 #endif /* !ARGP_FMTSTREAM_USE_LINEWRAP */
--- argp-standalone/argp-fmtstream.h
+++ argp-standalone/argp-fmtstream.h
@@ -153,6 +153,7 @@ extern ssize_t argp_fmtstream_printf (argp_fmtstream_t __fs,
 				      __const char *__fmt, ...)
      PRINTF_STYLE(2,3);
 
+#if __STDC_VERSION__ - 199900L < 1
 extern int __argp_fmtstream_putc (argp_fmtstream_t __fs, int __ch);
 extern int argp_fmtstream_putc (argp_fmtstream_t __fs, int __ch);
 
@@ -163,6 +164,7 @@ extern size_t __argp_fmtstream_write (argp_fmtstream_t __fs,
 				      __const char *__str, size_t __len);
 extern size_t argp_fmtstream_write (argp_fmtstream_t __fs,
 				    __const char *__str, size_t __len);
+#endif /* __STDC_VERSION__ - 199900L < 1 */
 
 /* Access macros for various bits of state.  */
 #define argp_fmtstream_lmargin(__fs) ((__fs)->lmargin)
@@ -172,6 +174,7 @@ extern size_t argp_fmtstream_write (argp_fmtstream_t __fs,
 #define __argp_fmtstream_rmargin argp_fmtstream_rmargin
 #define __argp_fmtstream_wmargin argp_fmtstream_wmargin
 
+#if __STDC_VERSION__ - 199900L < 1
 /* Set __FS's left margin to LMARGIN and return the old value.  */
 extern size_t argp_fmtstream_set_lmargin (argp_fmtstream_t __fs,
 					  size_t __lmargin);
@@ -193,6 +196,7 @@ extern size_t __argp_fmtstream_set_wmargin (argp_fmtstream_t __fs,
 /* Return the column number of the current output point in __FS.  */
 extern size_t argp_fmtstream_point (argp_fmtstream_t __fs);
 extern size_t __argp_fmtstream_point (argp_fmtstream_t __fs);
+#endif /* __STDC_VERSION__ - 199900L < 1 */
 
 /* Internal routines.  */
 extern void _argp_fmtstream_update (argp_fmtstream_t __fs);
@@ -216,7 +220,11 @@ extern int __argp_fmtstream_ensure (argp_fmtstream_t __fs, size_t __amount);
 #endif
 
 #ifndef ARGP_FS_EI
+#if defined(__GNUC__) && !defined(__GNUC_STDC_INLINE__)
 #define ARGP_FS_EI extern inline
+#else
+#define ARGP_FS_EI inline
+#endif
 #endif
 
 ARGP_FS_EI size_t
