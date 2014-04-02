7734c71e4d683b8fdd08cbc84b2ddcc12f967ba7
Krishnan Parthasarathi <kparthas@redhat.com>
2014-04-02 11:36:20 +0530

    dict: add dict_set_dynstr_with_alloc
    There is an overwhelming no. of instances of the following pattern in
    glusterd module.
    
        ...
    
        char *dynstr = gf_strdup (str);
        if (!dynstr)
           goto err;
        ret = dict_set_dynstr (dict, key, dynstr);
        if (ret)
           goto err;
    
        ...
    
    With this changes it would look as below,
    
       ret = dict_set_dynstr_with_alloc (dict, key, str);
       if (ret)
           goto err;
    
    Change-Id: I6a47b1cbab4834badadc48c56d0b5c8c06c6dd4d
    Signed-off-by: Krishnan Parthasarathi <kparthas@redhat.com>
    Reviewed-on: http://review.gluster.org/7379
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Jeff Darcy <jdarcy@redhat.com>

--- libglusterfs/src/dict.h
+++ libglusterfs/src/dict.h
@@ -228,6 +228,7 @@ GF_MUST_CHECK int dict_set_static_bin (dict_t *this, char *key, void *ptr, size_
 GF_MUST_CHECK int dict_set_str (dict_t *this, char *key, char *str);
 GF_MUST_CHECK int dict_set_dynmstr (dict_t *this, char *key, char *str);
 GF_MUST_CHECK int dict_set_dynstr (dict_t *this, char *key, char *str);
+GF_MUST_CHECK int dict_set_dynstr_with_alloc (dict_t *this, char *key, const char *str);
 GF_MUST_CHECK int dict_get_str (dict_t *this, char *key, char **str);
 
 GF_MUST_CHECK int dict_get_str_boolean (dict_t *this, char *key, int default_val);
