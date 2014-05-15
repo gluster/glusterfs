0147f7048a2bbef7e4ba6c87b31d88a4888c5154
Kotresh H R <khiremat@redhat.com>
2014-05-15 12:52:22 +0530

    gsyncd/geo-rep: Fix remote vol info fetching for non-root
    Signed-off-by: Kotresh H R <khiremat@redhat.com>
    
    Change-Id: If1d2cab3fcfe2391105551e54f0b9729a7c204e4
    BUG: 1077452
    Reviewed-on: http://review.gluster.org/7767
    Reviewed-by: Venky Shankar <vshankar@redhat.com>
    Tested-by: Venky Shankar <vshankar@redhat.com>

--- geo-replication/syncdaemon/monitor.py
+++ geo-replication/syncdaemon/monitor.py
@@ -267,7 +267,7 @@ def distribute(*resources):
         sbricks = {'host': 'localhost', 'dir': si.path}
         suuid = uuid.uuid5(uuid.NAMESPACE_URL, slave.get_url(canonical=True))
     elif isinstance(si, GLUSTER):
-        svol = Volinfo(si.volume, si.host)
+        svol = Volinfo(si.volume, slave.remote_addr)
         sbricks = svol.bricks
         suuid = svol.uuid
     else:
