e311014d94c7fcfd8c5e7abe3aca7d99f43e0d91
Kaleb S. KEITHLEY <kkeithle@redhat.com>
2014-07-08 08:25:36 -0400

    nfs: prevent assertion error with MOUNT over UDP
    see http://review.gluster.org/#/c/8258/, http://review.gluster.org/8241;
    and BZs 1116997, 1116503, respectively.
    
    The MOUNT service over UDP runs in a separate thread. This thread does
    not have the correct *THIS xlator set. *THIS points to the global (base)
    xlator structure, but GF_CALLOC() requires it to be the NFS-xlator so
    that assertions can get validated correctly.
    
    This is solved by passing the NFS-xlator to the pthread function, and
    setting the *THIS pointer explicitly in the new thread.
    
    It seems that on occasion (needs further investigation) MOUNT over UDP
    does not unregister itself. There can also be issues when the kernel NLM
    implementation has been registered at portmap/rpcbind, so adding some
    unregister procedures in the cleanup of the test-cases.
    
    Change-Id: Iea3bafd65bf480f2b12da91875c73571594d52d3
    BUG: 1117256
    Signed-off-by: Kaleb S. KEITHLEY <kkeithle@redhat.com>
    Reviewed-on: http://review.gluster.org/8262
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Niels de Vos <ndevos@redhat.com>
    Reviewed-by: Santosh Pradhan <spradhan@redhat.com>

--- xlators/nfs/server/src/mount3.c
+++ xlators/nfs/server/src/mount3.c
@@ -1922,7 +1922,7 @@ mnt3svc_init (xlator_t *nfsx)
         }
 
         if (nfs->mount_udp) {
-                pthread_create (&udp_thread, NULL, mount3udp_thread, NULL);
+                pthread_create (&udp_thread, NULL, mount3udp_thread, nfsx);
         }
         return &mnt3prog;
 err:
