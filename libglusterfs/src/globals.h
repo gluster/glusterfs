f86d3facd31655a2b51af5a6910fc9355d442622
Kaushal M <kaushal@redhat.com>
2014-07-23 12:47:33 +0530

    Bump op-version for 3.7.0
    Change-Id: I4542edeca140bc2252d765b5cfc2e24d1d90cdb1
    BUG: 1122398
    Reviewed-on: http://review.gluster.org/8354
    Reviewed-by: Atin Mukherjee <amukherj@redhat.com>
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Kaushal M <kaushal@redhat.com>

--- libglusterfs/src/globals.h
+++ libglusterfs/src/globals.h
@@ -24,8 +24,7 @@
  * 3.5.0                - 3
  * 3.5.1                - 30501
  * 3.6.0                - 30600
- *
- * TODO: Change above comment once gluster version is finalised
+ * 3.7.0                - 30700
  *
  * Starting with Gluster v3.6, the op-version will be multi-digit integer values
  * based on the Glusterfs version, instead of a simply incrementing integer
@@ -41,6 +40,8 @@
                                     introduction of newer versions */
 
 #define GD_OP_VERSION_3_6_0    30600 /* Op-Version for GlusterFS 3.6.0 */
+
+#define GD_OP_VERSION_3_7_0    30700 /* Op-version for GlusterFS 3.7.0 */
 
 #define GD_OP_VER_PERSISTENT_AFR_XATTRS GD_OP_VERSION_3_6_0
 
