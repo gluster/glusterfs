6fa42a74bd2dd8b0c7e7c6317417d29525c8022b
Vijaikumar M <vmallika@redhat.com>
2014-07-31 14:37:19 +0530

    glusterd/snapshot: Inherit the mount options of a original brick when creating snapshots.
    When creating a snapshot a LVM is created at the backend and is mounted
    under /var/run/gluster/snaps/... However, this mount does not inherit
    the mount options for the original brick acting as the parent for the
    snap.
    
    If the snap is restored, this could lead to performance degredations,
    functional limitations, or in extreme scenarios even potential data
    loss.
    
    Change-Id: I67d70fd83430d83dacc5380c6c928e27fb9c9e1b
    BUG: 1145088
    Signed-off-by: Vijaikumar M <vmallika@redhat.com>
    Reviewed-on: http://review.gluster.org/8394
    Reviewed-by: Krishnan Parthasarathi <kparthas@redhat.com>
    Tested-by: Krishnan Parthasarathi <kparthas@redhat.com>
    Signed-off-by: Sachin Pandit <spandit@redhat.com>
    Reviewed-on: http://review.gluster.org/8802
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Vijay Bellur <vbellur@redhat.com>

--- xlators/mgmt/glusterd/src/glusterd.h
+++ xlators/mgmt/glusterd/src/glusterd.h
@@ -193,6 +193,7 @@ struct glusterd_brickinfo {
         char               mount_dir[PATH_MAX];
         char               brick_id[1024];/*Client xlator name, AFR changelog name*/
         char               fstype [NAME_MAX]; /* Brick file-system type */
+        char               mnt_opts [1024]; /* Brick mount options */
         struct list_head   brick_list;
         uuid_t             uuid;
         int                port;
