26be0b3a9f334f33f1a6e53706045eb02983d713
Xavier Hernandez <xhernandez@datalab.es>
2014-08-04 20:50:31 +0200

    ec: Removed SSE2 dependency
    This patch implements the Galois Field multiplications using pure C
    code without any assembler support. This makes the ec xlator portable
    to other architectures.
    
    In the future it will be possible to use an optimized implementation
    of the multiplications using architecture dependent facilities (it
    will be automatically detected and configured). To allow bricks with
    different machine word sizes to be able to work seamlessly in the
    same volume, the minimum fragment length to be stored in any brick
    has been fixed to 512 bytes. Otherwise, different implementations
    will corrupt the data (SSE2 used 128 bytes, while new implementation
    would have used 64).
    
    This patch also removes the '-msse2' option added on patch
    http://review.gluster.org/8395/
    
    Change-Id: Iaf6e4ef3dcfda6c68f48f16ca46fc4fb61a215f4
    BUG: 1125166
    Signed-off-by: Xavier Hernandez <xhernandez@datalab.es>
    Reviewed-on: http://review.gluster.org/8413
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Dan Lambright <dlambrig@redhat.com>

--- xlators/cluster/ec/src/ec-method.h
+++ xlators/cluster/ec/src/ec-method.h
@@ -25,13 +25,10 @@
 
 #define EC_METHOD_MAX_FRAGMENTS 16
 
-#define EC_METHOD_WORD_SIZE 16
+#define EC_METHOD_WORD_SIZE 64
 
-#define EC_BUFFER_ALIGN_SIZE EC_METHOD_WORD_SIZE
-
-#define EC_METHOD_BITS EC_GF_BITS
-#define EC_METHOD_SIZE (1 << (EC_METHOD_BITS))
-#define EC_METHOD_CHUNK_SIZE (EC_METHOD_WORD_SIZE * EC_METHOD_BITS)
+#define EC_METHOD_CHUNK_SIZE (EC_METHOD_WORD_SIZE * EC_GF_BITS)
+#define EC_METHOD_WIDTH (EC_METHOD_WORD_SIZE / EC_GF_WORD_SIZE)
 
 void ec_method_initialize(void);
 size_t ec_method_encode(size_t size, uint32_t columns, uint32_t row,
