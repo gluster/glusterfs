gluster/glusterfs

commit 1f6e992f1aaa676be5bd47d17e58f1171825cf43

Do not hardcode setfattr(1) path
Turn setfattr(1) absolute path into an OS-dependant macro. Let compiler
option override it to fit custom installation if needed.

Backport of I8f469c5741a85b6e8d8f6299a9540b3d64611d2f

BUG: 1138897
Change-Id: I279752f2ec5db1abc25830cb9a23290cc401d517
Signed-off-by: Emmanuel Dreyfus <manu@netbsd.org>
Reviewed-on: http://review.gluster.org/8828
Tested-by: Gluster Build System <jenkins@build.gluster.com>
Reviewed-by: Vijay Bellur <vbellur@redhat.com>

xlators/mgmt/glusterd/src/glusterd-quota.c
@@ -28,6 +28,18 @@
 #include <sys/wait.h>
 #include <dlfcn.h>
 
+#ifndef _PATH_SETFATTR
+# ifdef GF_LINUX_HOST_OS
+#  define _PATH_SETFATTR "/usr/bin/setfattr"
+# endif
+# ifdef __NetBSD__
+#  define _PATH_SETFATTR "/usr/pkg/bin/setfattr"
+# endif
+# ifdef __FreeBSD__
+#  define _PATH_SETFATTR "/usr/local/bin/setfattr"
+# endif
+#endif
+
 /* Any negative pid to make it special client */
 #define QUOTA_CRAWL_PID "-100"
 
@@ -223,7 +235,7 @@ glusterd_quota_initiate_fs_crawl (glusterd_conf_t *priv, char *volname,
                 else if (type == GF_QUOTA_OPTION_TYPE_DISABLE)
 
                         runner_add_args (&runner, "/usr/bin/find", ".",
-                                         "-exec", "/usr/bin/setfattr", "-n",
+                                         "-exec", _PATH_SETFATTR, "-n",
                                          VIRTUAL_QUOTA_XATTR_CLEANUP_KEY, "-v",
                                          "1", "{}", "\\", ";", NULL);
 

commit 645bcf919dea96d3730bc5ce806197f13cf81ef0

OSX/FreeBSD: Regression fix
Introduced in "1f6e992f1aaa676be5bd47d17e58f1171825cf43"

Change-Id: I655cf613ca93a749ab5403cb3ec038e739993e2e
BUG: 1146279
Signed-off-by: Harshavardhana <harsha@harshavardhana.net>
Reviewed-on: http://review.gluster.org/8841
Reviewed-by: Justin Clift <justin@gluster.org>
Tested-by: Justin Clift <justin@gluster.org>

xlators/mgmt/glusterd/src/glusterd-quota.c
@@ -35,9 +35,6 @@
 # ifdef __NetBSD__
 #  define _PATH_SETFATTR "/usr/pkg/bin/setfattr"
 # endif
-# ifdef __FreeBSD__
-#  define _PATH_SETFATTR "/usr/local/bin/setfattr"
-# endif
 #endif
 
 /* Any negative pid to make it special client */
@@ -232,12 +229,27 @@ glusterd_quota_initiate_fs_crawl (glusterd_conf_t *priv, char *volname,
                         runner_add_args (&runner, "/usr/bin/find", "find", ".",
                                          NULL);
 
-                else if (type == GF_QUOTA_OPTION_TYPE_DISABLE)
+                else if (type == GF_QUOTA_OPTION_TYPE_DISABLE) {
 
+#if defined(GF_DARWIN_HOST_OS)
+                        runner_add_args (&runner, "/usr/bin/find", ".",
+                                         "-exec", "/usr/bin/xattr", "-w",
+                                         VIRTUAL_QUOTA_XATTR_CLEANUP_KEY, "1",
+                                         "{}", "\\", ";", NULL);
+#elif defined(__FreeBSD__)
+                        runner_add_args (&runner, "/usr/bin/find", ".",
+                                         "-exec", "/usr/sbin/setextattr",
+                                         EXTATTR_NAMESPACE_USER,
+                                         VIRTUAL_QUOTA_XATTR_CLEANUP_KEY, "1",
+                                         "{}", "\\", ";", NULL);
+#else
                         runner_add_args (&runner, "/usr/bin/find", ".",
                                          "-exec", _PATH_SETFATTR, "-n",
                                          VIRTUAL_QUOTA_XATTR_CLEANUP_KEY, "-v",
                                          "1", "{}", "\\", ";", NULL);
+#endif
+
+                }
 
                 if (runner_start (&runner) == -1)
                         _exit (EXIT_FAILURE);