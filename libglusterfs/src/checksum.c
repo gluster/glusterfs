c5140c2f1aa6a5b4dc2c7ef9ce4a89e9e4f14658
Niels de Vos <ndevos@redhat.com>
2014-11-02 19:15:49 +0100

    Replace copied (from rsync) checksum code by adler32() from zlib
    The weak checksum code that is included in libglusterfs has initialy
    been copied from the rsync sources. Instead of maintaining a copy of a
    function, we should use a function from a shared library. The algorithm
    seems to be Adler-32, zlib provides an implementation.
    
    The strong checksum function has already been replaced by MD5 from
    OpenSSL. It is time to also remove the comments about the origin of the
    implementation, because it is not correct anymore.
    
    Change-Id: I70c16ae1d1c36b458a035e4adb3e51a20afcf652
    BUG: 1149943
    Reported-by: Wade Mealing <wmealing@redhat.com>
    Signed-off-by: Niels de Vos <ndevos@redhat.com>
    Reviewed-on: http://review.gluster.org/9035
    Reviewed-by: Kaleb KEITHLEY <kkeithle@redhat.com>
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Vijay Bellur <vbellur@redhat.com>

--- libglusterfs/src/checksum.c
+++ libglusterfs/src/checksum.c
@@ -9,57 +9,28 @@
 */
 
 #include <openssl/md5.h>
+#include <zlib.h>
 #include <stdint.h>
 
-#include "glusterfs.h"
-
 /*
- * The "weak" checksum required for the rsync algorithm,
- * adapted from the rsync source code. The following comment
- * appears there:
- *
- * "a simple 32 bit checksum that can be upadted from either end
- *  (inspired by Mark Adler's Adler-32 checksum)"
+ * The "weak" checksum required for the rsync algorithm.
  *
  * Note: these functions are only called to compute checksums on
  * pathnames; they don't need to handle arbitrarily long strings of
  * data. Thus int32_t and uint32_t are sufficient
  */
-
 uint32_t
 gf_rsync_weak_checksum (unsigned char *buf, size_t len)
 {
-        int32_t i = 0;
-        uint32_t s1, s2;
-
-        uint32_t csum;
-
-        s1 = s2 = 0;
-        if (len >= 4) {
-                for (; i < (len-4); i+=4) {
-                        s2 += 4*(s1 + buf[i]) + 3*buf[i+1] + 2*buf[i+2] + buf[i+3];
-                        s1 += buf[i+0] + buf[i+1] + buf[i+2] + buf[i+3];
-                }
-        }
-
-        for (; i < len; i++) {
-                s1 += buf[i];
-                s2 += s1;
-        }
-
-        csum = (s1 & 0xffff) + (s2 << 16);
-
-        return csum;
+        return adler32 (0, buf, len);
 }
 
 
 /*
- * The "strong" checksum required for the rsync algorithm,
- * adapted from the rsync source code.
+ * The "strong" checksum required for the rsync algorithm.
  */
-
 void
 gf_rsync_strong_checksum (unsigned char *data, size_t len, unsigned char *md5)
 {
-        MD5(data, len, md5);
+        MD5 (data, len, md5);
 }
