22035d5e37db748cbdee2596d99006cea6b5282e
Xavier Hernandez <xhernandez@datalab.es>
2014-11-08 21:46:41 +0100

    ec: Fix posix compliance failures
    This patch solves some problems that caused dispersed volumes to not
    pass posix smoke tests:
    
    * Problems in open/create with O_WRONLY
        Opening files with -w- permissions using O_WRONLY returned an EACCES
        error because internally O_WRONLY was replaced with O_RDWR.
    
    * Problems with entrylk on renames.
        When source and destination were the same, ec tried to acquire
        the same entrylk twice, causing a deadlock.
    
    * Overwrite of a variable when reordering locks.
        On a rename, if the second lock needed to be placed at the beggining
        of the list, the 'lock' variable was overwritten and later its timer
        was cancelled, cancelling the incorrect one.
    
    * Handle O_TRUNC in open.
        When O_TRUNC was received in an open call, it was blindly propagated
        to child subvolumes. This caused a discrepancy between real file
        size and the size stored into trusted.ec.size xattr. This has been
        solved by removing O_TRUNC from open and later calling ftruncate.
    
    This is a backport of http://review.gluster.org/9420
    
    Change-Id: I20c3d6e1c11be314be86879be54b728e01013798
    BUG: 1159471
    Signed-off-by: Xavier Hernandez <xhernandez@datalab.es>
    Reviewed-on: http://review.gluster.org/9501
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Dan Lambright <dlambrig@redhat.com>
    Reviewed-by: Pranith Kumar Karampuri <pkarampu@redhat.com>
    Reviewed-by: Raghavendra Bhat <raghavendra@redhat.com>

---
+++ tests/bugs/bug-1161886/bug-1161886.c
@@ -0,0 +1,53 @@
+
+#include <stdio.h>
+#include "glfs.h"
+#include "glfs-handles.h"
+
+int
+main (int argc, char *argv[])
+{
+        glfs_t    *fs = NULL;
+        glfs_fd_t *fd = NULL;
+        int        ret = 1;
+
+        if (argc != 4) {
+                fprintf (stderr, "Syntax: %s <host> <volname> <file>\n", argv[0]);
+                return 1;
+        }
+
+        fs = glfs_new (argv[2]);
+        if (!fs) {
+                fprintf (stderr, "glfs_new: returned NULL\n");
+                return 1;
+        }
+
+        ret = glfs_set_volfile_server (fs, "tcp", argv[1], 24007);
+        if (ret != 0) {
+                fprintf (stderr, "glfs_set_volfile_server: retuned %d\n", ret);
+                goto out;
+        }
+        ret = glfs_set_logging (fs, "/dev/null", 7);
+        if (ret != 0) {
+                fprintf (stderr, "glfs_set_logging: returned %d\n", ret);
+                goto out;
+        }
+        ret = glfs_init (fs);
+        if (ret != 0) {
+                fprintf (stderr, "glfs_init: returned %d\n", ret);
+                goto out;
+        }
+
+        fd = glfs_open (fs, argv[3], O_RDWR | O_TRUNC);
+        if (fd == NULL) {
+                fprintf (stderr, "glfs_open: returned NULL\n");
+                goto out;
+        }
+        glfs_close(fd);
+
+        ret = 0;
+
+out:
+        glfs_fini (fs);
+
+        return ret;
+}
