87653bba6fc940adb4c95abd2cc205e67dcdfec6
Atin Mukherjee <amukherj@redhat.com>
2014-12-26 12:18:31 +0530

    glusterd: cluster quorum count check correction
    Due to the recent change introduced by commit
    da9deb54df91dedc51ebe165f3a0be646455cb5b cluster quorum count calucation now
    depends on whether the peer list is either all peers or global transaction peer
    list or the local transaction peer list.
    
    Change-Id: I9f63af9a0cb3cfd6369b050247d0ef3ac93d760f
    BUG: 1173414
    Signed-off-by: Atin Mukherjee <amukherj@redhat.com>
    Reviewed-on: http://review.gluster.org/9350
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Krishnan Parthasarathi <kparthas@redhat.com>
    Reviewed-by: Raghavendra Bhat <raghavendra@redhat.com>
    Reviewed-by: Avra Sengupta <asengupt@redhat.com>
    Reviewed-by: Kaushal M <kaushal@redhat.com>

--- xlators/mgmt/glusterd/src/glusterd-utils.h
+++ xlators/mgmt/glusterd/src/glusterd-utils.h
@@ -566,7 +566,8 @@ glusterd_do_quorum_action ();
 int
 glusterd_get_quorum_cluster_counts (xlator_t *this, int *active_count,
                                     int *quorum_count,
-                                    gf_boolean_t _xaction_peers);
+                                    struct list_head *peer_list,
+                                    gf_boolean_t _local__xaction_peers);
 
 int
 glusterd_get_next_global_opt_version_str (dict_t *opts, char **version_str);
@@ -577,7 +578,9 @@ glusterd_is_volume_in_server_quorum (glusterd_volinfo_t *volinfo);
 gf_boolean_t
 glusterd_is_any_volume_in_server_quorum (xlator_t *this);
 gf_boolean_t
-does_gd_meet_server_quorum (xlator_t *this, gf_boolean_t _xaction_peers);
+does_gd_meet_server_quorum (xlator_t *this,
+                            struct list_head *peers_list,
+                            gf_boolean_t _local__xaction_peers);
 
 int
 glusterd_generate_and_set_task_id (dict_t *dict, char *key);
@@ -785,11 +788,13 @@ glusterd_take_lvm_snapshot (glusterd_brickinfo_t *brickinfo,
 
 int32_t
 glusterd_snap_quorum_check (dict_t *dict, gf_boolean_t snap_volume,
-                            char **op_errstr);
+                            char **op_errstr,
+                            struct list_head *peers_list);
 
 int32_t
 glusterd_snap_quorum_check_for_create (dict_t *dict, gf_boolean_t snap_volume,
-                                       char **op_errstr);
+                                       char **op_errstr,
+                                       struct list_head *peers_list);
 
 int32_t
 glusterd_volume_quorum_check (glusterd_volinfo_t *volinfo, int64_t index,
