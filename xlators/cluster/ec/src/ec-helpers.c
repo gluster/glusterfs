63dc6e1942dffcddd99c5048a498ca00eead8baa
Xavier Hernandez <xhernandez@datalab.es>
2015-01-15 09:38:04 +0100

    ec: Remove unneeded 'inline' for ec_is_internal_xattr()
    An incorrectly placed 'inline' keyword caused compilation warnings
    with gcc 5.
    
    Change-Id: I2bf8c39b1514ea7dac13e82eb3b6ff4b98e62c79
    BUG: 1182267
    Signed-off-by: Xavier Hernandez <xhernandez@datalab.es>
    Reviewed-on: http://review.gluster.org/9452
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Kaleb KEITHLEY <kkeithle@redhat.com>
    Reviewed-by: Vijay Bellur <vbellur@redhat.com>

--- xlators/cluster/ec/src/ec-helpers.c
+++ xlators/cluster/ec/src/ec-helpers.c
@@ -695,7 +695,7 @@ uint64_t ec_adjust_size(ec_t * ec, uint64_t size, int32_t scale)
     return size;
 }
 
-inline gf_boolean_t
+gf_boolean_t
 ec_is_internal_xattr (dict_t *dict, char *key, data_t *value, void *data)
 {
         if (key &&
