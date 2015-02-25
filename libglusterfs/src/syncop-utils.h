ea4c9c23e7cd6750c39edea96b6fe1312bd1ed5e
Pranith Kumar K <pkarampu@redhat.com>
2015-02-25 11:37:57 +0530

    libglusterfs: Moved common functions as utils in syncop/common-utils
    These will be used by both afr and ec. Moved syncop_dirfd, syncop_ftw,
    syncop_dir_scan functions also into syncop-utils.c
    
    Change-Id: I467253c74a346e1e292d36a8c1a035775c3aa670
    BUG: 1177601
    Signed-off-by: Pranith Kumar K <pkarampu@redhat.com>
    Reviewed-on: http://review.gluster.org/9740
    Reviewed-by: Krutika Dhananjay <kdhananj@redhat.com>
    Reviewed-by: Anuradha Talur <atalur@redhat.com>
    Reviewed-by: Ravishankar N <ravishankar@redhat.com>
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Vijay Bellur <vbellur@redhat.com>

---
+++ libglusterfs/src/syncop-utils.h
@@ -0,0 +1,33 @@
+/*
+  Copyright (c) 2015, Red Hat, Inc. <http://www.redhat.com>
+  This file is part of GlusterFS.
+
+  This file is licensed to you under your choice of the GNU Lesser
+  General Public License, version 3 or any later version (LGPLv3 or
+  later), or the GNU General Public License, version 2 (GPLv2), in all
+  cases as published by the Free Software Foundation.
+*/
+
+#ifndef _SYNCOP_UTILS_H
+#define _SYNCOP_UTILS_H
+
+int
+syncop_ftw (xlator_t *subvol, loc_t *loc, int pid, void *data,
+            int (*fn) (xlator_t *subvol, gf_dirent_t *entry, loc_t *parent,
+                       void *data));
+
+int
+syncop_dir_scan (xlator_t *subvol, loc_t *loc, int pid, void *data,
+                 int (*fn) (xlator_t *subvol, gf_dirent_t *entry, loc_t *parent,
+                            void *data));
+
+int
+syncop_dirfd (xlator_t *subvol, loc_t *loc, fd_t **fd, int pid);
+
+int
+syncop_is_subvol_local (xlator_t *this, loc_t *loc, gf_boolean_t *is_local);
+
+int
+syncop_gfid_to_path (inode_table_t *itable, xlator_t *subvol, uuid_t gfid,
+                     char **path_p);
+#endif /* _SYNCOP_H */
