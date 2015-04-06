9d256d28dca22f01c061845baf6e0a6a655ecb4c
Manikandan Selvaganesh <mselvaga@redhat.com>
2015-04-06 13:06:31 +0530

    nfs : introducing logging framework for nfs translator
    Change-Id: I3a47cdd06595c87da8e822d11683d68b43c11cda
    BUG: 1194640
    Signed-off-by: Manikandan Selvaganesh <mselvaga@redhat.com>
    Reviewed-on: http://review.gluster.org/9945
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: jiffin tony Thottan <jthottan@redhat.com>
    Reviewed-by: Niels de Vos <ndevos@redhat.com>

--- libglusterfs/src/glfs-message-id.h
+++ libglusterfs/src/glfs-message-id.h
@@ -76,6 +76,10 @@
 #define GLFS_MSGID_COMP_UPCALL_END         (GLFS_MSGID_COMP_UPCALL +\
                                            GLFS_MSGID_SEGMENT)
 
+#define GLFS_MSGID_COMP_NFS                GLFS_MSGID_COMP_UPCALL_END
+#define GLFS_MSGID_COMP_NFS_END            (GLFS_MSGID_COMP_NFS +\
+                                           GLFS_MSGID_SEGMENT)
+
 /* --- new segments for messages goes above this line --- */
 
 #endif /* !_GLFS_MESSAGE_ID_H_ */
