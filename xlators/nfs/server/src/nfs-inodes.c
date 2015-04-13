8b9c580044f2ac7ecbf2628857c87c2946572019
Manikandan Selvaganesh <mselvaga@redhat.com>
2015-04-13 15:50:12 +0530

    nfs / nfs files : porting log messages to a new framework
            Backport of http://review.gluster.org/#/c/10214/
    
    Porting log messages to a new framework for nfs-common.c,
    nfs-fops.c, nfs-fops.h, nfs-inodes.c, nfs3-fh.c, nfs3-helpers.c.
    
    Cherry picked from f3ff9abbe5f80911a88222cbdbcfa8616b4743da
    > Change-Id: I55cd94de62eafe6ad3754cd065d7459a81a9f285
    > BUG: 1194640
    > Signed-off-by: Manikandan Selvaganesh <mselvaga@redhat.com>
    > Reviewed-on: http://review.gluster.org/10214
    > Tested-by: Gluster Build System <jenkins@build.gluster.com>
    > Reviewed-by: Niels de Vos <ndevos@redhat.com>
    
    Change-Id: I55cd94de62eafe6ad3754cd065d7459a81a9f285
    BUG: 1217722
    Signed-off-by: Manikandan Selvaganesh <mselvaga@redhat.com>
    Reviewed-on: http://review.gluster.org/10550
    Tested-by: NetBSD Build System
    Reviewed-by: jiffin tony Thottan <jthottan@redhat.com>
    Reviewed-by: Niels de Vos <ndevos@redhat.com>
    Tested-by: Gluster Build System <jenkins@build.gluster.com>

--- xlators/nfs/server/src/nfs-inodes.c
+++ xlators/nfs/server/src/nfs-inodes.c
@@ -20,6 +20,7 @@
 #include "nfs-inodes.h"
 #include "nfs-fops.h"
 #include "xlator.h"
+#include "nfs-messages.h"
 
 #include <libgen.h>
 
@@ -108,7 +109,8 @@ nfs_inode_create (xlator_t *nfsx, xlator_t *xl, nfs_user_t *nfu,
 
         newfd = fd_create (pathloc->inode, 0);
         if (!newfd) {
-                gf_log (GF_NFS, GF_LOG_ERROR, "Failed to create new fd");
+                gf_msg (GF_NFS, GF_LOG_ERROR, ENOMEM, NFS_MSG_NO_MEMORY,
+                        "Failed to create new fd");
                 ret = -ENOMEM;
                 goto wipe_nfl;
         }
@@ -218,7 +220,8 @@ nfs_inode_open (xlator_t *nfsx, xlator_t *xl, nfs_user_t *nfu, loc_t *loc,
 
         newfd = fd_create (loc->inode, 0);
         if (!newfd) {
-                gf_log (GF_NFS, GF_LOG_ERROR, "Failed to create fd");
+                gf_msg (GF_NFS, GF_LOG_ERROR, ENOMEM, NFS_MSG_NO_MEMORY,
+                        "Failed to create fd");
                 ret = -ENOMEM;
                 goto err;
         }
@@ -588,7 +591,8 @@ nfs_inode_opendir (xlator_t *nfsx, xlator_t *xl, nfs_user_t *nfu, loc_t *loc,
 
         newfd = fd_create (loc->inode, 0);
         if (!newfd) {
-                gf_log (GF_NFS, GF_LOG_ERROR, "Failed to create fd");
+                gf_msg (GF_NFS, GF_LOG_ERROR, ENOMEM, NFS_MSG_NO_MEMORY,
+                        "Failed to create fd");
                 ret = -ENOMEM;
                 goto err;
         }
