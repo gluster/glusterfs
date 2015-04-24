585fce89c120cb24240cc336e8cb01425c11ff8c
Kotresh HR <khiremat@redhat.com>
2015-04-24 19:10:11 +0530

    tests : Skip geo-rep regression for now
    As geo-rep regression is causing cores,
    it is being disabled for now and will be
    re-enabled once the issues are fixed.
    
    Change-Id: Ie846b599369acb4a8fd814d1e889cffb4610a2c8
    BUG: 1163543
    Signed-off-by: Kotresh HR <khiremat@redhat.com>
    Reviewed-on: http://review.gluster.org/10369
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Vijay Bellur <vbellur@redhat.com>
    Tested-by: Vijay Bellur <vbellur@redhat.com>

--- run-tests.sh
+++ run-tests.sh
@@ -164,7 +164,8 @@ function run_tests()
         return $match
     }
     RES=0
-    for t in $(find ${regression_testsdir}/tests | LC_COLLATE=C sort) ; do
+    for t in $(find ${regression_testsdir}/tests | grep -v geo-rep \
+    | LC_COLLATE=C sort) ; do
         if match $t "$@" ; then
             if [ -d $t ] ; then
                 echo "Running tests in directory $t"
@@ -205,6 +206,7 @@ function is_bad_test ()
 function run_all ()
 {
     find ${regression_testsdir}/tests -name '*.t' \
+    | grep -v geo-rep \
     | LC_COLLATE=C sort \
     | while read t; do
 	old_cores=$(ls /core.* 2> /dev/null | wc -l)
