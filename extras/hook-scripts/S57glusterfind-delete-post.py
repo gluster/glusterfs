d28b226131d420070fa5cee921a4ad0be9d6446a
Aravinda VK <avishwan@redhat.com>
2015-05-27 18:05:35 +0530

    tools/glusterfind: Cleanup glusterfind dir after a volume delete
    If `glusterfind delete` command was not run before volume delete, stale
    session directories exists in /var/lib/glusterd/glusterfind directories.
    
    Also shows these sessions in `glusterfind list`
    
    When Volume is deleted, Post hook will be run which cleans up the stale
    session directories
    
    BUG: 1225465
    Change-Id: I54c46c30313e92c1bb4cb07918ed2029b375462c
    Signed-off-by: Aravinda VK <avishwan@redhat.com>
    Reviewed-on: http://review.gluster.org/10944
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Kotresh HR <khiremat@redhat.com>
    Reviewed-by: Vijay Bellur <vbellur@redhat.com>

---
+++ extras/hook-scripts/S57glusterfind-delete-post.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+import os
+import shutil
+from errno import ENOENT
+from subprocess import Popen, PIPE
+from argparse import ArgumentParser
+
+
+DEFAULT_GLUSTERD_WORKDIR = "/var/lib/glusterd"
+
+
+def handle_rm_error(func, path, exc_info):
+    if exc_info[1].errno == ENOENT:
+        return
+
+    raise exc_info[1]
+
+
+def get_glusterd_workdir():
+    p = Popen(["gluster", "system::", "getwd"],
+              stdout=PIPE, stderr=PIPE)
+
+    out, _ = p.communicate()
+
+    if p.returncode == 0:
+        return out.strip()
+    else:
+        return DEFAULT_GLUSTERD_WORKDIR
+
+
+def get_args():
+    parser = ArgumentParser(description="Volume delete post hook script")
+    parser.add_argument("--volname")
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+    glusterfind_dir = os.path.join(get_glusterd_workdir(), "glusterfind")
+
+    # Check all session directories, if any directory found for
+    # the deleted volume, cleanup all the session directories
+    for session in os.listdir(glusterfind_dir):
+        # Possible session directory
+        volume_session_path = os.path.join(glusterfind_dir,
+                                           session,
+                                           args.volname)
+        if os.path.exists(volume_session_path):
+            shutil.rmtree(volume_session_path, onerror=handle_rm_error)
+
+        # Try to Remove directory, if any other dir exists for different
+        # volume, then rmdir will fail with ENOTEMPTY which is fine
+        try:
+            os.rmdir(os.path.join(glusterfind_dir, session))
+        except (OSError, IOError):
+            pass
+
+
+if __name__ == "__main__":
+    main()
