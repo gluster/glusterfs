3a94f4c52b56f08c326d3212ca5708e8daf8585b
Mohammed Rafi KC <rkavunga@redhat.com>
2015-06-01 16:53:32 +0530

    tiering:static function called from a non static inline function
    gcc v5.1.1 throws warning for calling a static function from a
    non-static inline function.
    
    
    <snippet from compiler warning>
      CC       tier.lo
    tier.c:610:15: warning: 'tier_migrate_using_query_file' is static but used in inline function 'tier_migrate_files_using_qfile' which is not static
             ret = tier_migrate_using_query_file ((void *)query_cbk_args);
                   ^
    tier.c:585:47: warning: 'tier_process_brick_cbk' is static but used in inline function 'tier_build_migration_qfile' which is not static
             ret = dict_foreach (args->brick_list, tier_process_brick_cbk,
                                                   ^
    tier.c:565:176: warning: 'demotion_qfile' is static but used in inline function 'tier_build_migration_qfile' which is not static
    tier.c:565:158: warning: 'promotion_qfile' is static but used in inline function 'tier_build_migration_qfile' which is not static
    tier.c:563:58: warning: 'demotion_qfile' is static but used in inline function 'tier_build_migration_qfile' which is not static
    tier.c:563:40: warning: 'promotion_qfile' is static but used in inline function 'tier_build_migration_qfile' which is not static
             ret = remove (GET_QFILE_PATH (is_promotion));
                                            ^
      CCLD     tier.la
    
    </snip>
    
    Change-Id: I46046feeb79ab4e2724b0ba6b02c9ec8b121ff4e
    BUG: 1226881
    Signed-off-by: Mohammed Rafi KC <rkavunga@redhat.com>
    Reviewed-on: http://review.gluster.org/11032
    Tested-by: NetBSD Build System <jenkins@build.gluster.org>
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Niels de Vos <ndevos@redhat.com>
    Reviewed-by: Anoop C S <achiraya@redhat.com>
    Reviewed-by: Kaleb KEITHLEY <kkeithle@redhat.com>

--- xlators/cluster/dht/src/tier.c
+++ xlators/cluster/dht/src/tier.c
@@ -544,7 +544,7 @@ out:
         return ret;
 }
 
-inline int
+static inline int
 tier_build_migration_qfile (demotion_args_t *args,
                             query_cbk_args_t *query_cbk_args,
                             gf_boolean_t is_promotion)
@@ -593,7 +593,7 @@ out:
         return ret;
 }
 
-inline int
+static inline int
 tier_migrate_files_using_qfile (demotion_args_t *comp,
                                 query_cbk_args_t *query_cbk_args,
                                 char *qfile)
