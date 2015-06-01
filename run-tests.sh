7a2fb3694d74ff9ab64d1cbaa113a5428a92269f
Niels de Vos <ndevos@redhat.com>
2015-06-01 16:10:40 +0200

    tests: use single-brick volume for mount-nfs-auth.t
    When running with the replica-3 volume, the "big_write" test sometimes
    becomes unresponsive. This seems to be an issue (bug 1226941) in the
    RPC/socket-layer, and not related to the NFS test itself.
    
    BUG: 1163543
    Change-Id: I51115e4b68d45f3ef7902b4f7a8535518d09408f
    Signed-off-by: Niels de Vos <ndevos@redhat.com>
    Reviewed-on: http://review.gluster.org/11085
    Tested-by: NetBSD Build System <jenkins@build.gluster.org>
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Kaleb KEITHLEY <kkeithle@redhat.com>

--- run-tests.sh
+++ run-tests.sh
@@ -195,7 +195,6 @@ function is_bad_test ()
 {
     local name=$1
     for bt in ./tests/basic/quota-anon-fd-nfs.t \
-              ./tests/basic/mount-nfs-auth.t\
 	      ; do
         [ x"$name" = x"$bt" ] && return 0 # bash: zero means true/success
     done
