fcb1b773b06235d67f24f662424d28e3b2c2f5a3
Sachin Pandit <spandit@redhat.com>
2015-06-04 11:01:38 +0530

    features/marker : Pass along xdata to lower translator
    The problem was in marker xlator, where during
    rename a NULL value is passed during STACK_WIND.
    Marker needs to pass the xdata un-modified to next
    translator if marker does not rely on that.
    
    Change-Id: I9e47e504fd241263987645abfed7ca13c0d54a80
    BUG: 1230693
    Signed-off-by: Sachin Pandit <spandit@redhat.com>
    Reviewed-on: http://review.gluster.org/11089
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Raghavendra G <rgowdapp@redhat.com>
    Tested-by: Raghavendra G <rgowdapp@redhat.com>
    Signed-off-by: Sachin Pandit <spandit@redhat.com>
    Reviewed-on: http://review.gluster.org/11182
    Reviewed-by: Vijay Bellur <vbellur@redhat.com>

--- xlators/features/marker/src/marker.c
+++ xlators/features/marker/src/marker.c
@@ -1339,7 +1339,7 @@ marker_do_rename (call_frame_t *frame, void *cookie, xlator_t *this,
 
         STACK_WIND (frame, marker_rename_cbk, FIRST_CHILD(this),
                     FIRST_CHILD(this)->fops->rename, &oplocal->loc,
-                    &local->loc, NULL);
+                    &local->loc, local->xdata);
 
         return 0;
 
@@ -1595,6 +1595,8 @@ marker_rename (call_frame_t *frame, xlator_t *this, loc_t *oldloc,
         lock.l_start  = 0;
         lock.l_type   = F_WRLCK;
         lock.l_whence = SEEK_SET;
+
+        local->xdata = dict_ref (xdata);
 
         STACK_WIND (frame,
                     marker_rename_inodelk_cbk,
