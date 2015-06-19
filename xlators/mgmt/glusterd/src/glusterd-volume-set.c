f460d1c9a2d83b24500c574aabce75438bdc8f9d
Avra Sengupta <asengupt@redhat.com>
2015-06-19 12:32:51 +0530

    glusterd/shared_storage: Added help description for enable-shared-storage option
    Added help description in gluster volume set help for
    cluster.enable-shared-storage option.
    
    Change-Id: I36481d1ca856739e83f0c9c7432ac0cb4131d665
    BUG: 1233544
    Signed-off-by: Avra Sengupta <asengupt@redhat.com>
    Reviewed-on: http://review.gluster.org/11324
    Tested-by: NetBSD Build System <jenkins@build.gluster.org>
    Reviewed-by: Rajesh Joseph <rjoseph@redhat.com>

--- xlators/mgmt/glusterd/src/glusterd-volume-set.c
+++ xlators/mgmt/glusterd/src/glusterd-volume-set.c
@@ -1821,6 +1821,11 @@ struct volopt_map_entry glusterd_volopt_map[] = {
           .value       = "disable",
           .type        = GLOBAL_DOC,
           .op_version  = GD_OP_VERSION_3_7_1,
+          .description = "Create and mount the shared storage volume"
+                         "(gluster_shared_storage) at "
+                         "/var/run/gluster/shared_storage on enabling this "
+                         "option. Unmount and delete the shared storage volume "
+                         " on disabling this option."
         },
 
 #if USE_GFDB /* no GFDB means tiering is disabled */
