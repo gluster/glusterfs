8849a9a836af64ec150fb4909d02d5eff1304ade
vmallika <vmallika@redhat.com>
2015-06-25 14:58:50 +0530

    quota: marker accounting goes bad with rename while writing a file
    With below test-case, marker accounting becomes bad:
    1) Create a volume with 1 brick
    2) fuse mount
    3) on one terminal write some data
       dd if=/dev/zero of=f1 bs=1M count=500 oflag=sync
    4) on another terminal execute below rename operation while the write is
    still in progress
        for i in {1..50}; do
            ii=`expr $i + 1`;
            mv f$i f$ii;
        done
    
    remove-xattr is already on while doing rename operation,
    we should not be doing again in background when reducing the
    parent size
    
    Change-Id: I969a64bb559e2341315928b55b99203e9ddee3f2
    BUG: 1235195
    Signed-off-by: vmallika <vmallika@redhat.com>
    Reviewed-on: http://review.gluster.org/11403
    Tested-by: NetBSD Build System <jenkins@build.gluster.org>
    Reviewed-by: Raghavendra G <rgowdapp@redhat.com>
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Tested-by: Raghavendra G <rgowdapp@redhat.com>

---
+++ tests/bugs/quota/bug-1235182.t
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# This regression test tries to ensure renaming a directory with content, and
+# no limit set, is accounted properly, when moved into a directory with quota
+# limit set.
+
+. $(dirname $0)/../../include.rc
+. $(dirname $0)/../../volume.rc
+
+cleanup;
+
+function usage()
+{
+        local QUOTA_PATH=$1;
+        $CLI volume quota $V0 list $QUOTA_PATH | grep "$QUOTA_PATH" | awk '{print $4}'
+}
+
+QDD=$(dirname $0)/quota
+# compile the test write program and run it
+build_tester $(dirname $0)/../../basic/quota.c -o $QDD
+
+TEST glusterd
+TEST pidof glusterd;
+TEST $CLI volume info;
+
+TEST $CLI volume create $V0 $H0:$B0/${V0}{1};
+TEST $CLI volume start $V0;
+
+TEST $CLI volume quota $V0 enable;
+
+TEST glusterfs --volfile-id=$V0 --volfile-server=$H0 $M0;
+
+TEST $CLI volume quota $V0 limit-usage / 1GB
+TEST $CLI volume quota $V0 hard-timeout 0
+TEST $CLI volume quota $V0 soft-timeout 0
+
+$QDD $M0/f1 256 400&
+PID=$!
+EXPECT_WITHIN $PROCESS_UP_TIMEOUT "0" STAT $M0/f1
+TESTS_EXPECTED_IN_LOOP=50
+for i in {1..50}; do
+        ii=`expr $i + 1`;
+        TEST_IN_LOOP mv $M0/f$i $M0/f$ii;
+done
+
+echo "Wait for process with pid $PID to complete"
+wait $PID
+echo "Process with pid $PID finished"
+
+EXPECT_WITHIN $MARKER_UPDATE_TIMEOUT "100.0MB" usage "/"
+
+TEST $CLI volume stop $V0
+TEST $CLI volume delete $V0
+EXPECT "1" get_aux
+
+rm -f $QDD
+
+cleanup;
--- xlators/features/marker/src/marker-quota.c
+++ xlators/features/marker/src/marker-quota.c
@@ -2580,10 +2580,14 @@ out:
 
 int32_t
 mq_remove_contri (xlator_t *this, loc_t *loc, quota_inode_ctx_t *ctx,
-                  inode_contribution_t *contri, quota_meta_t *delta)
+                  inode_contribution_t *contri, quota_meta_t *delta,
+                  gf_boolean_t remove_xattr)
 {
         int32_t              ret                         = -1;
         char                 contri_key[CONTRI_KEY_MAX]  = {0, };
+
+        if (remove_xattr == _gf_false)
+                goto done;
 
         GET_CONTRI_KEY (contri_key, contri->gfid, ret);
         if (ret < 0) {
@@ -2610,6 +2614,7 @@ mq_remove_contri (xlator_t *this, loc_t *loc, quota_inode_ctx_t *ctx,
                 }
         }
 
+done:
         LOCK (&contri->lock);
         {
                 contri->contribution += delta->size;
@@ -3037,6 +3042,7 @@ mq_reduce_parent_size_task (void *opaque)
         xlator_t                *this          = NULL;
         loc_t                   *loc           = NULL;
         int64_t                  contri        = 0;
+        gf_boolean_t             remove_xattr  = _gf_true;
 
         GF_ASSERT (opaque);
 
@@ -3063,7 +3069,11 @@ mq_reduce_parent_size_task (void *opaque)
         }
 
         if (contri >= 0) {
-                /* contri paramater is supplied only for rename operation */
+                /* contri paramater is supplied only for rename operation.
+                 * remove xattr is alreday performed, we need to skip
+                 * removexattr for rename operation
+                 */
+                remove_xattr = _gf_false;
                 delta.size = contri;
                 delta.file_count = 1;
                 delta.dir_count = 0;
@@ -3117,7 +3127,8 @@ mq_reduce_parent_size_task (void *opaque)
 
         mq_sub_meta (&delta, NULL);
 
-        ret = mq_remove_contri (this, loc, ctx, contribution, &delta);
+        ret = mq_remove_contri (this, loc, ctx, contribution, &delta,
+                                remove_xattr);
         if (ret < 0)
                 goto out;
 
