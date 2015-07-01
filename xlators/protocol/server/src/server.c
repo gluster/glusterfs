1e6d239f3d1c646289f6f7d7b88f5d7dedf497a0
Kaushal M <kaushal@redhat.com>
2015-07-01 11:44:53 +0530

    protocol/server: Correctly reconfigure auth.ssl-allow
    auth.ssl-allow wasn't being handled during reconfigure. This prevented
    the ssl-allow list from being live reloaded.
    
    Change-Id: If3435793a5684881b012de77cb254b1847b37810
    BUG: 1238072
    Signed-off-by: Kaushal M <kaushal@redhat.com>
    Reviewed-on: http://review.gluster.org/11487
    Tested-by: NetBSD Build System <jenkins@build.gluster.org>
    Reviewed-by: Raghavendra G <rgowdapp@redhat.com>
    Tested-by: Gluster Build System <jenkins@build.gluster.com>

--- xlators/protocol/server/src/server.c
+++ xlators/protocol/server/src/server.c
@@ -611,6 +611,7 @@ _delete_auth_opt (dict_t *this, char *key, data_t *value, void *data)
 {
         char *auth_option_pattern[] = { "auth.addr.*.allow",
                                         "auth.addr.*.reject",
+                                        "auth.login.*.ssl-allow",
                                         NULL};
         int i = 0;
 
@@ -630,6 +631,7 @@ _copy_auth_opt (dict_t *unused, char *key, data_t *value, void *xl_dict)
 {
         char *auth_option_pattern[] = { "auth.addr.*.allow",
                                         "auth.addr.*.reject",
+                                        "auth.login.*.ssl-allow",
                                         NULL};
         int i = 0;
 
