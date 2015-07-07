2d7583bf35c333935b0a666d99129876fbda5ac2
vmallika <vmallika@redhat.com>
2015-07-07 16:03:26 +0530

    quota/marker: set lk_owner when taking lock on parent in rename
    Before doing a rename operation marker takes inode lock on the file
    parent,
    here lk_owner is NULL and this can cause accounting problem with
    multiple rename on the same directory
    
    This patch fix the problem by setting lk_owner
    
    Change-Id: Ibb789e39b2833e425d0a5fca85282ff1465206cb
    BUG: 1240598
    Signed-off-by: vmallika <vmallika@redhat.com>
    Reviewed-on: http://review.gluster.org/11561
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Tested-by: NetBSD Build System <jenkins@build.gluster.org>
    Reviewed-by: Raghavendra G <rgowdapp@redhat.com>

--- libglusterfs/src/lkowner.h
+++ libglusterfs/src/lkowner.h
@@ -75,4 +75,23 @@ is_same_lkowner (gf_lkowner_t *l1, gf_lkowner_t *l2)
         return ((l1->len == l2->len) && !memcmp(l1->data, l2->data, l1->len));
 }
 
+static inline int
+is_lk_owner_null (gf_lkowner_t *lkowner)
+{
+        int is_null = 1;
+        int i       = 0;
+
+        if (lkowner == NULL || lkowner->len == 0)
+                goto out;
+
+        for (i = 0; i < lkowner->len; i++) {
+                if (lkowner->data[i] != 0) {
+                        is_null = 0;
+                        break;
+                }
+        }
+out:
+        return is_null;
+}
+
 #endif /* _LK_OWNER_H */
