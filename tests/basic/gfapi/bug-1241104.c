7e65e1b5a9a7bdaa7ca28bc5c1773bc5472f19af
Soumya Koduri <skoduri@redhat.com>
2015-07-10 12:40:24 +0530

    locks: Handle negative values for flock->l_len
    As per 'man 3 fcntl',
    "If l_len is positive, the area affected shall start at
    l_start and end at l_start+l_len−1. If l_len is negative,
    the area affected shall start at l_start+l_len and end at
    l_start−1. Locks may start and extend beyond the current
    end of a file, but shall not extend before the beginning
    of the file."
    
    Currently we return EINVAL if l_len is found to be negative.
    Fixed the same as mentioned in the man page.
    
    This is backport of the below patch
        - http://review.gluster.org/11613
    
    Change-Id: I493ce202c543185fc4ae7266d1aaf9d7e2a66991
    BUG: 1312200
    Signed-off-by: Soumya Koduri <skoduri@redhat.com>
    Reviewed-on: http://review.gluster.org/11613
    Reviewed-by: Kaleb KEITHLEY <kkeithle@redhat.com>
    Reviewed-by: Niels de Vos <ndevos@redhat.com>
    Reviewed-by: Raghavendra G <rgowdapp@redhat.com>
    Reviewed-on: http://review.gluster.org/13526
    Smoke: Gluster Build System <jenkins@build.gluster.com>
    NetBSD-regression: NetBSD Build System <jenkins@build.gluster.org>
    CentOS-regression: Gluster Build System <jenkins@build.gluster.com>

---
+++ tests/basic/gfapi/bug-1241104.c
@@ -0,0 +1,91 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <time.h>
+#include <limits.h>
+#include <alloca.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <glusterfs/api/glfs.h>
+#include <glusterfs/api/glfs-handles.h>
+#include <sys/wait.h>
+
+int gfapi = 1;
+
+#define LOG_ERR(func, ret) do { \
+        if (ret != 0) {            \
+                fprintf (stderr, "%s : returned error %d (%s)\n", \
+                         func, ret, strerror (errno)); \
+                goto out; \
+        } else { \
+                fprintf (stderr, "%s : returned %d\n", func, ret); \
+        } \
+        } while (0)
+
+int
+main (int argc, char *argv[])
+{
+        glfs_t    *fs = NULL;
+        int        ret = 0, i, status = 0;
+        glfs_fd_t *fd = NULL;
+        char      *filename = "file_tmp";
+        char      *volname = NULL;
+        char      *logfile = NULL;
+        struct flock lock = {0, };
+
+        if (argc != 3) {
+                fprintf (stderr, "Invalid argument\n");
+                exit(1);
+        }
+
+        volname = argv[1];
+        logfile = argv[2];
+
+        fs = glfs_new (volname);
+        if (!fs) {
+                fprintf (stderr, "glfs_new: returned NULL\n");
+                return -1;
+        }
+
+        ret = glfs_set_volfile_server (fs, "tcp", "localhost", 24007);
+        LOG_ERR("glfs_set_volfile_server", ret);
+
+        ret = glfs_set_logging (fs, logfile, 7);
+        LOG_ERR("glfs_set_logging", ret);
+
+        ret = glfs_init (fs);
+        LOG_ERR("glfs_init", ret);
+
+        fd = glfs_creat(fs, filename, O_RDWR|O_SYNC, 0644);
+        if (fd <= 0) {
+                ret = -1;
+                LOG_ERR ("glfs_creat", ret);
+        }
+        fprintf (stderr, "glfs-create fd - %d\n", fd);
+
+	/* validate locks for negative range */
+        lock.l_type = F_WRLCK;
+        lock.l_whence = SEEK_SET;
+        lock.l_start = 10;
+        lock.l_len = -9;
+
+        ret = glfs_posix_lock (fd, F_SETLK, &lock);
+        LOG_ERR ("glfs_posix_lock", ret);
+
+err:
+        glfs_close(fd);
+        LOG_ERR ("glfs_close", ret);
+
+out:
+        if (fs) {
+                ret = glfs_fini(fs);
+                fprintf (stderr, "glfs_fini(fs) returned %d \n", ret);
+        }
+
+        if (ret)
+                exit(1);
+        exit(0);
+}
+
+
