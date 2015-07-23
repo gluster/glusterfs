gluster/glusterfs

commit 2ca64411fd134eb03fc8b40dec91e94027ad82b1

geo-replication: fix memory leak in gsyncd
1. leak in str2argv() function:

char *strdup(const char *s);
The strdup() function returns a pointer to a new string which is a
duplicate of the  string  s. Memory for the new string is obtained
with malloc(3), and can be freed with free(3).

so when using strdup, user has to take care of freeing the memory
allocated by strdup library function.

2. leak in main() function:

str2argv() function calls calloc and allocates memory pointed to argv,
after return from str2argv() memory pointed to argv has to be cleaned
in main().

This patch is to fix 2 memory leaks mentioned above.

Change-Id: I6bf26101e0460a7324ac7bdb69905839688d4987
BUG: 1222898
Signed-off-by: Prasanna Kumar Kalever <prasanna.kalever@redhat.com>
Reviewed-on: http://review.gluster.org/10831
Tested-by: NetBSD Build System <jenkins@build.gluster.org>
Reviewed-by: Milind Changire <mchangir@redhat.com>
Reviewed-by: Kotresh HR <khiremat@redhat.com>
Reviewed-by: Venky Shankar <vshankar@redhat.com>

geo-replication/src/gsyncd.c
@@ -61,12 +61,13 @@ str2argv (char *str, char ***argv)
 {
         char *p         = NULL;
         char *savetok   = NULL;
+        char *temp      = NULL;
         int argc        = 0;
         size_t argv_len = 32;
         int ret         = 0;
 
         assert (str);
-        str = strdup (str);
+        temp = str = strdup (str);
         if (!str)
                 return -1;
 
@@ -88,10 +89,12 @@ str2argv (char *str, char ***argv)
                 (*argv)[argc - 1] = p;
         }
 
+        free(temp);
         return argc;
 
  error:
         fprintf (stderr, "out of memory\n");
+        free(temp);
         return -1;
 }
 
@@ -407,5 +410,6 @@ main (int argc, char **argv)
         fprintf (stderr, "invoking %s in restricted SSH session is not allowed\n",
                  b);
 
+        free(argv);
         return 1;
 }

commit 63ad603542044e473118d33bcc02f121c86beaa7

geo-rep: Fix gsyncd failing to start on slave
This is a regression introduced by 2ca6441.
The memory is freed while the caller expects
it be not. The patch fixes the same.

Change-Id: I76d95eef15b3f7af365b392f2426c8b0388254fc
BUG: 1222898
Signed-off-by: Kotresh HR <khiremat@redhat.com>
Reviewed-on: http://review.gluster.org/11751
Reviewed-by: Saravanakumar Arumugam <sarumuga@redhat.com>
Tested-by: Gluster Build System <jenkins@build.gluster.com>
Tested-by: NetBSD Build System <jenkins@build.gluster.org>
Reviewed-by: Milind Changire <mchangir@redhat.com>
Reviewed-by: Venky Shankar <vshankar@redhat.com>

geo-replication/src/gsyncd.c
@@ -62,14 +62,16 @@ str2argv (char *str, char ***argv)
         char *p         = NULL;
         char *savetok   = NULL;
         char *temp      = NULL;
+        char *temp1     = NULL;
         int argc        = 0;
         size_t argv_len = 32;
         int ret         = 0;
+        int i           = 0;
 
         assert (str);
         temp = str = strdup (str);
         if (!str)
-                return -1;
+                goto error;
 
         *argv = calloc (argv_len, sizeof (**argv));
         if (!*argv)
@@ -86,7 +88,10 @@ str2argv (char *str, char ***argv)
                         if (ret == -1)
                                 goto error;
                 }
-                (*argv)[argc - 1] = p;
+                temp1 = strdup (p);
+                if (!temp1)
+                        goto error;
+                (*argv)[argc - 1] = temp1;
         }
 
         free(temp);
@@ -95,6 +100,9 @@ str2argv (char *str, char ***argv)
  error:
         fprintf (stderr, "out of memory\n");
         free(temp);
+        for (i = 0; i < argc - 1; i++)
+                free((*argv)[i]);
+        free(*argv);
         return -1;
 }
 
@@ -346,6 +354,7 @@ main (int argc, char **argv)
         struct invocable *i     = NULL;
         char             *b     = NULL;
         char             *sargv = NULL;
+        int               j     = 0;
 
 #ifdef USE_LIBGLUSTERFS
         glusterfs_ctx_t *ctx = NULL;
@@ -410,6 +419,8 @@ main (int argc, char **argv)
         fprintf (stderr, "invoking %s in restricted SSH session is not allowed\n",
                  b);
 
+        for (j = 1; j < argc; j++)
+                free(argv[j]);
         free(argv);
         return 1;
 }