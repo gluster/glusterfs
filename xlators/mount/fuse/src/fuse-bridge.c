a52fd2cb7fa3aaff74461f58f32f4ff0b8e0904d
Kotresh HR <khiremat@redhat.com>
2015-08-04 16:20:13 +0530

    geo-rep: Don't log geo-rep safe errors in mount logs
    ENOENT is a safe error for geo-replication in case of
    rm -rf. RMDIR is recorded in changelog of each brick,
    geo-rep processes all changelogs among which one will
    succeed and rest will get ENOENT which can be ignored.
    Similarly ENOENT can also be ignored in case of all
    unlink operation during changelog replay that can
    happen when worker goes down and comes back.
    
    Change-Id: I6756f8f4c3fce7a159751a2bfce891ff16ad31a4
    BUG: 1250009
    Signed-off-by: Kotresh HR <khiremat@redhat.com>
    Reviewed-on: http://review.gluster.org/11833
    Tested-by: NetBSD Build System <jenkins@build.gluster.org>
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Aravinda VK <avishwan@redhat.com>
    Reviewed-by: Milind Changire <mchangir@redhat.com>
    Reviewed-by: Saravanakumar Arumugam <sarumuga@redhat.com>
    Reviewed-by: Venky Shankar <vshankar@redhat.com>

--- xlators/mount/fuse/src/fuse-bridge.c
+++ xlators/mount/fuse/src/fuse-bridge.c
@@ -1338,12 +1338,14 @@ fuse_unlink_cbk (call_frame_t *frame, void *cookie, xlator_t *this,
 
                 send_fuse_err (this, finh, 0);
         } else {
-                gf_log ("glusterfs-fuse",
-                        op_errno == ENOTEMPTY ? GF_LOG_DEBUG : GF_LOG_WARNING,
-                        "%"PRIu64": %s() %s => -1 (%s)", frame->root->unique,
-                        gf_fop_list[frame->root->op], state->loc.path,
-                        strerror (op_errno));
-
+                if (GF_IGNORE_IF_GSYNCD_SAFE_ERROR(frame, op_errno)) {
+                        gf_log ("glusterfs-fuse",
+                                op_errno == ENOTEMPTY ? GF_LOG_DEBUG :
+                                GF_LOG_WARNING, "%"PRIu64": %s() %s => -1 (%s)",
+                                frame->root->unique,
+                                gf_fop_list[frame->root->op], state->loc.path,
+                                strerror (op_errno));
+                }
                 send_fuse_err (this, finh, op_errno);
         }
 
