68a07320202efe24bb59937850e4bf24c6aad34c
Richard Wareing <rwareing@fb.com>
2015-08-27 21:06:37 -0700

    nfs: Fixes "Remote I/O error" mount failures
    - Fixes issue where NFS mount fail with "Remove I/O error" after the
      target directory has been deleted and re-created after the gNFSd has
      already cached the inode of the first generation of the target
      directory.
    - The solution is to follow the guidance of the AFR2 comments and
      refresh the inode by deleting it from cache and looking it up
      again.
    
    BUG: 1258069
    Change-Id: I9c7d8bd460ee9e5ea0b5b47d23886b1afcdcd563
    Reported-by: Richard Wareing <rwareing@fb.com>
    Signed-off-by: Niels de Vos <ndevos@redhat.com>
    Reviewed-on: http://review.gluster.org/12045
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Raghavendra Bhat <raghavendra@redhat.com>

--- xlators/nfs/server/src/mount3.c
+++ xlators/nfs/server/src/mount3.c
@@ -896,6 +896,21 @@ err:
         return ret;
 }
 
+int __mnt3_resolve_subdir (mnt3_resolve_t *mres);
+
+/*
+ * Per the AFR2 comments, this function performs the "fresh" lookup
+ * by deleting the inode from cache and calling __mnt3_resolve_subdir
+ * again.
+ */
+int __mnt3_fresh_lookup (mnt3_resolve_t *mres) {
+        inode_unlink (mres->resolveloc.inode,
+                mres->resolveloc.parent, mres->resolveloc.name);
+        strncpy (mres->remainingdir, mres->resolveloc.path,
+                strlen(mres->resolveloc.path));
+        nfs_loc_wipe (&mres->resolveloc);
+        return __mnt3_resolve_subdir (mres);
+}
 
 int32_t
 mnt3_resolve_subdir_cbk (call_frame_t *frame, void *cookie, xlator_t *this,
@@ -915,7 +930,11 @@ mnt3_resolve_subdir_cbk (call_frame_t *frame, void *cookie, xlator_t *this,
 
         mres = frame->local;
         mntxl = (xlator_t *)cookie;
-        if (op_ret == -1) {
+        if (op_ret == -1 && op_errno == ESTALE) {
+                /* Nuke inode from cache and try the LOOKUP
+                 * request again. */
+                return __mnt3_fresh_lookup (mres);
+        } else if (op_ret == -1) {
                 gf_log (GF_NFS, GF_LOG_ERROR, "path=%s (%s)",
                         mres->resolveloc.path, strerror (op_errno));
                 mntstat = mnt3svc_errno_to_mnterr (op_errno);
