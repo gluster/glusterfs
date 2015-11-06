a6beaa482538ecdc050029935085777862193af9
Mohamed Ashiq <mliyazud@redhat.com>
2015-11-07 00:19:48 +0530

    tiering:Message shown in gluster vol tier <volname> status output is incorrect
        Backport of http://review.gluster.org/12532
    
    Change-Id: I15a1a637090f1cc2f200d5c3582317e4aa3cf334
    BUG: 1279309
    Signed-off-by: Mohamed Ashiq <mliyazud@redhat.com>
    >Change-Id: I15a1a637090f1cc2f200d5c3582317e4aa3cf334
    >BUG: 1278927
    >Signed-off-by: Mohamed Ashiq <mliyazud@redhat.com>
    >Reviewed-on: http://review.gluster.org/12532
    >Tested-by: NetBSD Build System <jenkins@build.gluster.org>
    >Tested-by: Gluster Build System <jenkins@build.gluster.com>
    >Reviewed-by: Dan Lambright <dlambrig@redhat.com>
    >Tested-by: Dan Lambright <dlambrig@redhat.com>
    Reviewed-on: http://review.gluster.org/12547
    Tested-by: NetBSD Build System <jenkins@build.gluster.org>
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Dan Lambright <dlambrig@redhat.com>
    Tested-by: Dan Lambright <dlambrig@redhat.com>

--- cli/src/cli-rpc-ops.c
+++ cli/src/cli-rpc-ops.c
@@ -1837,7 +1837,9 @@ done:
                                     rsp.op_errstr);
         else {
                 if (rsp.op_ret)
-                        if (cmd == GF_DEFRAG_CMD_START_TIER) {
+
+                        if (cmd == GF_DEFRAG_CMD_START_TIER || cmd ==
+                            GF_DEFRAG_CMD_STATUS_TIER) {
                                 cli_err ("Tiering Migration Functionality: %s:"
                                          " failed%s%s", volname,
                                          strlen (msg) ? ": " : "", msg);
@@ -1846,7 +1848,8 @@ done:
                                          volname, strlen (msg) ? ": " : "",
                                          msg);
                 else
-                        if (cmd == GF_DEFRAG_CMD_START_TIER) {
+                        if (cmd == GF_DEFRAG_CMD_START_TIER || cmd ==
+                            GF_DEFRAG_CMD_STATUS_TIER) {
                                 cli_out ("Tiering Migration Functionality: %s:"
                                          " success%s%s", volname,
                                          strlen (msg) ? ": " : "", msg);
