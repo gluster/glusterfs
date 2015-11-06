8da83fc6bfa6488ea70ae60a67391fceb60b9686
N Balachandran <nbalacha@redhat.com>
2015-11-06 14:36:37 +0530

    tests/tier : Corrected filename in run-tests.sh
    bug-1214222-directories_miising_after_attach_tier.t
    was renamed to bug-1214222-directories_missing_after_attach_tier.t
     but run-tests.sh was not updated.
    
    Change-Id: I64d6475ffb08e3252e56b4083cb0e828ba3584d9
    BUG: 1278709
    Signed-off-by: N Balachandran <nbalacha@redhat.com>
    Reviewed-on: http://review.gluster.org/12528
    Tested-by: NetBSD Build System <jenkins@build.gluster.org>
    Reviewed-by: Dan Lambright <dlambrig@redhat.com>
    Tested-by: Dan Lambright <dlambrig@redhat.com>

--- run-tests.sh
+++ run-tests.sh
@@ -197,7 +197,7 @@ function is_bad_test ()
               ./tests/basic/mount-nfs-auth.t \
               ./tests/basic/quota-nfs.t \
               ./tests/basic/tier/tier_lookup_heal.t \
-              ./tests/basic/tier/bug-1214222-directories_miising_after_attach_tier.t \
+              ./tests/basic/tier/bug-1214222-directories_missing_after_attach_tier.t \
               ./tests/bugs/snapshot/bug-1109889.t \
               ./tests/bugs/distribute/bug-1066798.t \
               ./tests/bugs/glusterd/bug-1238706-daemons-stop-on-peer-cleanup.t \
