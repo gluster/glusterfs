156022b67f5da2ca27a607ee9e7608fdf5e91a20
vmallika <vmallika@redhat.com>
2015-12-03 14:55:36 +0530

    glusterd/quota: quota-version conflict in export/import volinfo
    When exporting/importing voinfo during handshake,
    quota conf and quota xattr version were using same key
    'quota-version' and updated wrong values when importing
    quota version values.
    
    Change-Id: If939d6f5bc4851d4114963877be72dda21834f0f
    BUG: 1287996
    Signed-off-by: vmallika <vmallika@redhat.com>
    Reviewed-on: http://review.gluster.org/12865
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Tested-by: NetBSD Build System <jenkins@build.gluster.org>
    Reviewed-by: Atin Mukherjee <amukherj@redhat.com>

--- xlators/mgmt/glusterd/src/glusterd-quota.c
+++ xlators/mgmt/glusterd/src/glusterd-quota.c
@@ -1499,12 +1499,12 @@ glusterd_op_quota (dict_t *dict, char **op_errstr, dict_t *rsp_dict)
 
 
         if (GF_QUOTA_OPTION_TYPE_ENABLE == type)
-                volinfo->quota_version++;
+                volinfo->quota_xattr_version++;
         ret = glusterd_store_volinfo (volinfo,
                                       GLUSTERD_VOLINFO_VER_AC_INCREMENT);
         if (ret) {
                 if (GF_QUOTA_OPTION_TYPE_ENABLE == type)
-                        volinfo->quota_version--;
+                        volinfo->quota_xattr_version--;
                 goto out;
         }
 
@@ -1515,7 +1515,7 @@ glusterd_op_quota (dict_t *dict, char **op_errstr, dict_t *rsp_dict)
                                                   "volfiles");
                 if (GF_QUOTA_OPTION_TYPE_ENABLE == type) {
                         /* rollback volinfo */
-                        volinfo->quota_version--;
+                        volinfo->quota_xattr_version--;
                         ret = glusterd_store_volinfo (volinfo,
                                       GLUSTERD_VOLINFO_VER_AC_INCREMENT);
                 }
