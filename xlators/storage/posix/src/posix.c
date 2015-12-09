60eaa066f508ed4b6facc3743f846cb80592048b
Susant Palai <spalai@redhat.com>
2015-12-09 03:53:45 -0500

    storage/posix: fix dict leak in posix_fgetxattr
    Change-Id: I8c53805993570e6f37786dde2103cb884c026791
    BUG: 1285230
    Signed-off-by: Susant Palai <spalai@redhat.com>
    Reviewed-on: http://review.gluster.org/12916
    Tested-by: Gluster Build System <jenkins@build.gluster.com>
    Tested-by: NetBSD Build System <jenkins@build.gluster.org>
    Reviewed-by: Pranith Kumar Karampuri <pkarampu@redhat.com>

--- xlators/storage/posix/src/posix.c
+++ xlators/storage/posix/src/posix.c
@@ -4271,7 +4271,7 @@ posix_fgetxattr (call_frame_t *frame, xlator_t *this,
         _fd = pfd->fd;
 
         /* Get the total size */
-        dict = get_new_dict ();
+        dict = dict_new ();
         if (!dict) {
                 op_ret = -1;
                 op_errno = ENOMEM;
@@ -4455,7 +4455,6 @@ done:
         if (dict) {
                 dict_del (dict, GFID_XATTR_KEY);
                 dict_del (dict, GF_XATTR_VOL_ID_KEY);
-                dict_ref (dict);
         }
 
 out:
