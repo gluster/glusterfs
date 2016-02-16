f24055c6f0cb1695fbe04cf1c322bd12e8ca1d42
Sakshi Bansal <sabansal@redhat.com>
2016-02-16 11:21:29 +0530

    dentry fop serializer: added new server side xlator for dentry fop serialization
    Fops like mkdir/create, lookup, rename, unlink, link that
    happen on a particular dentry must be serialized to ensure
    atomicity. This helps prevent race between parallel mkdir,
    mkdir and lookup etc.
    
    To acheive this all fops on dentry must take entry locks
    before they proceed, once they have acquired locks, they
    perform the fop and then release the lock.
    
    Some documentation from email conversation:
    [1] http://www.gluster.org/pipermail/gluster-devel/2015-December/047314.html
    
    [2] http://www.gluster.org/pipermail/gluster-devel/2015-August/046428.html
    
    Change-Id: I6e80ba3cabfa6facd5dda63bd482b9bf18b6b79b
    BUG: 1304962
    Signed-off-by: Sakshi Bansal <sabansal@redhat.com>
    Signed-off-by: Mohammed Rafi KC <rkavunga@redhat.com>
    Reviewed-on: https://review.gluster.org/18082
    Tested-by: Raghavendra G <rgowdapp@redhat.com>
    CentOS-regression: Gluster Build System <jenkins@build.gluster.org>
    Smoke: Gluster Build System <jenkins@build.gluster.org>
    Reviewed-by: Amar Tumballi <amarts@redhat.com>

--- libglusterfs/src/glfs-message-id.h
+++ libglusterfs/src/glfs-message-id.h
@@ -203,6 +203,9 @@ GLFS_MSGID_COMP_SYMLINK_CACHE_END
 #define GLFS_MSGID_COMP_POSIX2_END              (GLFS_MSGID_COMP_POSIX2 +\
                                                 GLFS_MSGID_SEGMENT)
 
+#define GLFS_MSGID_COMP_SDFS                  GLFS_MSGID_COMP_POSIX2_END
+#define GLFS_MSGID_COMP_SDFS_END              (GLFS_MSGID_COMP_SDFS +\
+                                               GLFS_MSGID_SEGMENT)
 /* --- new segments for messages goes above this line --- */
 
 #endif /* !_GLFS_MESSAGE_ID_H_ */
---
+++ xlators/features/sdfs/src/sdfs.h
@@ -0,0 +1,47 @@
+/*
+   Copyright (c) 2016 Red Hat, Inc. <http://www.redhat.com>
+   This file is part of GlusterFS.
+
+   This file is licensed to you under your choice of the GNU Lesser
+   General Public License, version 3 or any later version (LGPLv3 or
+   later), or the GNU General Public License, version 2 (GPLv2), in all
+   cases as published by the Free Software Foundation.
+*/
+
+#include "xlator.h"
+#include "call-stub.h"
+#include "sdfs-messages.h"
+
+#define SDFS_LOCK_COUNT_MAX    2
+
+typedef struct{
+        loc_t parent_loc;
+        char *basename;
+} sdfs_entry_lock_t;
+
+typedef struct {
+        sdfs_entry_lock_t entrylk[SDFS_LOCK_COUNT_MAX];
+        int               locked[SDFS_LOCK_COUNT_MAX];
+        int               lock_count;
+} sdfs_lock_t;
+
+struct sdfs_local {
+        call_frame_t *main_frame;
+        loc_t         loc;
+        loc_t         parent_loc;
+        call_stub_t  *stub;
+        sdfs_lock_t   *lock;
+        int           op_ret;
+        int           op_errno;
+        int           call_cnt;
+};
+typedef struct sdfs_local sdfs_local_t;
+
+#define SDFS_STACK_DESTROY(frame) do {                   \
+                sdfs_local_t *__local = NULL;            \
+                __local               = frame->local;    \
+                frame->local          = NULL;            \
+                STACK_DESTROY (frame->root);            \
+                sdfs_local_cleanup (__local);         \
+        } while (0)
+
