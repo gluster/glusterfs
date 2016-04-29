e63049cba0f2b94b0b082cc8bfd5921b8443ef45
Kotresh HR <khiremat@redhat.com>
2016-04-29 17:45:31 +0530

    features/bitrot: Introduce scrubber monitor thread
    The patch does following changes.
    
     1. Introduce scrubber monitor thread.
     2. Move scrub status related APIs to separate file
        and make part of libbitrot library.
    
    Problem:
         Earlier, each child of the scrubber was maintaining
    the state machine and hence there was no way to track
    the start and end time of scrubbing as each brick has
    it's own start and end time. Also each brick was maintaining
    it's own timer wheel instance. It was also not possible
    to get scrubbed files count per session as we could not
    get last child which finishes scrubbing to reset it to
    zero.
    
    Solution:
        Introduce scrubber monitor thread. It does following.
    
     1. Maintains the scrubber state machine. Earlier each
        child had it's own state machine. Now, only monitor
        maintains on behalf of all it's children.
     2. Maintains the timer wheel instance. Earlier each
        child had it's own timer wheel instance. Now, only
        monitor maintains on behalf of all it's children.
    
    As a result, we can track the scrub statistics easily
    and correctly.
    
    Upstream master:
    http://review.gluster.org/14044
    http://review.gluster.org/#/c/14146
    
    Upstream 3.7:
    http://review.gluster.org/14140
    
    NOTE: The patch #14146 is a compilation warning not detected
    in master branch and detected only in 3.7 branch. Since the
    compilation warning is introduced by patch #14044, the above
    two backports are made into this single patch in 3.7 and the
    same is backported to 3.8.
    
    BUG: 1332839
    Change-Id: I1cec0b229c851fec793a1288c23e43bcec755cba
    Signed-off-by: Kotresh HR <khiremat@redhat.com>
    Reviewed-on: http://review.gluster.org/14201
    Smoke: Gluster Build System <jenkins@build.gluster.com>
    NetBSD-regression: NetBSD Build System <jenkins@build.gluster.org>
    CentOS-regression: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Venky Shankar <vshankar@redhat.com>

---
+++ xlators/features/bit-rot/src/bitd/bit-rot-scrub-status.h
@@ -0,0 +1,46 @@
+/*
+   Copyright (c) 2016 Red Hat, Inc. <http://www.redhat.com>
+   This file is part of GlusterFS.
+
+   This file is licensed to you under your choice of the GNU Lesser
+   General Public License, version 3 or any later version (LGPLv3 or
+   later), or the GNU General Public License, version 2 (GPLv2), in all
+   cases as published by the Free Software Foundation.
+*/
+
+#ifndef __BIT_ROT_SCRUB_STATUS_H__
+#define __BIT_ROT_SCRUB_STATUS_H__
+
+#include <stdint.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+struct br_scrub_stats {
+        uint64_t       scrubbed_files;       /* Total number of scrubbed file */
+
+        uint64_t       unsigned_files;       /* Total number of unsigned file */
+
+        uint64_t       scrub_duration;            /* Duration of last scrub */
+
+        char           last_scrub_time[1024];    /*last scrub completion time */
+
+        struct         timeval scrub_start_tv;   /* Scrubbing starting time*/
+
+        struct         timeval scrub_end_tv;     /* Scrubbing finishing time */
+
+        pthread_mutex_t  lock;
+};
+
+typedef struct br_scrub_stats br_scrub_stats_t;
+
+void
+br_inc_unsigned_file_count (br_scrub_stats_t *scrub_stat);
+void
+br_inc_scrubbed_file (br_scrub_stats_t *scrub_stat);
+void
+br_update_scrub_start_time (br_scrub_stats_t *scrub_stat, struct timeval *tv);
+void
+br_update_scrub_finish_time (br_scrub_stats_t *scrub_stat, char *timestr,
+                             struct timeval *tv);
+
+#endif /* __BIT_ROT_SCRUB_STATUS_H__ */
