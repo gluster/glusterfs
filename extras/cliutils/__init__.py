19adaad015a8e13206f656eaee135881a2da58eb
Aravinda VK <avishwan@redhat.com>
2016-05-31 13:39:05 +0530

    extras/cliutils: Utils for creating CLI tools for Gluster
    Refer README.md for documentation.
    
    BUG: 1342356
    Change-Id: Ic88504177137136bbb4b8b2c304ecc4af9bcfe30
    Signed-off-by: Aravinda VK <avishwan@redhat.com>
    Reviewed-on: http://review.gluster.org/14627
    Reviewed-by: Prashanth Pai <ppai@redhat.com>
    Reviewed-by: Niels de Vos <ndevos@redhat.com>
    Smoke: Gluster Build System <jenkins@build.gluster.org>
    NetBSD-regression: NetBSD Build System <jenkins@build.gluster.org>
    CentOS-regression: Gluster Build System <jenkins@build.gluster.org>
    Reviewed-by: Jeff Darcy <jdarcy@redhat.com>

---
+++ extras/cliutils/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Reexporting the utility funcs and classes
+from cliutils import (runcli,
+                      sync_file_to_peers,
+                      execute_in_peers,
+                      execute,
+                      node_output_ok,
+                      node_output_notok,
+                      output_error,
+                      oknotok,
+                      yesno,
+                      get_node_uuid,
+                      Cmd,
+                      GlusterCmdException)
+
+
+# This will be useful when `from cliutils import *`
+__all__ = ["runcli",
+           "sync_file_to_peers",
+           "execute_in_peers",
+           "execute",
+           "node_output_ok",
+           "node_output_notok",
+           "output_error",
+           "oknotok",
+           "yesno",
+           "get_node_uuid",
+           "Cmd",
+           "GlusterCmdException"]
