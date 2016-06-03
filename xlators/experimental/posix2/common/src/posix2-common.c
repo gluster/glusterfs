ea4d4175ab3fc5cc1545ebde4d8d35ba0bf4aa3f
Shyam <srangana@redhat.com>
2016-06-03 14:58:16 -0400

    posix2: Creation of experimental shell for posix2 xlator
    DHT2 comes with a new backend on-disk format, that requires
    a new backend storage xlator. This experimental project will
    house the artifacts for the same.
    
    Change-Id: I71a3d60a0415d0c23b2294a02b838810fa8f101f
    BUG: 1338991
    Signed-off-by: Shyam <srangana@redhat.com>
    Reviewed-on: http://review.gluster.org/14646
    Smoke: Gluster Build System <jenkins@build.gluster.com>
    NetBSD-regression: NetBSD Build System <jenkins@build.gluster.org>
    CentOS-regression: Gluster Build System <jenkins@build.gluster.com>
    Reviewed-by: Jeff Darcy <jdarcy@redhat.com>

---
+++ xlators/experimental/posix2/common/src/posix2-common.c
@@ -0,0 +1,18 @@
+/*
+  Copyright (c) 2016 Red Hat, Inc. <http://www.redhat.com>
+  This file is part of GlusterFS.
+
+  This file is licensed to you under your choice of the GNU Lesser
+  General Public License, version 3 or any later version (LGPLv3 or
+  later), or the GNU General Public License, version 2 (GPLv2), in all
+  cases as published by the Free Software Foundation.
+*/
+
+/* File: posix2-common.c
+ * This file contains common routines across ds and mds posix xlators
+ * The entire functionality including comments is TODO.
+ */
+
+#include "glusterfs.h"
+#include "logging.h"
+#include "statedump.h"
