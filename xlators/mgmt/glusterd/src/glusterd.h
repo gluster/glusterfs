8cd970aadc89a8967b0217e41c9b5fd4e3cc862f
Jiffin Tony Thottan <jthottan@redhat.com>
2016-07-12 17:23:03 +0530

    Revert "glusterd-ganesha : copy ganesha export configuration files during reboot"
    This reverts commit f71e2fa49af185779b9f43e146effd122d4e9da0.
    
    Reason:
    As part of sync up node reboot this patch copies ganesha export conf file
    from a source node. This change is no more require if the export files are
    available in shared storage.
    
    Change-Id: Id9c1ae78377bbd7d5d80aa1c14f534e30feaae97
    BUG: 1355956
    Signed-off-by: Jiffin Tony Thottan <jthottan@redhat.com>
    Reviewed-on: http://review.gluster.org/14907
    Reviewed-by: soumya k <skoduri@redhat.com>
    Smoke: Gluster Build System <jenkins@build.gluster.org>
    CentOS-regression: Gluster Build System <jenkins@build.gluster.org>
    NetBSD-regression: NetBSD Build System <jenkins@build.gluster.org>
    Reviewed-by: Kaleb KEITHLEY <kkeithle@redhat.com>

--- xlators/mgmt/glusterd/src/glusterd.h
+++ xlators/mgmt/glusterd/src/glusterd.h
@@ -1068,8 +1068,7 @@ int glusterd_check_ganesha_cmd (char *key, char *value,
                                 char **errstr, dict_t *dict);
 int glusterd_op_stage_set_ganesha (dict_t *dict, char **op_errstr);
 int glusterd_op_set_ganesha (dict_t *dict, char **errstr);
-int ganesha_manage_export (char *volname, char *value, char **op_errstr,
-                           gf_boolean_t reboot);
+int ganesha_manage_export (dict_t *dict, char *value, char **op_errstr);
 gf_boolean_t glusterd_check_ganesha_export (glusterd_volinfo_t *volinfo);
 int stop_ganesha (char **op_errstr);
 int tear_down_cluster (gf_boolean_t run_teardown);
