df6a127b4dbb96dd970d255635ccfeae17a38186
Ravishankar N <ravishankar@redhat.com>
2016-08-10 11:24:19 +0530

    libglusterfs: move alloca0 definition to common-utils
    ...and remove their definitons from EC and AFR.
    Also `s/alloca+memset0/alloca0` wherever it is used.
    
    Change-Id: I3b71e596d12a7d8900f5d761af6b98305c8874d5
    BUG: 1366226
    Signed-off-by: Ravishankar N <ravishankar@redhat.com>
    Reviewed-on: http://review.gluster.org/15147
    Smoke: Gluster Build System <jenkins@build.gluster.org>
    NetBSD-regression: NetBSD Build System <jenkins@build.gluster.org>
    Reviewed-by: Pranith Kumar Karampuri <pkarampu@redhat.com>
    CentOS-regression: Gluster Build System <jenkins@build.gluster.org>

--- xlators/cluster/afr/src/afr.h
+++ xlators/cluster/afr/src/afr.h
@@ -45,7 +45,6 @@ typedef int (*afr_inode_refresh_cbk_t) (call_frame_t *frame, xlator_t *this, int
 
 typedef int (*afr_changelog_resume_t) (call_frame_t *frame, xlator_t *this);
 
-#define alloca0(size) ({void *__ptr; __ptr = alloca(size); memset(__ptr, 0, size); __ptr;})
 #define AFR_COUNT(array,max) ({int __i; int __res = 0; for (__i = 0; __i < max; __i++) if (array[__i]) __res++; __res;})
 #define AFR_INTERSECT(dst,src1,src2,max) ({int __i; for (__i = 0; __i < max; __i++) dst[__i] = src1[__i] && src2[__i];})
 #define AFR_CMP(a1,a2,len) ({int __cmp = 0; int __i; for (__i = 0; __i < len; __i++) if (a1[__i] != a2[__i]) { __cmp = 1; break;} __cmp;})
