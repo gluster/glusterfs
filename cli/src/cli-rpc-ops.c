6f182733d943aff5212175c58dd59b2da64b9d8b
Atin Mukherjee <amukherj@redhat.com>
2016-09-02 10:42:44 +0530

    cli: fix volume status xml generation
    While generating xml, if CLI fails in between xml output doesn't get dumped into
    stdout. Fix is to invoke cli_xml_output_vol_status_end () in such failures.
    
    Change-Id: I7cb3097f5ae23092e6d20f68bd75aa190c31ed88
    BUG: 1372553
    Signed-off-by: Atin Mukherjee <amukherj@redhat.com>
    Reviewed-on: http://review.gluster.org/15384
    NetBSD-regression: NetBSD Build System <jenkins@build.gluster.org>
    CentOS-regression: Gluster Build System <jenkins@build.gluster.org>
    Smoke: Gluster Build System <jenkins@build.gluster.org>
    Reviewed-by: Samikshan Bairagya <samikshan@gmail.com>
    Reviewed-by: Prashanth Pai <ppai@redhat.com>

--- cli/src/cli-rpc-ops.c
+++ cli/src/cli-rpc-ops.c
@@ -8152,7 +8152,7 @@ gf_cli_status_cbk (struct rpc_req *req, struct iovec *iov,
                         if (ret) {
                                 gf_log ("cli", GF_LOG_ERROR,
                                         "Error outputting to xml");
-                                goto out;
+                                goto xml_end;
                         }
                 }
                 if (cmd & GF_CLI_STATUS_TASKS) {
@@ -8161,17 +8161,18 @@ gf_cli_status_cbk (struct rpc_req *req, struct iovec *iov,
                         if (ret) {
                                 gf_log ("cli", GF_LOG_ERROR,"Error outputting "
                                         "to xml");
-                                goto out;
+                                goto xml_end;
                         }
                 } else {
                         ret = cli_xml_output_vol_status (local, dict);
                         if (ret) {
                                 gf_log ("cli", GF_LOG_ERROR,
                                         "Error outputting to xml");
-                                goto out;
+                                goto xml_end;
                         }
                 }
 
+xml_end:
                 if (!local->all) {
                         ret = cli_xml_output_vol_status_end (local);
                         if (ret) {
@@ -8420,7 +8421,7 @@ gf_cli_status_volume_all (call_frame_t *frame, xlator_t *this, void *data)
                 if (ret) {
                         gf_log ("cli", GF_LOG_ERROR,
                                 "Error outputting to xml");
-                        goto out;
+                        goto xml_end;
                 }
         }
 
@@ -8457,6 +8458,7 @@ gf_cli_status_volume_all (call_frame_t *frame, xlator_t *this, void *data)
                 dict_unref (dict);
         }
 
+xml_end:
         if (global_state->mode & GLUSTER_MODE_XML) {
                 ret = cli_xml_output_vol_status_end (local);
         }
