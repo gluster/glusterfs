451aa4a28bf0be49629d956141dfbdebdf37bde7
Mohit Agrawal <moagrawa@redhat.com>
2016-09-29 13:35:26 +0530

    socket: log the client identifier in ssl connect
    Problem: client identifier is not logged in message in ssl_setup_connection
    
    Solutuion: In ssl_setup_connection xl_private is not available in rpc_transport
               so changed to this peerinfo.identifier.
    
    BUG: 1380275
    Change-Id: I05006a3d63e46de8c388298c22faa9a3329eb6f3
    Signed-off-by: Mohit Agrawal <moagrawa@redhat.com>
    Reviewed-on: http://review.gluster.org/15596
    NetBSD-regression: NetBSD Build System <jenkins@build.gluster.org>
    Smoke: Gluster Build System <jenkins@build.gluster.org>
    CentOS-regression: Gluster Build System <jenkins@build.gluster.org>
    Reviewed-by: Jeff Darcy <jdarcy@redhat.com>
    Reviewed-by: Atin Mukherjee <amukherj@redhat.com>
    Reviewed-by: Vijay Bellur <vbellur@redhat.com>

--- rpc/rpc-transport/socket/src/socket.c
+++ rpc/rpc-transport/socket/src/socket.c
@@ -343,14 +343,14 @@ ssl_setup_connection (rpc_transport_t *this, int server)
 	gf_log(this->name,GF_LOG_INFO,"peer CN = %s", peer_CN);
         gf_log (this->name, GF_LOG_INFO,
                 "SSL verification succeeded (client: %s)",
-                this->xl_private?this->xl_private->client_uid:"");
+                this->peerinfo.identifier);
         return gf_strdup(peer_CN);
 
 	/* Error paths. */
 ssl_error:
 	gf_log (this->name, GF_LOG_ERROR,
                 "SSL connect error (client: %s)",
-                this->xl_private?this->xl_private->client_uid:"");
+                this->peerinfo.identifier);
 	ssl_dump_error_stack(this->name);
 free_ssl:
 	SSL_free(priv->ssl_ssl);
