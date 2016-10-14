8d1b94f6e43175df60ca9067db1811997d3ef4fc
Niels de Vos <ndevos@redhat.com>
2016-10-14 12:49:29 +0200

    libglusterfs: add gf_get_mem_type()
    gfapi needs to provide a function towards applications to free memory
    that it allocated. Depending on how the application is compiled/linked,
    it could use a different memory allocator than Gluster itself. Therefore
    it is not safe for gfapi to request applications to free memory with
    'standard' free().
    
    Examples for this are Gluster allocated structures with GF_CALLOC() when
    memory accounting is enabled (the default). Some gfapi functions use
    malloc() to allocate memory as a workaround, but the free() from the
    jemalloc implementation should not be combined with the malloc() from
    glibc.
    
    Cherry picked from commit db4e26ed71a01e5f760fbc3c7051962426f102c9:
    > Change-Id: I626cd1a60abf8965f9263290f4045d1f69fc2093
    > BUG: 1344714
    > Signed-off-by: Niels de Vos <ndevos@redhat.com>
    > Reviewed-on: http://review.gluster.org/15108
    > Smoke: Gluster Build System <jenkins@build.gluster.org>
    > Reviewed-by: soumya k <skoduri@redhat.com>
    > Reviewed-by: jiffin tony Thottan <jthottan@redhat.com>
    > NetBSD-regression: NetBSD Build System <jenkins@build.gluster.org>
    > CentOS-regression: Gluster Build System <jenkins@build.gluster.org>
    > Reviewed-by: Shyamsundar Ranganathan <srangana@redhat.com>
    
    This also includes commit da2aabe3 which fixes an unused variable
    warning.
    
    Change-Id: I626cd1a60abf8965f9263290f4045d1f69fc2093
    BUG: 1347717
    Signed-off-by: Niels de Vos <ndevos@redhat.com>
    Reviewed-on: http://review.gluster.org/15639
    Smoke: Gluster Build System <jenkins@build.gluster.org>
    NetBSD-regression: NetBSD Build System <jenkins@build.gluster.org>
    CentOS-regression: Gluster Build System <jenkins@build.gluster.org>
    Reviewed-by: jiffin tony Thottan <jthottan@redhat.com>

--- libglusterfs/src/mem-pool.h
+++ libglusterfs/src/mem-pool.h
@@ -97,6 +97,8 @@ gf_asprintf (char **string_ptr, const char *format, ...);
 void
 __gf_free (void *ptr);
 
+int
+gf_get_mem_type (void *ptr);
 
 static inline
 void* __gf_default_malloc (size_t size)
