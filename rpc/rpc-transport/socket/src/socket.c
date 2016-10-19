ec64ce2e1684003f4e7a20d4372e414bfbddb6fb
Mohit Agrawal <moagrawa@redhat.com>
2016-10-19 07:27:40 +0530

    rpc/socket.c : Modify socket_poller code in case of ENODATA error code.
    Problem:  Continuous warning message(ENODATA) are coming in socket_rwv
              while SSL is enabled.
    
    Solution: To avoid the warning message update one condition in
              socket_poller loop code before break from loop in case
              of error returned by poll functions.
    
    BUG: 1386450
    Change-Id: I19b3a92d4c3ba380738379f5679c1c354f0ab9b1
    Signed-off-by: Mohit Agrawal <moagrawa@redhat.com>
    Reviewed-on: http://review.gluster.org/15677
    Smoke: Gluster Build System <jenkins@build.gluster.org>
    NetBSD-regression: NetBSD Build System <jenkins@build.gluster.org>
    CentOS-regression: Gluster Build System <jenkins@build.gluster.org>
    Reviewed-by: Raghavendra G <rgowdapp@redhat.com>

--- rpc/rpc-transport/socket/src/socket.c
+++ rpc/rpc-transport/socket/src/socket.c
@@ -2542,7 +2542,7 @@ socket_poller (void *ctx)
 			       "poll error on socket");
 			break;
 		}
-		if (ret < 0 && errno != ENODATA) {
+		if (ret < 0) {
                         GF_LOG_OCCASIONALLY (poll_err_cnt, this->name,
                                              GF_LOG_ERROR,
                                              "socket_poller %s failed (%s)",
