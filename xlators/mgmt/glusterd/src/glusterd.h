2b5f080ba39ff0a7e2b5c3bc554f5680cf925858
Atin Mukherjee <amukherj@redhat.com>
2016-10-21 18:27:45 +0530

    glusterd: use GF_BRICK_STOPPING as intermediate brickinfo->status state
    On a volume stop trigger glusterd issues a brick-op to terminate the brick
    process during brick-op phase , however in the commit-op glusterd once again
    tries to kill the same process if it exists and then mark the brickinfo->status
    flag to GF_BRICK_STOPPED. In the former case, if brick is successfully killed
    there is a possibility that GlusterD will receive RPC_CLNT_DISCONNECT from the
    said brick process before even the commit op phase is executed and hence by that
    time brickinfo->status will still be set to GF_BRICK_STARTED.
    
    BRICK_DISCONNECT event should be only sent if a brick has been killed and not
    through a volume stop/remove brick trigger, however due to this trace, this
    event is also sent out on a volume stop.
    
    Fix is to introduce an intermediate state GF_BRICK_STOPPING which can be used to
    mark the brick status at brick op phase of volume stop/remove brick to avoid
    sending spurious BRICK_DISCONNECT events on a volume stop trigger.
    
    >Reviewed-on: http://review.gluster.org/15699
    >Smoke: Gluster Build System <jenkins@build.gluster.org>
    >NetBSD-regression: NetBSD Build System <jenkins@build.gluster.org>
    >CentOS-regression: Gluster Build System <jenkins@build.gluster.org>
    >Reviewed-by: Kaushal M <kaushal@redhat.com>
    
    Change-Id: Ieed4450e1c988715e0f9958be44faa6b14be81e1
    BUG: 1388461
    Signed-off-by: Atin Mukherjee <amukherj@redhat.com>
    Reviewed-on: http://review.gluster.org/15722
    NetBSD-regression: NetBSD Build System <jenkins@build.gluster.org>
    CentOS-regression: Gluster Build System <jenkins@build.gluster.org>
    Smoke: Gluster Build System <jenkins@build.gluster.org>
    Reviewed-by: Prashanth Pai <ppai@redhat.com>

--- xlators/mgmt/glusterd/src/glusterd.h
+++ xlators/mgmt/glusterd/src/glusterd.h
@@ -189,6 +189,7 @@ typedef struct {
 typedef enum gf_brick_status {
         GF_BRICK_STOPPED,
         GF_BRICK_STARTED,
+        GF_BRICK_STOPPING,
 } gf_brick_status_t;
 
 struct glusterd_brickinfo {
