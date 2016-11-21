47e69455d3aede77960fd81a7cf3d6b4a869dbfa
Rajesh Joseph <rjoseph@redhat.com>
2016-11-22 01:51:19 +0530

    glusterfsd: glusterfs_ctx_defaults_init should not re-initialize ctx->locks
    glusterfs_ctx_new already initialize ctx->locks therefore the second
    initialization in glusterfs_ctx_defaults_init does not make sense.
    
    Change-Id: I6027cbd311da8e80585e0f0dcd6916e3bc8dd284
    BUG: 1397419
    Signed-off-by: Rajesh Joseph <rjoseph@redhat.com>
    Reviewed-on: http://review.gluster.org/15905
    NetBSD-regression: NetBSD Build System <jenkins@build.gluster.org>
    Reviewed-by: Poornima G <pgurusid@redhat.com>
    CentOS-regression: Gluster Build System <jenkins@build.gluster.org>
    Smoke: Gluster Build System <jenkins@build.gluster.org>
    Reviewed-by: Vijay Bellur <vbellur@redhat.com>
    Reviewed-by: Atin Mukherjee <amukherj@redhat.com>

--- glusterfsd/src/glusterfsd.c
+++ glusterfsd/src/glusterfsd.c
@@ -1531,7 +1531,6 @@ glusterfs_ctx_defaults_init (glusterfs_ctx_t *ctx)
         if (!ctx->logbuf_pool)
                 goto out;
 
-        LOCK_INIT (&ctx->lock);
         pthread_mutex_init (&ctx->notify_lock, NULL);
         pthread_cond_init (&ctx->notify_cond, NULL);
 
