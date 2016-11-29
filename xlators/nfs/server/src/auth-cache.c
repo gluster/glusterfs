93c292500dacf6c7611ba37b14650516ef0857ce
Niels de Vos <ndevos@redhat.com>
2016-11-29 16:51:58 +0100

    refcount: return pointer to the structure instead of a counter
    There are no real users of the counter. It was thought of a handy tool
    to track and debug refcounting, but it is not used at all. Some parts of
    the code would benefit from a pointer getting returned instead.
    
    BUG: 1399780
    Change-Id: I97e52c48420fed61be942ea27ff4849b803eed12
    Signed-off-by: Niels de Vos <ndevos@redhat.com>
    Reviewed-on: http://review.gluster.org/15971
    Smoke: Gluster Build System <jenkins@build.gluster.org>
    NetBSD-regression: NetBSD Build System <jenkins@build.gluster.org>
    CentOS-regression: Gluster Build System <jenkins@build.gluster.org>
    Reviewed-by: Xavier Hernandez <xhernandez@datalab.es>
    Reviewed-by: Kaleb KEITHLEY <kkeithle@redhat.com>

--- xlators/nfs/server/src/auth-cache.c
+++ xlators/nfs/server/src/auth-cache.c
@@ -145,8 +145,9 @@ auth_cache_add (struct auth_cache *cache, char *hashkey,
         GF_VALIDATE_OR_GOTO (GF_NFS, cache, out);
         GF_VALIDATE_OR_GOTO (GF_NFS, cache->cache_dict, out);
 
-        ret = GF_REF_GET (entry);
-        if (ret == 0) {
+        /* FIXME: entry is passed as parameter, this can never fail? */
+        entry = GF_REF_GET (entry);
+        if (!entry) {
                 /* entry does not have any references */
                 ret = -1;
                 goto out;
@@ -221,8 +222,9 @@ auth_cache_get (struct auth_cache *cache, char *hashkey,
                 if (!entry_data)
                         goto unlock;
 
-                lookup_res = (struct auth_cache_entry *)(entry_data->data);
-                if (GF_REF_GET (lookup_res) == 0) {
+                /* FIXME: this is dangerous use of entry_data */
+                lookup_res = GF_REF_GET ((struct auth_cache_entry *) entry_data->data);
+                if (lookup_res == NULL) {
                         /* entry has been free'd */
                         ret = ENTRY_EXPIRED;
                         goto unlock;
