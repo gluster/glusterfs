f3778b0b8029fa9ef92607718034e777f8a01104
Kevin Vigor <kvigor@fb.com>
2016-12-15 13:41:14 -0800

    transport: allow OS to assign us a port
    Replace complex and slow port selection code with bind(0) which
    already respects privileged ports.
    
    Change-Id: I408a8528e58e1aafcd32eba6a8f1a759e0bf274e
    BUG: 1405628
    Reviewed-on-release-3.8-fb: http://review.gluster.org/16150
    Signed-off-by: Jeff Darcy <jdarcy@redhat.com>
    Reviewed-on: https://review.gluster.org/16178
    Smoke: Gluster Build System <jenkins@build.gluster.org>
    Reviewed-by: Niels de Vos <ndevos@redhat.com>
    NetBSD-regression: NetBSD Build System <jenkins@build.gluster.org>
    CentOS-regression: Gluster Build System <jenkins@build.gluster.org>
    Reviewed-by: Vijay Bellur <vbellur@redhat.com>

--- rpc/rpc-transport/socket/src/name.c
+++ rpc/rpc-transport/socket/src/name.c
@@ -42,6 +42,10 @@ static int32_t
 af_inet_bind_to_port_lt_ceiling (int fd, struct sockaddr *sockaddr,
                                  socklen_t sockaddr_len, uint32_t ceiling)
 {
+#if GF_DISABLE_PRIVPORT_TRACKING
+        _assign_port (sockaddr, 0);
+        return bind (fd, sockaddr, sockaddr_len);
+#else
         int32_t         ret                             = -1;
         uint16_t        port                            = ceiling - 1;
         unsigned char   ports[GF_PORT_ARRAY_SIZE]       = {0,};
@@ -88,6 +92,7 @@ loop:
         }
 
         return ret;
+#endif /* GF_DISABLE_PRIVPORT_TRACKING */
 }
 
 static int32_t
