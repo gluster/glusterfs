83abcba6b42f94eb5a6495a634d4055362a9d79d
Kaleb S. KEITHLEY <kkeithle@redhat.com>
2017-03-27 16:09:56 -0400

    build: conditionally build legacy gNFS server and associated sub-packaging
    Plus some additional logic in glusterd to ensure gnfs (glusterfs)
    daemons are never started if server/nfs xlator is not installed.
    
    As a service, nfs is still initialized. The glusterfs-gnfs RPM
    may be installed or uninstalled independent of anything else,
    including on a system where gluster is actively running, so the
    existence of the xlator is always tested before trying to start
    gnfs.
    
    Change-Id: I56743ad1cb36a84917226d7d26cb9d015d441e66
    BUG: 1326219
    Signed-off-by: Kaleb S. KEITHLEY <kkeithle@redhat.com>
    Reviewed-on: https://review.gluster.org/16958
    Smoke: Gluster Build System <jenkins@build.gluster.org>
    CentOS-regression: Gluster Build System <jenkins@build.gluster.org>
    NetBSD-regression: NetBSD Build System <jenkins@build.gluster.org>

--- xlators/mgmt/glusterd/src/glusterd-svc-mgmt.h
+++ xlators/mgmt/glusterd/src/glusterd-svc-mgmt.h
@@ -29,7 +29,6 @@ struct glusterd_svc_ {
         char                      name[PATH_MAX];
         glusterd_conn_t           conn;
         glusterd_proc_t           proc;
-        glusterd_svc_build_t      build;
         glusterd_svc_manager_t    manager;
         glusterd_svc_start_t      start;
         glusterd_svc_stop_t       stop;
