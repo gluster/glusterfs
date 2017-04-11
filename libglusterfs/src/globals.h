6b8df081b46ac4f485c86a5052fc30472e74bfbb
Milind Changire <mchangir@redhat.com>
2017-04-11 12:30:06 +0530

    rpc: add options to manage socket keepalive lifespan
    Problem:
    Default values for handling socket timeouts for brick responses are
    insufficient for aggressive applications such as databases.
    
    Solution:
    Add 1:1 gluster options for keepalive, keepalive-idle,
    keepalive-interval and keepalive-timeout as per the socket level options
    available as per tcp(7) man page.
    
    Default values for options are NOT agressive and continue to be values
    which result in default timeout when only the keep alive option is
    turned on.
    
    These options are Linux specific and will not be applicable to the
    *BSDs.
    
    Change-Id: I2a08ecd949ca8ceb3e090d336ad634341e2dbf14
    BUG: 1426059
    Signed-off-by: Milind Changire <mchangir@redhat.com>
    Reviewed-on: https://review.gluster.org/16731
    Smoke: Gluster Build System <jenkins@build.gluster.org>
    CentOS-regression: Gluster Build System <jenkins@build.gluster.org>
    NetBSD-regression: NetBSD Build System <jenkins@build.gluster.org>
    Reviewed-by: Raghavendra G <rgowdapp@redhat.com>

--- libglusterfs/src/globals.h
+++ libglusterfs/src/globals.h
@@ -80,6 +80,8 @@
 
 #define GD_OP_VERSION_3_10_1   31001 /* Op-version for GlusterFS 3.10.1 */
 
+#define GD_OP_VERSION_3_10_2   31002 /* Op-version for GlusterFS 3.10.2 */
+
 #define GD_OP_VERSION_4_0_0    40000 /* Op-version for GlusterFS 4.0.0 */
 
 #define GD_OP_VER_PERSISTENT_AFR_XATTRS GD_OP_VERSION_3_6_0
