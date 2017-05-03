e313450978f4ffa1dfdbdd78a18b62625e250603
Kaushal M <kaushal@redhat.com>
2017-05-03 13:07:31 +0530

    libglusterfs: Correct op-version for 3.11
    Change-Id: Ida58626b3c25ed97f3b0b94b3fa5be58d607fede
    BUG: 1447543
    Signed-off-by: Kaushal M <kaushal@redhat.com>
    Reviewed-on: https://review.gluster.org/17155
    Smoke: Gluster Build System <jenkins@build.gluster.org>
    Reviewed-by: Niels de Vos <ndevos@redhat.com>
    NetBSD-regression: NetBSD Build System <jenkins@build.gluster.org>
    CentOS-regression: Gluster Build System <jenkins@build.gluster.org>

--- libglusterfs/src/globals.h
+++ libglusterfs/src/globals.h
@@ -38,7 +38,7 @@
  */
 #define GD_OP_VERSION_MIN  1 /* MIN is the fresh start op-version, mostly
                                 should not change */
-#define GD_OP_VERSION_MAX  GD_OP_VERSION_4_0_0 /* MAX VERSION is the maximum
+#define GD_OP_VERSION_MAX  GD_OP_VERSION_3_11_0 /* MAX VERSION is the maximum
                                                   count in VME table, should
                                                   keep changing with
                                                   introduction of newer
@@ -83,8 +83,6 @@
 #define GD_OP_VERSION_3_10_2   31002 /* Op-version for GlusterFS 3.10.2 */
 
 #define GD_OP_VERSION_3_11_0   31100 /* Op-version for GlusterFS 3.11.0 */
-
-#define GD_OP_VERSION_4_0_0    40000 /* Op-version for GlusterFS 4.0.0 */
 
 #define GD_OP_VER_PERSISTENT_AFR_XATTRS GD_OP_VERSION_3_6_0
 
