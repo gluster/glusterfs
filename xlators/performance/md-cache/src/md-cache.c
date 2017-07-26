1510feab499c5d43d73147213714dda6de340b2d
Günther Deschner <gd@samba.org>
2017-07-26 18:10:33 +0200

    md-cache: Use correct xattr keynames for virtual glusterfs ACLs.
    The "glusterfs.posix_acl." prefix does not catch the glusterfs posix acl
    xattr keynames which are
    
    	* "glusterfs.posix.acl" and
    	* "glusterfs.posix.default_acl"
    
    Using the GF_POSIX_ACL_ACCESS and GF_POSIX_ACL_DEFAULT defines directly
    is the savest option.
    
    Guenther
    
    Change-Id: I5aba64b26b6cbec850ea02316dd9f069400e857f
    BUG: 1476295
    Signed-off-by: Guenther Deschner <gd@samba.org>
    Reviewed-on: https://review.gluster.org/17909
    Reviewed-by: Michael Adam <obnox@samba.org>
    Smoke: Gluster Build System <jenkins@build.gluster.org>
    Reviewed-by: Niels de Vos <ndevos@redhat.com>
    Tested-by: Niels de Vos <ndevos@redhat.com>
    CentOS-regression: Gluster Build System <jenkins@build.gluster.org>

--- xlators/performance/md-cache/src/md-cache.c
+++ xlators/performance/md-cache/src/md-cache.c
@@ -2891,7 +2891,10 @@ reconfigure (xlator_t *this, dict_t *options)
 
 	GF_OPTION_RECONF ("cache-posix-acl", conf->cache_posix_acl, options, bool, out);
 	mdc_key_load_set (mdc_keys, "system.posix_acl_", conf->cache_posix_acl);
-	mdc_key_load_set (mdc_keys, "glusterfs.posix_acl.", conf->cache_posix_acl);
+	mdc_key_load_set (mdc_keys, GF_POSIX_ACL_ACCESS,
+			  conf->cache_posix_acl);
+	mdc_key_load_set (mdc_keys, GF_POSIX_ACL_DEFAULT,
+			  conf->cache_posix_acl);
 
         GF_OPTION_RECONF ("cache-swift-metadata", conf->cache_swift_metadata,
                           options, bool, out);
@@ -2962,7 +2965,10 @@ init (xlator_t *this)
 
 	GF_OPTION_INIT ("cache-posix-acl", conf->cache_posix_acl, bool, out);
 	mdc_key_load_set (mdc_keys, "system.posix_acl_", conf->cache_posix_acl);
-	mdc_key_load_set (mdc_keys, "glusterfs.posix_acl.", conf->cache_posix_acl);
+	mdc_key_load_set (mdc_keys, GF_POSIX_ACL_ACCESS,
+			  conf->cache_posix_acl);
+	mdc_key_load_set (mdc_keys, GF_POSIX_ACL_DEFAULT,
+			  conf->cache_posix_acl);
 
         GF_OPTION_INIT ("cache-swift-metadata",
                         conf->cache_swift_metadata, bool, out);
