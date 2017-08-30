24b95089a18a6a40e7703cb344e92025d67f3086
Milind Changire <mchangir@redhat.com>
2017-08-30 11:25:29 +0530

    rpc: destroy transport after client_t
    Problem:
    1. Ref counting increment on the client_t object is done in
       rpcsvc_request_init() which is incorrect.
    2. Ref not taken when delegating to grace_time_handler()
    
    Solution:
    1. Only fop requests which require processing down the graph via
       stack 'frames' now ref count the request in get_frame_from_request()
    2. Take ref on client_t object in server_rpc_notify() but avoid
       dropping in RPCSVC_EVENT_TRANSPORT_DESRTROY. Drop the ref
       unconditionally when exiting out of grace_time_handler().
       Also, avoid dropping ref on client_t in
       RPCSVC_EVENT_TRANSPORT_DESTROY when ref mangement as been
       delegated to grace_time_handler()
    
    Change-Id: Ic16246bebc7ea4490545b26564658f4b081675e4
    BUG: 1481600
    Reported-by: Raghavendra G <rgowdapp@redhat.com>
    Signed-off-by: Milind Changire <mchangir@redhat.com>
    Reviewed-on: https://review.gluster.org/17982
    Tested-by: Raghavendra G <rgowdapp@redhat.com>
    Reviewed-by: Raghavendra G <rgowdapp@redhat.com>
    CentOS-regression: Gluster Build System <jenkins@build.gluster.org>
    Smoke: Gluster Build System <jenkins@build.gluster.org>

--- rpc/rpc-lib/src/rpcsvc.c
+++ rpc/rpc-lib/src/rpcsvc.c
@@ -411,9 +411,6 @@ rpcsvc_request_init (rpcsvc_t *svc, rpc_transport_t *trans,
         req->progver = rpc_call_progver (callmsg);
         req->procnum = rpc_call_progproc (callmsg);
         req->trans = rpc_transport_ref (trans);
-        if (req->trans->xl_private) {
-                gf_client_ref (req->trans->xl_private);
-        }
         req->count = msg->count;
         req->msg[0] = progmsg;
         req->iobref = iobref_ref (msg->iobref);
