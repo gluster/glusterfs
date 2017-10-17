bd4048f91a7aa701f18f777eb897437951ee0682
Kotresh HR <khiremat@redhat.com>
2017-10-17 11:28:43 -0400

    glusterd: Validate changelog on geo-rep volume
    If geo-rep is configured on volume, don't allow
    to disable changelog.
    
    Change-Id: I7d1ba8b2939c8fe6ee6c59fb923d9aa1bdab553c
    BUG: 1503227
    Signed-off-by: Kotresh HR <khiremat@redhat.com>

--- xlators/mgmt/glusterd/src/glusterd-messages.h
+++ xlators/mgmt/glusterd/src/glusterd-messages.h
@@ -41,7 +41,7 @@
 
 #define GLUSTERD_COMP_BASE      GLFS_MSGID_GLUSTERD
 
-#define GLFS_NUM_MESSAGES       612
+#define GLFS_NUM_MESSAGES       613
 
 #define GLFS_MSGID_END          (GLUSTERD_COMP_BASE + GLFS_NUM_MESSAGES + 1)
 /* Messaged with message IDs */
@@ -4944,6 +4944,14 @@
  *
  */
 #define GD_MSG_PORTS_EXHAUSTED (GLUSTERD_COMP_BASE + 612)
+
+/*!
+ * @messageid
+ * @diagnosis
+ * @recommendedaction
+ *
+ */
+#define GD_MSG_CHANGELOG_GET_FAIL (GLUSTERD_COMP_BASE + 613)
 
 /*------------*/
 
