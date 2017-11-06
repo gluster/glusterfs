2beb3177b804e5316cc17618f1fa7c8e1ad2ebe8
ShyamsundarR <srangana@redhat.com>
2017-11-06 09:57:05 -0500

    libglusterfs: Correct opversion to reflect 3.13
    Change-Id: I55c7be68d2aa498d7f7931eb204d678b23e4979c
    BUG: 1510022
    Signed-off-by: ShyamsundarR <srangana@redhat.com>

--- libglusterfs/src/globals.h
+++ libglusterfs/src/globals.h
@@ -38,7 +38,7 @@
  */
 #define GD_OP_VERSION_MIN  1 /* MIN is the fresh start op-version, mostly
                                 should not change */
-#define GD_OP_VERSION_MAX  GD_OP_VERSION_4_0_0 /* MAX VERSION is the maximum
+#define GD_OP_VERSION_MAX  GD_OP_VERSION_3_13_0 /* MAX VERSION is the maximum
                                                   count in VME table, should
                                                   keep changing with
                                                   introduction of newer
@@ -91,8 +91,6 @@
 #define GD_OP_VERSION_3_12_2   31202 /* Op-version for GlusterFS 3.12.2 */
 
 #define GD_OP_VERSION_3_13_0   31300 /* Op-version for GlusterFS 3.13.0 */
-
-#define GD_OP_VERSION_4_0_0    40000 /* Op-version for GlusterFS 4.0.0 */
 
 #define GD_OP_VER_PERSISTENT_AFR_XATTRS GD_OP_VERSION_3_6_0
 
