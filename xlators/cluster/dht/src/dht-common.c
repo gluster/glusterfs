gluster/glusterfs

commit d9f773ba719397c12860f494a8cd38109e4b2fe3

cluster/dht: Fixed leak in dht_populate_inode_for_dentry
Fixed an issue in dht_populate_inode_for_dentry where a layout is
set in the inode without checking if it is already set. This overwrites
the value each time without freeing the already existing layout.

Change-Id: I651bf539a0b82b4ddc4c355890c16a8e91f5f1fd
BUG: 1541264
Signed-off-by: N Balachandran <nbalacha@redhat.com>

xlators/cluster/dht/src/dht-common.c
@@ -6337,15 +6337,20 @@ dht_populate_inode_for_dentry (xlator_t *this, xlator_t *subvol,
                 return;
         }
 
+        gf_uuid_copy (loc.gfid, orig_entry->d_stat.ia_gfid);
+        loc.inode = inode_ref (orig_entry->inode);
+
+        if (is_revalidate (&loc)) {
+                loc_wipe (&loc);
+                return;
+        }
+
         layout = dht_layout_new (this, 1);
         if (!layout)
                 goto out;
 
         ret = dht_layout_merge (this, layout, subvol, 0, 0, orig_entry->dict);
         if (!ret) {
-                gf_uuid_copy (loc.gfid, orig_entry->d_stat.ia_gfid);
-                loc.inode = inode_ref (orig_entry->inode);
-
                 ret = dht_layout_normalize (this, &loc, layout);
                 if (ret == 0) {
                         dht_layout_set (this, orig_entry->inode, layout);
@@ -6507,6 +6512,7 @@ list:
                                                                        entry,
                                                                        orig_entry);
                                 }
+
                         }
                 } else {
                         if (orig_entry->inode) {

xlators/cluster/dht/src/dht-common.h
@@ -751,7 +751,7 @@ typedef struct dht_fd_ctx {
 
 #define ENTRY_MISSING(op_ret, op_errno) (op_ret == -1 && op_errno == ENOENT)
 
-#define is_revalidate(loc) (dht_inode_ctx_layout_get (loc->inode, this, NULL) == 0)
+#define is_revalidate(loc) (dht_inode_ctx_layout_get ((loc)->inode, this, NULL) == 0)
 
 #define is_last_call(cnt) (cnt == 0)
 

commit 896dcf284160706e868464df8bc78855b58dbdae

cluster/dht: Fixed a leak in inode_ref
Introduced by commit d9f773ba719397c128

Change-Id: I3f3103a5a80daed7562ace72e5aa53b77e74fb94
BUG: 1541264
Signed-off-by: N Balachandran <nbalacha@redhat.com>

xlators/cluster/dht/src/dht-common.c
@@ -6341,8 +6341,7 @@ dht_populate_inode_for_dentry (xlator_t *this, xlator_t *subvol,
         loc.inode = inode_ref (orig_entry->inode);
 
         if (is_revalidate (&loc)) {
-                loc_wipe (&loc);
-                return;
+                goto out;
         }
 
         layout = dht_layout_new (this, 1);
@@ -6358,13 +6357,13 @@ dht_populate_inode_for_dentry (xlator_t *this, xlator_t *subvol,
                         layout = NULL;
                 }
 
-                loc_wipe (&loc);
         }
 
         if (layout)
                 dht_layout_unref (this, layout);
 
 out:
+        loc_wipe (&loc);
         return;
 }
 