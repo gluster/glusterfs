35da87e24132e38feeca8234757ee9d6d1fa40ff
Amar Tumballi <amarts@redhat.com>
2018-02-13 06:33:53 +0530

    posix: fix the syncop_writev() arguments
    Due to merging https://review.gluster.org/#/c/19131 before
    https://review.gluster.org/18804 the build started failing.
    
    Fixing with this patch.
    
    Change-Id: Ia1eedf481a0f6d25f1357ee56d6bbb7a7e9d9793
    Signed-off-by: Amar Tumballi <amarts@redhat.com>

--- xlators/storage/posix/src/posix-entry-ops.c
+++ xlators/storage/posix/src/posix-entry-ops.c
@@ -2222,7 +2222,7 @@ posix_put (call_frame_t *frame, xlator_t *this, loc_t *loc,
         }
 
         op_ret = syncop_writev (this, fd, vector, count, offset, iobref,
-                                flags, xdata, NULL);
+                                flags, NULL, NULL, xdata, NULL);
         if (op_ret < 0) {
                 op_errno = errno;
                 gf_msg (this->name, GF_LOG_ERROR, errno, P_MSG_WRITE_FAILED,
