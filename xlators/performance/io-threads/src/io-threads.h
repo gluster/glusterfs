e2766c32631d27519e688ebec48c408b36e3cc79
Varsha Rao <varao@redhat.com>
2018-02-27 15:58:56 +0530

    perfomance/io-threads: Add option to disable client disconnect feature
    > Add options to disable new features
    > Commit ID: c071992e8d
    > https://review.gluster.org/#/c/18291/
    > By Michael Goulet <mgoulet@fb.com>
    
    This patch is required to forward port io-threads namespace patch.
    Updates: #401
    
    Change-Id: Ice477fdf4b8934f9fac0b4a2f6c93db97429a586
    Signed-off-by: Varsha Rao <varao@redhat.com>

--- xlators/performance/io-threads/src/io-threads.h
+++ xlators/performance/io-threads/src/io-threads.h
@@ -79,6 +79,7 @@ struct iot_conf {
         gf_boolean_t        watchdog_running;
         pthread_t           watchdog_thread;
         gf_boolean_t        queue_marked[GF_FOP_PRI_MAX];
+        gf_boolean_t        cleanup_disconnected_reqs;
 };
 
 typedef struct iot_conf iot_conf_t;
