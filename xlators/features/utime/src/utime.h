487aaafacac8af16c08eb44fe9bf178768d372fa
Kotresh HR <khiremat@redhat.com>
2018-04-30 04:43:58 -0400

    utime: ctime client side xlator
    The client side utime xlator does two things.
    
    1. Update unix epoch time in frame->root->ctime
    2. Update the frame->root->flags based on the fop
       which indicates time attributes that should be
       updated for the parent/entry.
    
    Credits: Rafi KC <rkavunga@redhat.com>
    Updates: #208
    Change-Id: I9cad297040c70798a0a8468a080eb4aeff73138d
    Signed-off-by: Kotresh HR <khiremat@redhat.com>

---
+++ xlators/features/utime/src/utime.h
@@ -0,0 +1,19 @@
+/*
+  Copyright (c) 2018 Red Hat, Inc. <http://www.redhat.com>
+  This file is part of GlusterFS.
+
+  This file is licensed to you under your choice of the GNU Lesser
+  General Public License, version 3 or any later version (LGPLv3 or
+  later), or the GNU General Public License, version 2 (GPLv2), in all
+  cases as published by the Free Software Foundation.
+*/
+
+#ifndef __UTIME_H__
+#define __UTIME_H__
+
+#include "glusterfs.h"
+#include "xlator.h"
+#include "defaults.h"
+#include "utime-autogen-fops.h"
+
+#endif /* __UTIME_H__ */
