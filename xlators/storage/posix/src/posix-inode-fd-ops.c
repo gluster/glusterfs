b21b83af0baab3c651b6fd2e4657ca66080a7bcb
Mohit Agrawal <moagrawa@redhat.com>
2018-05-30 15:09:29 +0530

    dht: Delete MDS internal xattr from dict in dht_getxattr_cbk
    Problem: At the time of fetching xattr to heal xattr by afr
             it is not able to fetch xattr because posix_getxattr
             has a check to ignore if xattr name is MDS
    
    Solution: To ignore same xattr update a check in dht_getxattr_cbk
              instead of having a check in posix_getxattr
    
    Backport of:
     > BUG: 1584098
     > Change-Id: I86cd2b2ee08488cb6c12f407694219d57c5361dc
     > Signed-off-by: Mohit Agrawal <moagrawa@redhat.com>
    
    Change-Id: I86cd2b2ee08488cb6c12f407694219d57c5361dc
    fixes: bz#1611116
    Signed-off-by: Mohit Agrawal <moagrawa@redhat.com>

--- xlators/storage/posix/src/posix-inode-fd-ops.c
+++ xlators/storage/posix/src/posix-inode-fd-ops.c
@@ -2863,26 +2863,6 @@ out:
         return ret;
 }
 
-gf_boolean_t
-posix_is_mds_xattr (const char *name)
-{
-        regex_t regcmpl;
-        char *key = {"trusted.glusterfs.*.mds$"};
-        regmatch_t result[1] = {{0} };
-        gf_boolean_t status = _gf_false;
-
-        if (regcomp (&regcmpl, key, REG_EXTENDED)) {
-                goto out;
-        }
-        if (!regexec (&regcmpl, name, 1, result, 0)) {
-                status = _gf_true;
-                goto out;
-        }
-out:
-        regfree(&regcmpl);
-        return status;
-}
-
 
 /**
  * posix_getxattr - this function returns a dictionary with all the
@@ -2943,13 +2923,6 @@ posix_getxattr (call_frame_t *frame, xlator_t *this,
                 op_errno = ENOATTR;
                 goto out;
         }
-
-        if (name && posix_is_mds_xattr (name)) {
-                op_ret = -1;
-                op_errno = ENOATTR;
-                goto out;
-        }
-
 
         if (loc->inode && IA_ISDIR(loc->inode->ia_type) && name &&
             ZR_FILE_CONTENT_REQUEST(name)) {
@@ -3330,10 +3303,6 @@ posix_getxattr (call_frame_t *frame, xlator_t *this,
                 }
 
                 if (posix_is_gfid2path_xattr (keybuffer)) {
-                        goto ignore;
-                }
-
-                if (posix_is_mds_xattr (keybuffer)) {
                         goto ignore;
                 }
 
