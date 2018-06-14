gluster/glusterfs

commit 20fa80057eb430fd72b4fa31b9b65598b8ec1265

afr: heal gfid as a part of entry heal
Problem:
If a brick crashes after an entry (file or dir) is created but before
gfid is assigned, the good bricks will have pending entry heal xattrs
but the heal won't complete because afr_selfheal_recreate_entry() tries
to create the entry again and it fails with EEXIST.

Fix:
We could have fixed posx_mknod/mkdir etc to assign the gfid if the file
already exists but the right thing to do seems to be to trigger a lookup
on the bad brick and let it heal the gfid instead of winding an
mknod/mkdir in the first place.

Change-Id: I82f76665a7541f1893ef8d847b78af6466aff1ff
BUG: 1493415
Signed-off-by: Ravishankar N <ravishankar@redhat.com>

xlators/cluster/afr/src/afr-self-heal-common.c
@@ -20,6 +20,84 @@ void
 afr_heal_synctask (xlator_t *this, afr_local_t *local);
 
 int
+afr_lookup_and_heal_gfid (xlator_t *this, inode_t *parent, const char *name,
+                          inode_t *inode, struct afr_reply *replies,
+                          int source,  void *gfid)
+{
+        afr_private_t *priv = NULL;
+        call_frame_t   *frame    = NULL;
+        afr_local_t *local = NULL;
+        unsigned char *wind_on = NULL;
+        ia_type_t ia_type = IA_INVAL;
+        dict_t *xdata = NULL;
+        loc_t loc = {0, };
+        int ret = 0;
+        int i = 0;
+
+        priv = this->private;
+        wind_on = alloca0 (priv->child_count);
+        ia_type = replies[source].poststat.ia_type;
+
+        /* gfid heal on those subvolumes that do not have gfid associated
+         * with the inode and update those replies.
+         */
+        for (i = 0; i < priv->child_count; i++) {
+                if (!replies[i].valid || replies[i].op_ret != 0)
+                        continue;
+                if (!gf_uuid_is_null (replies[i].poststat.ia_gfid) ||
+                    replies[i].poststat.ia_type != ia_type)
+                        continue;
+
+                wind_on[i] = 1;
+        }
+
+        if (AFR_COUNT(wind_on, priv->child_count) == 0)
+                return 0;
+
+        xdata = dict_new ();
+        if (!xdata) {
+                ret = -ENOMEM;
+                goto out;
+        }
+
+        ret = dict_set_static_bin (xdata, "gfid-req", gfid, 16);
+        if (ret) {
+                ret = -ENOMEM;
+                goto out;
+        }
+
+        frame = afr_frame_create (this);
+        if (!frame) {
+                ret = -ENOMEM;
+                goto out;
+        }
+
+        local = frame->local;
+        loc.parent = inode_ref (parent);
+        gf_uuid_copy (loc.pargfid, parent->gfid);
+        loc.name = name;
+        loc.inode = inode_ref (inode);
+
+        AFR_ONLIST (wind_on, frame, afr_selfheal_discover_cbk, lookup,
+                    &loc, xdata);
+
+        for (i = 0; i < priv->child_count; i++) {
+                if (!wind_on[i])
+                        continue;
+                afr_reply_wipe (&replies[i]);
+                afr_reply_copy (&replies[i], &local->replies[i]);
+        }
+out:
+        loc_wipe (&loc);
+        if (frame)
+                AFR_STACK_DESTROY (frame);
+        if (xdata)
+                dict_unref (xdata);
+
+        return ret;
+}
+
+int
 afr_gfid_sbrain_source_from_src_brick (xlator_t *this,
                                        struct afr_reply *replies,
                                        char *src_brick)

xlators/cluster/afr/src/afr-self-heal-entry.c
@@ -64,7 +64,6 @@ afr_selfheal_entry_delete (xlator_t *this, inode_t *dir, const char *name,
 	return ret;
 }
 
-
 int
 afr_selfheal_recreate_entry (call_frame_t *frame, int dst, int source,
                              unsigned char *sources, inode_t *dir,
@@ -164,7 +163,6 @@ out:
 	return ret;
 }
 
-
 static int
 __afr_selfheal_heal_dirent (call_frame_t *frame, xlator_t *this, fd_t *fd,
 			    char *name, inode_t *inode, int source,
@@ -186,6 +184,14 @@ __afr_selfheal_heal_dirent (call_frame_t *frame, xlator_t *this, fd_t *fd,
         if ((replies[source].op_ret < 0) &&
             (replies[source].op_errno != ENOENT))
                 return -replies[source].op_errno;
+
+        if (replies[source].op_ret == 0) {
+                ret = afr_lookup_and_heal_gfid (this, fd->inode, name,
+                                                inode, replies, source,
+                                             &replies[source].poststat.ia_gfid);
+                if (ret)
+                        return ret;
+        }
 
 	for (i = 0; i < priv->child_count; i++) {
 		if (!healed_sinks[i])
@@ -223,8 +229,12 @@ afr_selfheal_detect_gfid_and_type_mismatch (xlator_t *this,
         int             i        = 0;
         int             ret      = -1;
         afr_private_t  *priv     = NULL;
+        void *gfid               = NULL;
+        ia_type_t ia_type        = IA_INVAL;
 
         priv = this->private;
+        gfid = &replies[src_idx].poststat.ia_gfid;
+        ia_type = replies[src_idx].poststat.ia_type;
 
         for (i = 0; i < priv->child_count; i++) {
                 if (i == src_idx)
@@ -236,8 +246,8 @@ afr_selfheal_detect_gfid_and_type_mismatch (xlator_t *this,
                 if (replies[i].op_ret != 0)
                         continue;
 
-                if (gf_uuid_compare (replies[src_idx].poststat.ia_gfid,
-                                     replies[i].poststat.ia_gfid)) {
+                if (gf_uuid_compare (gfid, replies[i].poststat.ia_gfid) &&
+                    (ia_type == replies[i].poststat.ia_type)) {
                         ret = afr_gfid_split_brain_source (this, replies, inode,
                                                            pargfid, bname,
                                                            src_idx, i,
@@ -251,8 +261,7 @@ afr_selfheal_detect_gfid_and_type_mismatch (xlator_t *this,
                         return ret;
                 }
 
-                if ((replies[src_idx].poststat.ia_type) !=
-                    (replies[i].poststat.ia_type)) {
+                if (ia_type != replies[i].poststat.ia_type) {
                         gf_msg (this->name, GF_LOG_ERROR, 0,
                                 AFR_MSG_SPLIT_BRAIN, "Type mismatch detected "
                                 "for <gfid:%s>/%s>, %s on %s and %s on %s. "
@@ -309,6 +318,12 @@ __afr_selfheal_merge_dirent (call_frame_t *frame, xlator_t *this, fd_t *fd,
 			sources[i] = 1;
 		}
 	}
+
+        ret = afr_lookup_and_heal_gfid (this, fd->inode, name, inode, replies,
+                                        source,
+                                        &replies[source].poststat.ia_gfid);
+        if (ret)
+                return ret;
 
         /* In case of type mismatch / unable to resolve gfid mismatch on the
          * entry, return -1.*/
@@ -542,8 +557,18 @@ afr_selfheal_entry_dirent (call_frame_t *frame, xlator_t *this,
         struct afr_reply  *replies      = NULL;
         struct afr_reply  *par_replies  = NULL;
         afr_private_t     *priv         = NULL;
+        dict_t            *xattr        = NULL;
 
 	priv = this->private;
+
+        xattr = dict_new ();
+        if (!xattr)
+                return -ENOMEM;
+        ret = dict_set_int32 (xattr, GF_GFIDLESS_LOOKUP, 1);
+        if (ret) {
+                dict_unref (xattr);
+                return -1;
+        }
 
         sources = alloca0 (priv->child_count);
         sinks   = alloca0 (priv->child_count);
@@ -576,7 +601,7 @@ afr_selfheal_entry_dirent (call_frame_t *frame, xlator_t *this,
 
 		inode = afr_selfheal_unlocked_lookup_on (frame, fd->inode, name,
 							 replies, locked_on,
-                                                         NULL);
+                                                         xattr);
 		if (!inode) {
 			ret = -ENOMEM;
 			goto unlock;
@@ -608,6 +633,8 @@ unlock:
                 afr_replies_wipe (replies, priv->child_count);
         if (par_replies)
                 afr_replies_wipe (par_replies, priv->child_count);
+        if (xattr)
+                dict_unref (xattr);
 
 	return ret;
 }

xlators/cluster/afr/src/afr-self-heal-name.c
@@ -18,49 +18,17 @@ int
 __afr_selfheal_assign_gfid (xlator_t *this, inode_t *parent, uuid_t pargfid,
                             const char *bname, inode_t *inode,
                             struct afr_reply *replies, void *gfid,
-                            unsigned char *locked_on,
+                            unsigned char *locked_on, int source,
                             gf_boolean_t is_gfid_absent)
 {
 	int             ret          = 0;
         int             up_count     = 0;
         int             locked_count = 0;
-        int             i            = 0;
 	afr_private_t  *priv         = NULL;
-	dict_t         *xdata        = NULL;
-	loc_t           loc          = {0, };
-        call_frame_t   *new_frame    = NULL;
-        afr_local_t    *new_local    = NULL;
-        unsigned char  *wind_on      = NULL;
 
 	priv = this->private;
-        wind_on = alloca0 (priv->child_count);
-
-        new_frame = afr_frame_create (this);
-        if (!new_frame) {
-                ret = -ENOMEM;
-                goto out;
-        }
-
-        new_local = new_frame->local;
 
 	gf_uuid_copy (parent->gfid, pargfid);
-
-	xdata = dict_new ();
-	if (!xdata) {
-                ret = -ENOMEM;
-		goto out;
-	}
-
-	ret = dict_set_static_bin (xdata, "gfid-req", gfid, 16);
-	if (ret) {
-		ret = -ENOMEM;
-		goto out;
-	}
-
-	loc.parent = inode_ref (parent);
-	loc.inode = inode_ref (inode);
-	gf_uuid_copy (loc.pargfid, pargfid);
-	loc.name = bname;
 
         if (is_gfid_absent) {
                 /* Ensure all children of AFR are up before performing gfid heal, to
@@ -79,34 +47,10 @@ __afr_selfheal_assign_gfid (xlator_t *this, inode_t *parent, uuid_t pargfid,
                 }
         }
 
-        /* gfid heal on those subvolumes that do not have gfid associated
-         * with the inode and update those replies.
-         */
-        for (i = 0; i < priv->child_count; i++) {
-                if (replies[i].valid && replies[i].op_ret == 0 &&
-                    !gf_uuid_is_null (replies[i].poststat.ia_gfid) &&
-                    !gf_uuid_compare (replies[i].poststat.ia_gfid, gfid))
-                        continue;
-                wind_on[i] = 1;
-        }
-
-        AFR_ONLIST (wind_on, new_frame, afr_selfheal_discover_cbk, lookup,
-                    &loc, xdata);
-
-        for (i = 0; i < priv->child_count; i++) {
-                if (!wind_on[i])
-                        continue;
-                afr_reply_wipe (&replies[i]);
-                afr_reply_copy (&replies[i], &new_local->replies[i]);
-        }
+        afr_lookup_and_heal_gfid (this, parent, bname, inode, replies, source,
+                                  gfid);
 
 out:
-	loc_wipe (&loc);
-        if (xdata)
-                dict_unref (xdata);
-        if (new_frame)
-                AFR_STACK_DESTROY (new_frame);
-
 	return ret;
 }
 
@@ -481,7 +425,7 @@ __afr_selfheal_name_do (call_frame_t *frame, xlator_t *this, inode_t *parent,
 
         is_gfid_absent = (gfid_idx == -1) ? _gf_true : _gf_false;
 	ret = __afr_selfheal_assign_gfid (this, parent, pargfid, bname, inode,
-                                          replies, gfid, locked_on,
+                                          replies, gfid, locked_on, source,
                                           is_gfid_absent);
         if (ret)
                 return ret;

xlators/cluster/afr/src/afr-self-heal.h
@@ -110,6 +110,10 @@ afr_selfheal_metadata (call_frame_t *frame, xlator_t *this, inode_t *inode);
 int
 afr_selfheal_entry (call_frame_t *frame, xlator_t *this, inode_t *inode);
 
+int
+afr_lookup_and_heal_gfid (xlator_t *this, inode_t *parent, const char *name,
+                          inode_t *inode, struct afr_reply *replies, int source,
+                          void *gfid);
 
 int
 afr_selfheal_inodelk (call_frame_t *frame, xlator_t *this, inode_t *inode,

tests/bugs/replicate/bug-1493415-gfid-heal.t
@@ -1,0 +1,68 @@
+#!/bin/bash
+. $(dirname $0)/../../include.rc
+. $(dirname $0)/../../volume.rc
+. $(dirname $0)/../../afr.rc
+cleanup;
+
+TEST glusterd
+TEST pidof glusterd
+TEST $CLI volume create $V0 replica 2 $H0:$B0/${V0}{0,1}
+TEST $CLI volume start $V0
+
+TEST $GFS --volfile-id=$V0 --volfile-server=$H0 --attribute-timeout=0 --entry-timeout=0 $M0;
+EXPECT_WITHIN $CHILD_UP_TIMEOUT "1" afr_child_up_status $V0 0
+EXPECT_WITHIN $CHILD_UP_TIMEOUT "1" afr_child_up_status $V0 1
+TEST $CLI volume set $V0 self-heal-daemon off
+
+# Create base entry in indices/xattrop
+echo "Data" > $M0/FILE
+
+#------------------------------------------------------------------------------#
+TEST touch $M0/f1
+gfid_f1=$(gf_get_gfid_xattr $B0/${V0}0/f1)
+gfid_str_f1=$(gf_gfid_xattr_to_str $gfid_f1)
+
+# Remove gfid xattr and .glusterfs hard link from 2nd brick. This simulates a
+# brick crash at the point where file got created but no xattrs were set.
+TEST setfattr -x trusted.gfid $B0/${V0}1/f1
+TEST rm $B0/${V0}1/.glusterfs/${gfid_str_f1:0:2}/${gfid_str_f1:2:2}/$gfid_str_f1
+
+# Assume there were no pending xattrs on parent dir due to 1st brick crashing
+# too. Then name heal from client must heal the gfid.
+EXPECT_WITHIN $UMOUNT_TIMEOUT "Y" force_umount $M0
+TEST $GFS --volfile-id=$V0 --volfile-server=$H0 --attribute-timeout=0 --entry-timeout=0 $M0;
+EXPECT_WITHIN $CHILD_UP_TIMEOUT "1" afr_child_up_status $V0 0
+EXPECT_WITHIN $CHILD_UP_TIMEOUT "1" afr_child_up_status $V0 1
+TEST stat $M0/f1
+EXPECT "$gfid_f1" gf_get_gfid_xattr $B0/${V0}1/f1
+TEST stat $B0/${V0}1/.glusterfs/${gfid_str_f1:0:2}/${gfid_str_f1:2:2}/$gfid_str_f1
+
+#------------------------------------------------------------------------------#
+TEST mkdir $M0/dir
+TEST touch $M0/dir/f2
+gfid_f2=$(gf_get_gfid_xattr $B0/${V0}0/dir/f2)
+gfid_str_f2=$(gf_gfid_xattr_to_str $gfid_f2)
+
+# Remove gfid xattr and .glusterfs hard link from 2nd brick. This simulates a
+# brick crash at the point where file got created but no xattrs were set.
+TEST setfattr -x trusted.gfid $B0/${V0}1/dir/f2
+TEST rm $B0/${V0}1/.glusterfs/${gfid_str_f2:0:2}/${gfid_str_f2:2:2}/$gfid_str_f2
+
+#Now simulate setting of pending entry xattr on parent dir of 1st brick.
+TEST setfattr -n trusted.afr.$V0-client-1 -v 0x000000000000000000000001 $B0/${V0}0/dir
+create_brick_xattrop_entry $B0/${V0}0 dir
+
+#Trigger entry-heal via shd
+TEST $CLI volume set $V0 self-heal-daemon on
+EXPECT_WITHIN $PROCESS_UP_TIMEOUT "Y" glustershd_up_status
+EXPECT_WITHIN $CHILD_UP_TIMEOUT "1" afr_child_up_status_in_shd $V0 0
+EXPECT_WITHIN $CHILD_UP_TIMEOUT "1" afr_child_up_status_in_shd $V0 1
+
+TEST $CLI volume heal $V0
+EXPECT_WITHIN $HEAL_TIMEOUT "^0$" get_pending_heal_count $V0
+
+EXPECT "$gfid_f2" gf_get_gfid_xattr $B0/${V0}1/dir/f2
+TEST stat $B0/${V0}1/.glusterfs/${gfid_str_f2:0:2}/${gfid_str_f2:2:2}/$gfid_str_f2
+
+#------------------------------------------------------------------------------#
+cleanup;

commit f4586121c3b009175b65dc706b6e74da4bb9f47c

afr: heal gfids when file is not present on all bricks
commit 20fa80057eb430fd72b4fa31b9b65598b8ec1265 introduced a regression
wherein if a file is present in only 1 brick of replica *and* doesn't
have a gfid associated with it, it doesn't get healed upon the next
lookup from the client. Fix it.

Change-Id: I7d1111dcb45b1b8b8340a7d02558f05df70aa599
fixes: bz#1591193
Signed-off-by: Ravishankar N <ravishankar@redhat.com>

xlators/cluster/afr/src/afr-self-heal-common.c
@@ -22,7 +22,7 @@ afr_heal_synctask (xlator_t *this, afr_local_t *local);
 int
 afr_lookup_and_heal_gfid (xlator_t *this, inode_t *parent, const char *name,
                           inode_t *inode, struct afr_reply *replies,
-                          int source,  void *gfid)
+                          int source,  unsigned char *sources, void *gfid)
 {
         afr_private_t *priv = NULL;
         call_frame_t   *frame    = NULL;
@@ -37,6 +37,23 @@ afr_lookup_and_heal_gfid (xlator_t *this, inode_t *parent, const char *name,
         priv = this->private;
         wind_on = alloca0 (priv->child_count);
         ia_type = replies[source].poststat.ia_type;
+        if ((ia_type == IA_INVAL) &&
+            (AFR_COUNT(sources, priv->child_count) == priv->child_count)) {
+                /* If a file is present on some bricks of the replica but parent
+                 * dir does not have pending xattrs, all bricks are sources and
+                 * the 'source' we  selected earlier might be one where the file
+                 * is not actually present. Hence check if file is present in
+                 * any of the sources.*/
+                for (i = 0; i < priv->child_count; i++) {
+                        if (i == source)
+                                continue;
+                        if (sources[i] && replies[i].valid &&
+                            replies[i].op_ret == 0) {
+                                ia_type = replies[i].poststat.ia_type;
+                                break;
+                        }
+                }
+        }
 
         /* gfid heal on those subvolumes that do not have gfid associated
          * with the inode and update those replies.
@@ -1256,6 +1273,21 @@ afr_mark_split_brain_source_sinks_by_policy (call_frame_t *frame,
         return fav_child;
 }
 
+gf_boolean_t
+afr_is_file_empty_on_all_children (afr_private_t *priv,
+                                   struct afr_reply *replies)
+{
+        int i = 0;
+
+        for (i = 0; i < priv->child_count; i++) {
+                if ((!replies[i].valid) || (replies[i].op_ret != 0) ||
+                    (replies[i].poststat.ia_size != 0))
+                        return _gf_false;
+        }
+
+        return _gf_true;
+}
+
 int
 afr_mark_source_sinks_if_file_empty (xlator_t *this, unsigned char *sources,
                                      unsigned char *sinks,
@@ -1274,11 +1306,8 @@ afr_mark_source_sinks_if_file_empty (xlator_t *this, unsigned char *sources,
                 return -1;
 
         if (type == AFR_DATA_TRANSACTION) {
-                for (i = 0; i < priv->child_count; i++) {
-                        if (replies[i].poststat.ia_size != 0)
+                if (!afr_is_file_empty_on_all_children(priv, replies))
                                 return -1;
-                }
-
                 goto mark;
         }
 

xlators/cluster/afr/src/afr-self-heal-data.c
@@ -683,6 +683,7 @@ __afr_selfheal_data (call_frame_t *frame, xlator_t *this, fd_t *fd,
 	int source = -1;
         gf_boolean_t did_sh = _gf_true;
         gf_boolean_t is_arbiter_the_only_sink = _gf_false;
+        gf_boolean_t empty_file = _gf_false;
 
 	priv = this->private;
 
@@ -723,6 +724,11 @@ __afr_selfheal_data (call_frame_t *frame, xlator_t *this, fd_t *fd,
 		source = ret;
 
                 if (AFR_IS_ARBITER_BRICK(priv, source)) {
+                        empty_file = afr_is_file_empty_on_all_children (priv,
+                                                                locked_replies);
+                        if (empty_file)
+                                goto restore_time;
+
                         did_sh = _gf_false;
                         goto unlock;
                 }
@@ -759,7 +765,7 @@ restore_time:
 	afr_selfheal_restore_time (frame, this, fd->inode, source,
 					healed_sinks, locked_replies);
 
-        if (!is_arbiter_the_only_sink) {
+        if (!is_arbiter_the_only_sink || !empty_file) {
                 ret = afr_selfheal_inodelk (frame, this, fd->inode, this->name,
                                             0, 0, data_lock);
                 if (ret < priv->child_count) {

xlators/cluster/afr/src/afr-self-heal-entry.c
@@ -187,7 +187,7 @@ __afr_selfheal_heal_dirent (call_frame_t *frame, xlator_t *this, fd_t *fd,
 
         if (replies[source].op_ret == 0) {
                 ret = afr_lookup_and_heal_gfid (this, fd->inode, name,
-                                                inode, replies, source,
+                                                inode, replies, source, sources,
                                              &replies[source].poststat.ia_gfid);
                 if (ret)
                         return ret;
@@ -320,7 +320,7 @@ __afr_selfheal_merge_dirent (call_frame_t *frame, xlator_t *this, fd_t *fd,
 	}
 
         ret = afr_lookup_and_heal_gfid (this, fd->inode, name, inode, replies,
-                                        source,
+                                        source, sources,
                                         &replies[source].poststat.ia_gfid);
         if (ret)
                 return ret;

xlators/cluster/afr/src/afr-self-heal-name.c
@@ -19,7 +19,7 @@ __afr_selfheal_assign_gfid (xlator_t *this, inode_t *parent, uuid_t pargfid,
                             const char *bname, inode_t *inode,
                             struct afr_reply *replies, void *gfid,
                             unsigned char *locked_on, int source,
-                            gf_boolean_t is_gfid_absent)
+                            unsigned char *sources, gf_boolean_t is_gfid_absent)
 {
 	int             ret          = 0;
         int             up_count     = 0;
@@ -48,7 +48,7 @@ __afr_selfheal_assign_gfid (xlator_t *this, inode_t *parent, uuid_t pargfid,
         }
 
         afr_lookup_and_heal_gfid (this, parent, bname, inode, replies, source,
-                                  gfid);
+                                  sources, gfid);
 
 out:
 	return ret;
@@ -426,7 +426,7 @@ __afr_selfheal_name_do (call_frame_t *frame, xlator_t *this, inode_t *parent,
         is_gfid_absent = (gfid_idx == -1) ? _gf_true : _gf_false;
 	ret = __afr_selfheal_assign_gfid (this, parent, pargfid, bname, inode,
                                           replies, gfid, locked_on, source,
-                                          is_gfid_absent);
+                                          sources, is_gfid_absent);
         if (ret)
                 return ret;
 

xlators/cluster/afr/src/afr-self-heal.h
@@ -113,7 +113,7 @@ afr_selfheal_entry (call_frame_t *frame, xlator_t *this, inode_t *inode);
 int
 afr_lookup_and_heal_gfid (xlator_t *this, inode_t *parent, const char *name,
                           inode_t *inode, struct afr_reply *replies, int source,
-                          void *gfid);
+                          unsigned char *sources, void *gfid);
 
 int
 afr_selfheal_inodelk (call_frame_t *frame, xlator_t *this, inode_t *inode,
@@ -352,5 +352,9 @@ afr_mark_source_sinks_if_file_empty (xlator_t *this, unsigned char *sources,
                                      unsigned char *locked_on,
                                      struct afr_reply *replies,
                                      afr_transaction_type type);
+
+gf_boolean_t
+afr_is_file_empty_on_all_children (afr_private_t *priv,
+                                   struct afr_reply *replies);
 
 #endif /* !_AFR_SELFHEAL_H */

tests/bugs/replicate/bug-1591193-assign-gfid-and-heal.t
@@ -1,0 +1,128 @@
+#!/bin/bash
+
+. $(dirname $0)/../../include.rc
+. $(dirname $0)/../../volume.rc
+. $(dirname $0)/../../afr.rc
+
+cleanup;
+
+function check_gfid_and_link_count
+{
+        local file=$1
+
+        file_gfid_b0=$(gf_get_gfid_xattr $B0/${V0}0/$file)
+        TEST [ ! -z $file_gfid_b0 ]
+        file_gfid_b1=$(gf_get_gfid_xattr $B0/${V0}1/$file)
+        file_gfid_b2=$(gf_get_gfid_xattr $B0/${V0}2/$file)
+        EXPECT $file_gfid_b0 echo $file_gfid_b1
+        EXPECT $file_gfid_b0 echo $file_gfid_b2
+
+        EXPECT "2" stat -c %h $B0/${V0}0/$file
+        EXPECT "2" stat -c %h $B0/${V0}1/$file
+        EXPECT "2" stat -c %h $B0/${V0}2/$file
+}
+TESTS_EXPECTED_IN_LOOP=30
+
+##############################################################################
+# Test on 1x3 volume
+TEST glusterd
+TEST pidof glusterd
+TEST $CLI volume create $V0 replica 3 $H0:$B0/${V0}{0,1,2};
+TEST $CLI volume start $V0;
+
+EXPECT_WITHIN $PROCESS_UP_TIMEOUT "1" brick_up_status $V0 $H0 $B0/${V0}0
+EXPECT_WITHIN $PROCESS_UP_TIMEOUT "1" brick_up_status $V0 $H0 $B0/${V0}1
+EXPECT_WITHIN $PROCESS_UP_TIMEOUT "1" brick_up_status $V0 $H0 $B0/${V0}2
+
+EXPECT_WITHIN $PROCESS_UP_TIMEOUT "Y" glustershd_up_status
+EXPECT_WITHIN $CHILD_UP_TIMEOUT "1" afr_child_up_status_in_shd $V0 0
+EXPECT_WITHIN $CHILD_UP_TIMEOUT "1" afr_child_up_status_in_shd $V0 1
+EXPECT_WITHIN $CHILD_UP_TIMEOUT "1" afr_child_up_status_in_shd $V0 2
+
+TEST glusterfs --volfile-id=/$V0 --volfile-server=$H0 --attribute-timeout=0 --entry-timeout=0 $M0
+EXPECT_WITHIN $PROCESS_UP_TIMEOUT "1" afr_child_up_status $V0 0
+EXPECT_WITHIN $PROCESS_UP_TIMEOUT "1" afr_child_up_status $V0 1
+EXPECT_WITHIN $PROCESS_UP_TIMEOUT "1" afr_child_up_status $V0 2
+
+
+# Create files directly in the backend on different bricks
+echo $RANDOM >> $B0/${V0}0/file1
+echo $RANDOM >> $B0/${V0}1/file2
+echo $RANDOM >> $B0/${V0}2/file3
+
+# To prevent is_fresh_file code path
+sleep 2
+
+# Access them from mount to trigger name + gfid heal.
+TEST stat $M0/file1
+TEST stat $M0/file2
+TEST stat $M0/file3
+
+# Launch index heal to complete any pending data/metadata heals.
+TEST $CLI volume heal $V0
+EXPECT_WITHIN $HEAL_TIMEOUT "^0$" get_pending_heal_count $V0
+
+# Check each file has a gfid and the .glusterfs hardlink
+check_gfid_and_link_count file1
+check_gfid_and_link_count file2
+check_gfid_and_link_count file3
+
+TEST rm $M0/file1
+TEST rm $M0/file2
+TEST rm $M0/file3
+cleanup;
+
+##############################################################################
+# Test on 1x (2+1) volume
+TEST glusterd
+TEST pidof glusterd
+TEST $CLI volume create $V0 replica 3 arbiter 1 $H0:$B0/${V0}{0,1,2};
+TEST $CLI volume start $V0;
+
+EXPECT_WITHIN $PROCESS_UP_TIMEOUT "1" brick_up_status $V0 $H0 $B0/${V0}0
+EXPECT_WITHIN $PROCESS_UP_TIMEOUT "1" brick_up_status $V0 $H0 $B0/${V0}1
+EXPECT_WITHIN $PROCESS_UP_TIMEOUT "1" brick_up_status $V0 $H0 $B0/${V0}2
+
+EXPECT_WITHIN $PROCESS_UP_TIMEOUT "Y" glustershd_up_status
+EXPECT_WITHIN $CHILD_UP_TIMEOUT "1" afr_child_up_status_in_shd $V0 0
+EXPECT_WITHIN $CHILD_UP_TIMEOUT "1" afr_child_up_status_in_shd $V0 1
+EXPECT_WITHIN $CHILD_UP_TIMEOUT "1" afr_child_up_status_in_shd $V0 2
+
+TEST glusterfs --volfile-id=/$V0 --volfile-server=$H0 --attribute-timeout=0 --entry-timeout=0 $M0
+EXPECT_WITHIN $PROCESS_UP_TIMEOUT "1" afr_child_up_status $V0 0
+EXPECT_WITHIN $PROCESS_UP_TIMEOUT "1" afr_child_up_status $V0 1
+EXPECT_WITHIN $PROCESS_UP_TIMEOUT "1" afr_child_up_status $V0 2
+
+
+# Create files directly in the backend on different bricks
+echo $RANDOM >> $B0/${V0}0/file1
+echo $RANDOM >> $B0/${V0}1/file2
+touch $B0/${V0}2/file3
+
+# To prevent is_fresh_file code path
+sleep 2
+
+# Access them from mount to trigger name + gfid heal.
+TEST stat $M0/file1
+TEST stat $M0/file2
+
+# Though file is created on all 3 bricks, lookup will fail as arbiter blames the
+# other 2 bricks and ariter is not 'readable'.
+TEST ! stat $M0/file3
+
+# Launch index heal to complete any pending data/metadata heals.
+TEST $CLI volume heal $V0
+EXPECT_WITHIN $HEAL_TIMEOUT "^0$" get_pending_heal_count $V0
+
+# Now file3 should be accesible from mount.
+TEST stat $M0/file3
+
+# Check each file has a gfid and the .glusterfs hardlink
+check_gfid_and_link_count file1
+check_gfid_and_link_count file2
+check_gfid_and_link_count file3
+
+TEST rm $M0/file1
+TEST rm $M0/file2
+TEST rm $M0/file3
+cleanup;