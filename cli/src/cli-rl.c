gluster/glusterfs

commit 5acb74d7daee0fc5c501a82e604ec2abdb3cf88a

cli : fix few coverity in cli
This patch fixes DEADCODE and FORWARD_NULL.

CID : 1124495, 1124380, 1124381

Change-Id: I79992e396dbcb1bfe6cd0614d49a8da3f67d648d
updates: bz#789278
Signed-off-by: Sunny Kumar <sunkumar@redhat.com>

cli/src/cli-rl.c
@@ -295,6 +295,8 @@ cli_rl_autocomplete_prepare (struct cli_state *state, const char *text)
         if (!word)
                 goto out;
 
+        if (!tokens)
+                return 0;
         matches = cli_rl_get_matches (state, word, token);
 
         state->matches = matches;

cli/src/cli-xml-output.c
@@ -2411,10 +2411,10 @@ cli_xml_output_vol_info_option (xmlTextWriterPtr writer, char *substr,
                         break;
                 ptr1++;
                 ptr2++;
-                if (!ptr1)
-                        goto out;
-                if (!ptr2)
-                        goto out;
+                if (!*ptr1)
+                        break;
+                if (!*ptr2)
+                        break;
         }
         if (*ptr2 == '\0')
                 goto out;

commit 5bcfea1afdb6f7b19dd14372bcae2c367c9b3c93

cli : fix coverity cli-rl.c
This fixes new coverity issue introduced by commit 5acb74d7da.

Fixes 1395188.

Change-Id: I629e5f214150672e309abb2fc58f55f1f58d8fb7
updates: bz#789278
Signed-off-by: Sunny Kumar <sunkumar@redhat.com>

cli/src/cli-rl.c
@@ -295,7 +295,7 @@ cli_rl_autocomplete_prepare (struct cli_state *state, const char *text)
         if (!word)
                 goto out;
 
-        if (!tokens)
+        if (!token)
                 return 0;
         matches = cli_rl_get_matches (state, word, token);
 