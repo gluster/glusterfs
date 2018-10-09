ba12c00f6d5b1e79796c1cb03f87035ebafd7f19
Sunny Kumar <sunkumar@redhat.com>
2018-10-09 12:43:32 +0530

    cliutils :  python2 to python3 compat
    This patch fixes import issue in cliutils.
    
    Provided solution is to use relative import.
    
    Change-Id: I14c9a0b528ef52e7c91f6b17b569c68c2ced8912
    updates: #411
    Signed-off-by: Sunny Kumar <sunkumar@redhat.com>
    (cherry picked from commit 8d4c5e022bba1b99786ce13f407c27024beccc23)

--- extras/cliutils/__init__.py
+++ extras/cliutils/__init__.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 # Reexporting the utility funcs and classes
-from cliutils import (runcli,
-                      sync_file_to_peers,
-                      execute_in_peers,
-                      execute,
-                      node_output_ok,
-                      node_output_notok,
-                      output_error,
-                      oknotok,
-                      yesno,
-                      get_node_uuid,
-                      Cmd,
-                      GlusterCmdException,
-                      set_common_args_func)
+from .cliutils import (runcli,
+                       sync_file_to_peers,
+                       execute_in_peers,
+                       execute,
+                       node_output_ok,
+                       node_output_notok,
+                       output_error,
+                       oknotok,
+                       yesno,
+                       get_node_uuid,
+                       Cmd,
+                       GlusterCmdException,
+                       set_common_args_func)
 
 
 # This will be useful when `from cliutils import *`
