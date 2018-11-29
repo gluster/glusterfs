cc0da4931533d21dbc3b9478984e24a7056dc835
Raghavendra Bhat <raghavendra@redhat.com>
2018-11-29 12:04:45 -0500

    features/bitrot: compare the signature with proper length
    * The scrubber was comparing the checksum of the file that it
      calculated (by reading the file) with the on disk signature
      (stored via xattr) wrongly. It was using strlen to calculate
      the signature, while the actual length of the signature is
      given by the brick. Just use the actual length that the brick
      provides instead of trying to calculate the signature length via
      strlen API.
    
    * In posix, gfid2path was using the same string that contains the
      list of all the xattrs of file to save the value of the gfid2path
      xattr as well. This causes confusion when gfid2path xattr is queried
      by scrubber for getting the actual path of a corrupted file. Use
      separate string to fetch the value of the xattr instead of the string
      that contains the list of xattrs.
    
    Backport of:
     > Patch: https://review.gluster.org/21752/
     > BUG: 1654805
     > Change-Id: I2d664ab524d2b312233476cb35863dde3122e9a9
    (cherry picked from commit f77fb6d568616592ab25501c402c140d15235ca9)
     
    Change-Id: I2d664ab524d2b312233476cb35863dde3122e9a9
    fixes: bz#1654370
    Signed-off-by: Raghavendra Bhat <raghavendra@redhat.com>

--- xlators/features/bit-rot/src/bitd/bit-rot-scrub.c
+++ xlators/features/bit-rot/src/bitd/bit-rot-scrub.c
@@ -130,6 +130,8 @@ bitd_scrub_post_compute_check(xlator_t *this, br_child_t *child, fd_t *fd,
 
     (void)memcpy(*signature, signptr, sizeof(br_isignature_out_t) + signlen);
 
+    (*signature)->signaturelen = signlen;
+
 unref_dict:
     dict_unref(xattr);
 out:
@@ -222,7 +224,7 @@ bitd_compare_ckum(xlator_t *this, br_isignature_out_t *sign, unsigned char *md,
     GF_VALIDATE_OR_GOTO(this->name, md, out);
     GF_VALIDATE_OR_GOTO(this->name, entry, out);
 
-    if (strncmp(sign->signature, (char *)md, strlen(sign->signature)) == 0) {
+    if (strncmp(sign->signature, (char *)md, sign->signaturelen) == 0) {
         gf_msg_debug(this->name, 0,
                      "%s [GFID: %s | Brick: %s] "
                      "matches calculated checksum",
