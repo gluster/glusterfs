64d800940e67b75d51f536816ee92bae59d6f850
Sheetal Pamecha <sheetal.pamecha08@gmail.com>
2018-12-13 11:14:10 +0530

    cluster/ec: NULL pointer deferencing clang fix
    Removing VALIDATE_OR_GOTO check on "this"
    
    Change-Id: I154deaca5302b41c1cafd87077de880dd03ec613
    Updates: bz#1622665
    Signed-off-by: Sheetal Pamecha <sheetal.pamecha08@gmail.com>

--- xlators/cluster/ec/src/ec-locks.c
+++ xlators/cluster/ec/src/ec-locks.c
@@ -285,7 +285,6 @@ ec_entrylk(call_frame_t *frame, xlator_t *this, uintptr_t target,
 
     gf_msg_trace("ec", 0, "EC(ENTRYLK) %p", frame);
 
-    VALIDATE_OR_GOTO(this, out);
     GF_VALIDATE_OR_GOTO(this->name, frame, out);
     GF_VALIDATE_OR_GOTO(this->name, this->private, out);
 
