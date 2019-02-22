gluster/glusterfs

commit c3fcff9ccbfcec1be242fd5cf210c9995586b078

glupy: remove from the build
Based on the proposal to remove few features as they are not
actively maintained [1], removing 'glupy' translator from the
build.

[1] https://lists.gluster.org/pipermail/gluster-users/2018-July/034400.html

This patch aims at clearing the translator from build and tests.
A followup is needed to remove the code from repository.

Updates: bz#1642810
Change-Id: I41d0c1956330c3bbca62c540ccf9ab01bbf3a092
Signed-off-by: Amar Tumballi <amarts@redhat.com>

configure.ac
@@ -125,12 +125,6 @@ AC_CONFIG_FILES([Makefile
                 xlators/features/changelog/src/Makefile
                 xlators/features/changelog/lib/Makefile
                 xlators/features/changelog/lib/src/Makefile
-                xlators/features/glupy/Makefile
-                xlators/features/glupy/examples/Makefile
-                xlators/features/glupy/src/Makefile
-                xlators/features/glupy/src/setup.py
-                xlators/features/glupy/src/__init__.py
-                xlators/features/glupy/src/glupy/Makefile
                 xlators/features/locks/Makefile
                 xlators/features/locks/src/Makefile
                 xlators/features/quota/Makefile
@@ -1393,42 +1387,6 @@ if test -n "$LIBAIO"; then
    BUILD_LIBAIO=yes
 fi
 
-dnl glupy section
-BUILD_GLUPY=no
-
-AC_ARG_ENABLE([glupy], AS_HELP_STRING([--disable-glupy], [disable building glupy]))
-
-if test "x$enable_glupy" != "xno"; then
-    if test "x${have_python}" = "xyes" -a -n "${PYTHON_CFLAGS}"; then
-        case $host_os in
-            darwin*)
-                BUILD_GLUPY=no
-                ;;
-            *)
-                BUILD_GLUPY=yes
-                ;;
-        esac
-    else
-        AC_MSG_WARN([
-        ---------------------------------------------------------------------------------
-        cannot build glupy. python ${PYTHON_VERSION} and python-devel/python-dev package are required.
-        ---------------------------------------------------------------------------------])
-    fi
-
-    if test "x$BUILD_GLUPY" = "xyes"; then
-        GLUPY_SUBDIR=glupy
-        GLUPY_SUBDIR_MAKEFILE=xlators/features/glupy/Makefile
-        GLUPY_SUBDIR_SRC_MAKEFILE=xlators/features/glupy/src/Makefile
-
-        echo "building glupy with -isystem ${PYTHON_CFLAGS} ${PYTHON_LIBS}"
-
-        AC_SUBST(GLUPY_SUBDIR)
-        AC_SUBST(GLUPY_SUBDIR_MAKEFILE)
-        AC_SUBST(GLUPY_SUBDIR_SRC_MAKEFILE)
-    fi
-fi
-dnl end glupy section
-
 dnl gnfs section
 BUILD_GNFS="no"
 AC_ARG_ENABLE([gnfs],
@@ -1592,7 +1550,7 @@ case $host_os in
 esac
 dnl GF_XLATOR_DEFAULT_LDFLAGS is for most xlators that expose a common set of symbols
 GF_XLATOR_DEFAULT_LDFLAGS='-avoid-version -export-symbols $(top_srcdir)/xlators/xlator.sym $(UUID_LIBS) $(GF_NO_UNDEFINED)'
-dnl GF_XLATOR_LDFLAGS is for xlators that expose extra symbols, e.g. dht and glupy
+dnl GF_XLATOR_LDFLAGS is for xlators that expose extra symbols, e.g. dht
 GF_XLATOR_LDFLAGS='-avoid-version $(UUID_LIBS) $(GF_NO_UNDEFINED)'
 
 AC_SUBST(GF_HOST_OS)
@@ -1679,7 +1637,6 @@ echo "Linux-AIO            : $BUILD_LIBAIO"
 echo "Enable Debug         : $BUILD_DEBUG"
 echo "Enable ASAN          : $BUILD_ASAN"
 echo "Block Device xlator  : $BUILD_BD_XLATOR"
-echo "glupy                : $BUILD_GLUPY"
 echo "Use syslog           : $USE_SYSLOG"
 echo "XML output           : $BUILD_XML_OUTPUT"
 echo "Encryption xlator    : $BUILD_CRYPT_XLATOR"

glusterfs.spec.in
@@ -1090,13 +1090,6 @@ exit 0
 %exclude %{_includedir}/glusterfs/api
 %exclude %{_libdir}/libgfapi.so
 %{_libdir}/*.so
-# Glupy Translator examples
-%dir %{_libdir}/glusterfs/%{version}%{?prereltag}/xlator
-%dir %{_libdir}/glusterfs/%{version}%{?prereltag}/xlator/features
-%dir %{_libdir}/glusterfs/%{version}%{?prereltag}/xlator/features/glupy
-     %{_libdir}/glusterfs/%{version}%{?prereltag}/xlator/features/glupy/debug-trace.*
-     %{_libdir}/glusterfs/%{version}%{?prereltag}/xlator/features/glupy/helloworld.*
-     %{_libdir}/glusterfs/%{version}%{?prereltag}/xlator/features/glupy/negative.*
 %{_libdir}/pkgconfig/libgfchangelog.pc
 
 %files client-xlators
@@ -1111,29 +1104,12 @@ exit 0
 %dir %{_libdir}/glusterfs/%{version}%{?prereltag}/xlator/encryption
      %{_libdir}/glusterfs/%{version}%{?prereltag}/xlator/encryption/rot-13.so
 %dir %{_libdir}/glusterfs/%{version}%{?prereltag}/xlator/features
-     %{_libdir}/glusterfs/%{version}%{?prereltag}/xlator/features/glupy.so
-%if ( %{_usepython3} )
-%dir %{_libdir}/glusterfs/%{version}%{?prereltag}/xlator/features/glupy/__pycache__
-     %{_libdir}/glusterfs/%{version}%{?prereltag}/xlator/features/glupy/__pycache__/*
-%endif
      %{_libdir}/glusterfs/%{version}%{?prereltag}/xlator/features/quiesce.so
 %dir %{_libdir}/glusterfs/%{version}%{?prereltag}/xlator/playground
      %{_libdir}/glusterfs/%{version}%{?prereltag}/xlator/playground/template.so
 %dir %{_libdir}/glusterfs/%{version}%{?prereltag}/xlator/testing
 %dir %{_libdir}/glusterfs/%{version}%{?prereltag}/xlator/testing/performance
      %{_libdir}/glusterfs/%{version}%{?prereltag}/xlator/testing/performance/symlink-cache.so
-# Glupy Python files
-%if ( %{_usepython3} )
-%dir %{python3_sitelib}/gluster
-%dir %{python3_sitelib}/gluster/__pycache__
-     %{python3_sitelib}/gluster/__pycache__/*
-%dir %{python3_sitelib}/gluster/glupy
-     %{python3_sitelib}/gluster/glupy/*
-%else
-%dir %{python2_sitelib}/gluster
-%dir %{python2_sitelib}/gluster/glupy
-     %{python2_sitelib}/gluster/glupy/*
-%endif
 
 %files fuse
 # glusterfs is a symlink to glusterfsd, -server depends on -fuse.
@@ -1205,11 +1181,9 @@ exit 0
 # so that all other gluster submodules can reside in the same namespace.
 %if ( %{_usepython3} )
 %dir %{python3_sitelib}/gluster
-     %{python3_sitelib}/gluster/__init__.*
      %{python3_sitelib}/gluster/cliutils
 %else
 %dir %{python2_sitelib}/gluster
-     %{python2_sitelib}/gluster/__init__.*
      %{python2_sitelib}/gluster/cliutils
 %endif
 

xlators/features/Makefile.am
@@ -2,10 +2,9 @@ if BUILD_CLOUDSYNC
   CLOUDSYNC_DIR = cloudsync
 endif
 
+SUBDIRS = locks quota read-only quiesce marker index barrier arbiter upcall \
+	compress changelog gfid-access snapview-client snapview-server trash \
+	shard bit-rot leases selinux sdfs namespace $(CLOUDSYNC_DIR) thin-arbiter \
+	utime
 
-SUBDIRS = locks quota read-only quiesce marker index barrier \
-          arbiter compress changelog \
-          gfid-access $(GLUPY_SUBDIR) upcall snapview-client snapview-server \
-          trash shard bit-rot leases selinux sdfs namespace $(CLOUDSYNC_DIR) thin-arbiter \
-          utime
 CLEANFILES =

MAINTAINERS
@@ -313,10 +313,6 @@ P: Mohammed Rafi KC <rkavunga@redhat.com>
 S: Maintained
 F: geo-replication/
 
-Glupy
-S: Orphan
-F: xlators/features/glupy/
-
 Glusterfind
 M: Milind Changire <mchangir@redhat.com>
 P: Aravinda VK <avishwan@redhat.com>

tests/features/glupy.t
@@ -1,31 +1,0 @@
-#!/bin/bash
-
-. $(dirname $0)/../include.rc
-. $(dirname $0)/../volume.rc
-
-echo $PYTHON_PATH >&2
-cleanup;
-
-TEST mkdir -p $B0/glupytest
-cat > $B0/glupytest.vol <<EOF
-volume vol-posix
-    type storage/posix
-    option directory $B0/glupytest
-end-volume
-
-volume vol-glupy
-    type features/glupy
-    option module-name helloworld
-    subvolumes vol-posix
-end-volume
-EOF
-
-TEST glusterfs -f $B0/glupytest.vol -l $LOGDIR/glupy.log $M0;
-
-TEST touch $M0/filename;
-EXPECT "filename" ls $M0
-TEST rm -f $M0/filename;
-
-EXPECT_WITHIN $UMOUNT_TIMEOUT "Y" force_umount $M0;
-
-cleanup;

commit 29cd2e760da3cf7299e8e978e388d9b262c370a8

build: include gluster/__init__.* files for python packaging
The gluster/__init__.py file was originaly part of the glupy
installation. This file is required to have the python-gluster package
function correctly, it is expected to provide the 'gluster' namespace
for other python packages (like from the libgfapi-python project).

Because glupy does not exist anymore, this file is now added to in a new
extras/python directory.

Change-Id: I14fe959778ee3344d7d54ba342c7928a4d8080a2
Fixes: c3fcff9ccbfcec1be242fd5cf210c9995586b078
Fixes: 8293d21280fd6ddfc9bb54068cf87794fc6be207
Updates: bz#1642810
Signed-off-by: Niels de Vos <ndevos@redhat.com>

configure.ac
@@ -226,6 +226,7 @@ AC_CONFIG_FILES([Makefile
                 extras/hook-scripts/reset/Makefile
                 extras/hook-scripts/reset/post/Makefile
                 extras/hook-scripts/reset/pre/Makefile
+                extras/python/Makefile
                 extras/snap_scheduler/Makefile
                 events/Makefile
                 events/src/Makefile
@@ -630,6 +631,7 @@ AM_PATH_PYTHON([2.6],,[:])
 if test -n "${PYTHON}"; then
    have_python=yes
 fi
+AM_CONDITIONAL(HAVE_PYTHON, test "x$have_python" = "xyes")
 
 dnl Use pkg-config to get runtime search path missing from ${PYTHON}-config
 dnl Just do "true" on failure so that configure does not bail out

extras/Makefile.am
@@ -11,7 +11,7 @@ EditorModedir = $(docdir)
 EditorMode_DATA = glusterfs-mode.el glusterfs.vim
 
 SUBDIRS = init.d systemd benchmarking hook-scripts $(OCF_SUBDIR) LinuxRPM \
-          $(GEOREP_EXTRAS_SUBDIR) snap_scheduler firewalld cliutils
+          $(GEOREP_EXTRAS_SUBDIR) snap_scheduler firewalld cliutils python
 
 confdir = $(sysconfdir)/glusterfs
 if WITH_SERVER

extras/python/Makefile.am
@@ -1,0 +1,7 @@
+if HAVE_PYTHON
+# Install __init__.py into the Python site-packages area
+pypkgdir = @BUILD_PYTHON_SITE_PACKAGES@/gluster
+pypkg_PYTHON = __init__.py
+endif
+
+EXTRA_DIST = __init__.py

extras/python/__init__.py
@@ -1,0 +1,2 @@
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)

glusterfs.spec.in
@@ -1186,9 +1186,12 @@ exit 0
 # so that all other gluster submodules can reside in the same namespace.
 %if ( %{_usepython3} )
 %dir %{python3_sitelib}/gluster
+     %{python3_sitelib}/gluster/__init__.*
+     %{python3_sitelib}/gluster/__pycache__
      %{python3_sitelib}/gluster/cliutils
 %else
 %dir %{python2_sitelib}/gluster
+     %{python2_sitelib}/gluster/__init__.*
      %{python2_sitelib}/gluster/cliutils
 %endif
 