e52e463814fae7d3678f54d7d29b1a45e47d0410
Raghavendra G <rgowdapp@redhat.com>
2019-03-27 16:07:45 +0530

    core: move "dict is NULL" logs to DEBUG log level
    Too many logs get printed if dict_ref() and dict_unref() are passed
    NULL pointer.
    
    Backport of https://review.gluster.org/#/c/glusterfs/+/22128/
    
    fixes: bz#1693201
    Change-Id: I18afd849d64318f68baa7b549ee310dac0e1e786
    Signed-off-by: Milind Changire <mchangir@redhat.com>
    Signed-off-by: Raghavendra G <rgowdapp@redhat.com>

--- libglusterfs/src/dict.c
+++ libglusterfs/src/dict.c
@@ -652,7 +652,7 @@ dict_unref (dict_t *this)
         uint64_t ref = 0;
 
         if (!this) {
-                gf_msg_callingfn ("dict", GF_LOG_WARNING, EINVAL,
+                gf_msg_callingfn ("dict", GF_LOG_DEBUG, EINVAL,
                                   LG_MSG_INVALID_ARG, "dict is NULL");
                 return;
         }
@@ -667,7 +667,7 @@ dict_t *
 dict_ref (dict_t *this)
 {
         if (!this) {
-                gf_msg_callingfn ("dict", GF_LOG_WARNING, EINVAL,
+                gf_msg_callingfn ("dict", GF_LOG_DEBUG, EINVAL,
                                   LG_MSG_INVALID_ARG, "dict is NULL");
                 return NULL;
         }
