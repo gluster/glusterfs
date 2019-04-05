e26fc565990091ac18acd2859296029d3b7609b4
Mohit Agrawal <moagrawal@redhat.com>
2019-04-05 09:46:24 +0530

    glusterfsd: Multiple shd processes are spawned on brick_mux environment
    Problem: Multiple shd processes are spawned while starting volumes
             in the loop on brick_mux environment.glusterd spawn a process
             based on a pidfile and shd daemon is taking some time to
             update pid in pidfile due to that glusterd is not able to
             get shd pid
    
    Solution: Commit cd249f4cb783f8d79e79468c455732669e835a4f changed
              the code to update pidfile in parent for any gluster daemon
              after getting the status of forking child in parent.To resolve
              the same correct the condition update pidfile in parent only
              for glusterd and for rest of the daemon pidfile is updated in
              child
    
    > Change-Id: Ifd14797fa949562594a285ec82d58384ad717e81
    > fixes: bz#1684404
    > Signed-off-by: Mohit Agrawal <moagrawal@redhat.com>
    > (Cherry picked from commit 66986594a9023c49e61b32769b7e6b260b600626)
    > (Reviewed on upstream link https://review.gluster.org/#/c/glusterfs/+/22290/)
    
    Change-Id: Ie295e905c01f2749bc39da5511537cc9a85832e5
    fixes: bz#1696513
    Signed-off-by: Mohit Agrawal <moagrawal@redhat.com>

--- libglusterfs/src/common-utils.c
+++ libglusterfs/src/common-utils.c
@@ -4000,15 +4000,14 @@ gf_is_service_running (char *pidfile, int *pid)
         ret = lockf (fno, F_TEST, 0);
         if (ret == -1)
                 running = _gf_true;
-        if (!pid) {
-                goto out;
-        }
 
         ret = fscanf (file, "%d", pid);
         if (ret <= 0) {
                 gf_msg ("", GF_LOG_ERROR, errno, LG_MSG_FILE_OP_FAILED,
                         "Unable to read pidfile: %s", pidfile);
                 *pid = -1;
+                running = _gf_false;
+                goto out;
         }
 
         running = gf_is_pid_running (*pid);
