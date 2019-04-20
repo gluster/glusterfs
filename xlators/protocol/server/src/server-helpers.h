48d160756813875cf1889b4ce96493d25f96c726
Amar Tumballi <amarts@redhat.com>
2019-04-20 11:55:12 +0530

    protocol: remove compound fop
    Compound fops are kept on wire as a backward compatibility with
    older AFR modules. The AFR module used beyond 4.x releases are
    not using compound fops. Hence removing the compound fop in the
    protocol code.
    
    Note that, compound-fops was already an 'option' in AFR, and
    completely removed since 4.1.x releases.
    
    So, point to note is, with this change, we have 2 ways to upgrade
    when clients of 3.x series are present.
    
    i) set 'use-compound-fops' option to 'false' on any volume which
       is of replica type. And then upgrade the servers.
    
    ii) Do a two step upgrade. First from current version (which will
        already be EOL if it's using compound) to a 4.1..6.x version,
        and then an upgrade to 7.x.
    
    Consider the overall code which we are removing for the option
    seems quite high, I believe it is worth it.
    
    updates: bz#1693692
    Signed-off-by: Amar Tumballi <amarts@redhat.com>
    Change-Id: I0a8876d0367a15e1410ec845f251d5d3097ee593

--- xlators/protocol/server/src/server-helpers.h
+++ xlators/protocol/server/src/server-helpers.h
@@ -86,22 +86,6 @@ int
 getactivelkinfo_rsp_cleanup_v2(gfx_getactivelk_rsp *rsp);
 
 int
-server_populate_compound_response(xlator_t *this, gfs3_compound_rsp *rsp,
-                                  call_frame_t *frame,
-                                  compound_args_cbk_t *args_cbk, int index);
-int
-server_get_compound_resolve(server_state_t *state, gfs3_compound_req *req);
-
-int
-server_populate_compound_request(gfs3_compound_req *req, call_frame_t *frame,
-                                 default_args_t *this_args, int index);
-void
-server_compound_rsp_cleanup(gfs3_compound_rsp *rsp, compound_args_cbk_t *args);
-
-void
-server_compound_req_cleanup(gfs3_compound_req *req, int len);
-
-int
 unserialize_req_locklist(gfs3_setactivelk_req *req, lock_migration_info_t *lmi);
 
 int
@@ -119,22 +103,5 @@ serialize_rsp_dirent_v2(gf_dirent_t *entries, gfx_readdir_rsp *rsp);
 
 int
 serialize_rsp_direntp_v2(gf_dirent_t *entries, gfx_readdirp_rsp *rsp);
-
-int
-server_populate_compound_response_v2(xlator_t *this, gfx_compound_rsp *rsp,
-                                     call_frame_t *frame,
-                                     compound_args_cbk_t *args_cbk, int index);
-int
-server_get_compound_resolve_v2(server_state_t *state, gfx_compound_req *req);
-
-int
-server_populate_compound_request_v2(gfx_compound_req *req, call_frame_t *frame,
-                                    default_args_t *this_args, int index);
-void
-server_compound_rsp_cleanup_v2(gfx_compound_rsp *rsp,
-                               compound_args_cbk_t *args);
-
-void
-server_compound_req_cleanup_v2(gfx_compound_req *req, int len);
 
 #endif /* !_SERVER_HELPERS_H */
