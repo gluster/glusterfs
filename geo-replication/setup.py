319d6a15db0484acf4a24b8d727b056a939e70f5
kshithijiyer <kshithij.ki@gmail.com>
2019-07-04 19:03:34 +0530

    geo-replication/setup.py missing license details in setup()
    geo-replication/setup.py is missing license details in
    setup() and is using string 'syncdaemon' instead
    of using the variable name which has the same value.
    
    Present:
    setup(
        name=name,
        version="",
        description='GlusterFS Geo Replication',
        license='',
        author='Red Hat, Inc.',
        author_email='gluster-devel@gluster.org',
        url='http://www.gluster.org',
        packages=['syncdaemon', ],
        test_suite='nose.collector',
        install_requires=[],
        scripts=[],
        entry_points={},
    )
    
    Changing to:
    setup(
        name=name,
        version="",
        description='GlusterFS Geo Replication',
        license='GPLV2 and LGPLV3+',
        author='Red Hat, Inc.',
        author_email='gluster-devel@gluster.org',
        url='http://www.gluster.org',
        packages=[name, ],
        test_suite='nose.collector',
        install_requires=[],
        scripts=[],
        entry_points={},
    )
    
    fixes: bz#1727107
    
    Change-Id: I742c4c510da55a22256c819da7e2fc9622119f63
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>

--- geo-replication/setup.py
+++ geo-replication/setup.py
@@ -1,7 +1,7 @@
 #
 # Copyright (c) 2011-2014 Red Hat, Inc. <http://www.redhat.com>
 # This file is part of GlusterFS.
-
+#
 # This file is licensed to you under your choice of the GNU Lesser
 # General Public License, version 3 or any later version (LGPLv3 or
 # later), or the GNU General Public License, version 2 (GPLv2), in all
@@ -20,11 +20,11 @@ setup(
     name=name,
     version="",
     description='GlusterFS Geo Replication',
-    license='',
+    license='GPLV2 and LGPLV3+',
     author='Red Hat, Inc.',
     author_email='gluster-devel@gluster.org',
     url='http://www.gluster.org',
-    packages=['syncdaemon', ],
+    packages=[name, ],
     test_suite='nose.collector',
     install_requires=[],
     scripts=[],
