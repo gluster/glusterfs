ea95631ff47c8048f039faedbc0faa918c4e165a
Pranith Kumar K <pkarampu@redhat.com>
2019-09-05 16:12:39 +0530

    cluster/ec: quorum-count implementation
    fixes: #721
    Change-Id: I5333540e3c635ccf441cf1f4696e4c8986e38ea8
    Signed-off-by: Pranith Kumar K <pkarampu@redhat.com>

--- xlators/cluster/ec/src/ec-types.h
+++ xlators/cluster/ec/src/ec-types.h
@@ -655,6 +655,7 @@ struct _ec {
     gf_boolean_t optimistic_changelog;
     gf_boolean_t parallel_writes;
     uint32_t stripe_cache;
+    uint32_t quorum_count;
     uint32_t background_heals;
     uint32_t heal_wait_qlen;
     uint32_t self_heal_window_size; /* max size of read/writes */
