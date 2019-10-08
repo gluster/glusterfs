0a9ca8162725b2f6b17a4d2e8cc863d27bb8770e
Yaniv Kaul <ykaul@redhat.com>
2019-10-09 00:25:42 +0300

    dict.{c,h}: remove the lock from the _data struct
    I'm not sure why it was there and I did not see any use for it.
    In the hope I did not miss anything, I removed it.
    
    Change-Id: I02fa2e8e2a598b488fddbff4c7168dc4a41929b2
    updates: bz#1193929
    Signed-off-by: Yaniv Kaul <ykaul@redhat.com>

--- libglusterfs/src/glusterfs/dict.h
+++ libglusterfs/src/glusterfs/dict.h
@@ -97,7 +97,6 @@ typedef struct _data_pair data_pair_t;
 struct _data {
     char *data;
     gf_atomic_t refcount;
-    gf_lock_t lock;
     gf_dict_data_type_t data_type;
     int32_t len;
     gf_boolean_t is_static;
