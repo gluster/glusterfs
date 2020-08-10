1d5eb3682a90628a012467c6be197c15401ccba5
Daniel Morante <daniel@morante.net>
2020-08-09 21:30:45 -0400

    FreeBSD patches for fuse mount utility
    Change-Id: Ib2bac85c28905bb8997fbb64db2308f2a6f31720
    Fixes: #1376

--- contrib/fuse-lib/mount.c
+++ contrib/fuse-lib/mount.c
@@ -390,6 +390,7 @@ fuse_mount_sys (const char *mountpoint, char *fsname,
         build_iovec (&iov, &iovlen, "from", "/dev/fuse", -1);
         build_iovec (&iov, &iovlen, "volname", source, -1);
         build_iovec (&iov, &iovlen, "fd", fdstr, -1);
+        build_iovec (&iov, &iovlen, "allow_other", NULL, -1);
         ret = nmount (iov, iovlen, mountflags);
 #else
         ret = mount (source, mountpoint, fstype, mountflags,
