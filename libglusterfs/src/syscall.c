2c37b5c86f51e940f6125ab1b663f65a375b2c77
Daniel Morante <daniel@morante.net>
2020-08-09 21:38:52 -0400

    BSD: extattr_set_link and extattr_set_fd return bytes written not 0
    Change-Id: I120006dab4e199ad3d3f4a5ebc9c352f3c49ea7d
    Fixes: #1376

--- libglusterfs/src/syscall.c
+++ libglusterfs/src/syscall.c
@@ -506,7 +506,7 @@ sys_lsetxattr(const char *path, const char *name, const void *value,
 #endif
 
 #ifdef GF_BSD_HOST_OS
-    return FS_RET_CHECK0(
+    return FS_RET_CHECK(
         extattr_set_link(path, EXTATTR_NAMESPACE_USER, name, value, size),
         errno);
 #endif
@@ -624,7 +624,7 @@ sys_fsetxattr(int filedes, const char *name, const void *value, size_t size,
 #endif
 
 #ifdef GF_BSD_HOST_OS
-    return FS_RET_CHECK0(
+    return FS_RET_CHECK(
         extattr_set_fd(filedes, EXTATTR_NAMESPACE_USER, name, value, size),
         errno);
 #endif
