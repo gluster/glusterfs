14200438772e70a66309e5017a2228ecb1dd181e
Daniel Morante <daniel@morante.net>
2020-08-09 21:44:33 -0400

    Use sysctl for get_mem_size() on FreeBSD
    Change-Id: Iab2600260707af02e2b8da9e489cbb12b9fc14a6
    Fixes: #1376

--- libglusterfs/src/common-utils.c
+++ libglusterfs/src/common-utils.c
@@ -3119,7 +3119,7 @@ get_mem_size()
     memsize = page_size * num_pages;
 #endif
 
-#if defined GF_DARWIN_HOST_OS
+#if defined GF_DARWIN_HOST_OS || defined __FreeBSD__
 
     size_t len = sizeof(memsize);
     int name[] = {CTL_HW, HW_PHYSMEM};
