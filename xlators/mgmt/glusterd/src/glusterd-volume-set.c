c6b9f4ec876fee5b22fb5e4611bb1f06db49f76c
Mohit Agrawal <moagrawa@redhat.com>
2020-12-15 17:06:18 +0530

    core: Implement gracefull shutdown for a brick process
    Resolve reviewer comment specific to avoid memory leak
    
    Fixes: #1749
    Change-Id: I68fbbb39160a4595fb8b1b19836f44b356e89716
    Signed-off-by: Mohit Agrawal <moagrawa@redhat.com>

--- xlators/mgmt/glusterd/src/glusterd-volume-set.c
+++ xlators/mgmt/glusterd/src/glusterd-volume-set.c
@@ -2930,17 +2930,16 @@ struct volopt_map_entry glusterd_volopt_map[] = {
                     "brick multiplexing. Brick multiplexing ensures that "
                     "compatible brick instances can share one single "
                     "brick process."},
-    /*{.key = GLUSTER_BRICK_GRACEFUL_CLEANUP,
+    {.key = GLUSTER_BRICK_GRACEFUL_CLEANUP,
      .voltype = "mgmt/glusterd",
      .value = "disable",
      .op_version = GD_OP_VERSION_9_0,
      .validate_fn = validate_boolean,
-     .type = GLOBAL_DOC,
+     .type = GLOBAL_NO_DOC,
      .description = "This global option can be used to enable/disable "
                     "graceful cleanup, after enable graceful cleanup "
                     "glusterd always sends a detach rpc request to stop a "
                     "brick process."},
-    */
     {.key = GLUSTERD_VOL_CNT_PER_THRD,
      .voltype = "mgmt/glusterd",
      .value = GLUSTERD_VOL_CNT_PER_THRD_DEFAULT_VALUE,
