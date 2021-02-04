36d27890e1e0e17c29d4019a44029fd7b4533a23
Pranith Kumar Karampuri <pranith.karampuri@phonepe.com>
2021-02-04 18:30:26 +0530

    cluster/afr: Change default self-heal-window-size to 1MB (#2113)
    At the moment self-heal-window-size is 128KB. This leads to healing data
    in 128KB chunks. With the growth of data and the avg file sizes
    nowadays, 1MB seems like a better default.
    
    Change-Id: I70c42c83b16c7adb53d6b5762969e878477efb5c
    Fixes: #2067
    Signed-off-by: Pranith Kumar K <pranith.karampuri@phonepe.com>

--- xlators/cluster/afr/src/afr-self-heal-data.c
+++ xlators/cluster/afr/src/afr-self-heal-data.c
@@ -353,6 +353,12 @@ afr_selfheal_data_do(call_frame_t *frame, xlator_t *this, fd_t *fd, int source,
     }
 
     block = 128 * 1024 * priv->data_self_heal_window_size;
+    if (HAS_HOLES((&replies[source].poststat))) {
+        /*Reduce the possibility of data-block allocations in case of files
+         * with holes. Correct way to fix it would be to use seek fop while
+         * healing data*/
+        block = 128 * 1024;
+    }
 
     type = afr_data_self_heal_type_get(priv, healed_sinks, source, replies);
 
