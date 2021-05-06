8ce957bcbdd7e7ad7c65eba399f7e454eeef0b8d
Nikhil Ladha <nladha@redhat.com>
2021-05-06 10:45:46 +0530

    coverity: null dereference (#2395)
    Fix:
    Updated the code to make it more readable and fixed
    the NULL dereferencing.
    
    CID: 1234622
    Updates: #1060
    
    Change-Id: I05bd203bc46fe84be86398bd664a3485409c3bfe
    Signed-off-by: nik-redhat <nladha@redhat.com>

--- xlators/cluster/dht/src/dht-lock.c
+++ xlators/cluster/dht/src/dht-lock.c
@@ -905,37 +905,35 @@ dht_nonblocking_inodelk_cbk(call_frame_t *frame, void *cookie, xlator_t *this,
     dht_local_t *local = NULL;
     int lk_index = 0, call_cnt = 0;
     char gfid[GF_UUID_BUF_SIZE] = {0};
+    dht_ilock_wrap_t *my_layout;
 
     local = frame->local;
     lk_index = (long)cookie;
 
+    my_layout = &(local->lock[0].layout.my_layout);
+
     if (op_ret == -1) {
-        local->lock[0].layout.my_layout.op_ret = -1;
-        local->lock[0].layout.my_layout.op_errno = op_errno;
+        my_layout->op_ret = -1;
+        my_layout->op_errno = op_errno;
 
-        if (local && local->lock[0].layout.my_layout.locks[lk_index]) {
-            uuid_utoa_r(local->lock[0]
-                            .layout.my_layout.locks[lk_index]
-                            ->loc.inode->gfid,
-                        gfid);
+        if (my_layout->locks[lk_index]) {
+            uuid_utoa_r(my_layout->locks[lk_index]->loc.inode->gfid, gfid);
 
-            gf_msg_debug(
-                this->name, op_errno,
-                "inodelk failed on gfid: %s "
-                "subvolume: %s",
-                gfid,
-                local->lock[0].layout.my_layout.locks[lk_index]->xl->name);
+            gf_msg_debug(this->name, op_errno,
+                         "inodelk failed on gfid: %s "
+                         "subvolume: %s",
+                         gfid, my_layout->locks[lk_index]->xl->name);
         }
 
         goto out;
     }
 
-    local->lock[0].layout.my_layout.locks[lk_index]->locked = _gf_true;
+    my_layout->locks[lk_index]->locked = _gf_true;
 
 out:
     call_cnt = dht_frame_return(frame);
     if (is_last_call(call_cnt)) {
-        if (local->lock[0].layout.my_layout.op_ret < 0) {
+        if (my_layout->op_ret < 0) {
             dht_inodelk_cleanup(frame);
             return 0;
         }
