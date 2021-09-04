9cd6735ff5d2f56f13078ac17743d1caa6db4267
Dmitry Antipov <dantipov@cloudlinux.com>
2021-09-04 09:32:48 +0300

    libglusterfs: drop core infrastructure of memory pools (#2675)
    Separate memory allocation type tracking from core infrastructure of
    memory pools and drop the latter alongside with '--disable-mempool'
    configuration option, provide '--disable-allocation-tracking' to control
    the former instead.
    
    Signed-off-by: Dmitry Antipov <dantipov@cloudlinux.com>
    Updates: #1000

--- cli/src/cli.c
+++ cli/src/cli.c
@@ -798,8 +798,6 @@ main(int argc, char *argv[])
     int ret = -1;
     glusterfs_ctx_t *ctx = NULL;
 
-    mem_pools_init();
-
     ctx = glusterfs_ctx_new();
     if (!ctx)
         return ENOMEM;
@@ -867,9 +865,6 @@ main(int argc, char *argv[])
     ret = gf_event_dispatch(ctx->event_pool);
 
 out:
-    //        glusterfs_ctx_destroy (ctx);
-
-    mem_pools_fini();
 
     return ret;
 }
