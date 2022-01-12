7c66a9f3bf794884653efcfec57e842fc9351a2b
Sheetal Pamecha <spamecha@redhat.com>
2022-01-12 15:58:49 +0530

    glusterd: add stripe_count in volume info (#3070)
    * glusterd: add stripe_count in volume info
    
    Change-Id: Ib04434b41b7c299cee0ed8d81deb5a68a17b0c0a
    Fixes: #3066
    Signed-off-by: Sheetal Pamecha <spamecha@redhat.com>
    
    * Add stipe-count only if version is less than 10
    
    Change-Id: Ibb589df2bb4c00c71850d787c998aff746321a17
    Signed-off-by: Sheetal Pamecha <spamecha@redhat.com>

--- xlators/mgmt/glusterd/src/glusterd-store.c
+++ xlators/mgmt/glusterd/src/glusterd-store.c
@@ -834,6 +834,16 @@ glusterd_volume_exclude_options_write(int fd, glusterd_volinfo_t *volinfo)
     }
     total_len += ret;
 
+    if (conf->op_version < GD_OP_VERSION_10_0) {
+        ret = snprintf(buf + total_len, sizeof(buf) - total_len, "%s=%d\n",
+                       GLUSTERD_STORE_KEY_VOL_STRIPE_CNT, STRIPE_COUNT);
+        if (ret < 0 || ret >= sizeof(buf) - total_len) {
+            ret = -1;
+            goto out;
+        }
+        total_len += ret;
+    }
+
     if ((conf->op_version >= GD_OP_VERSION_3_7_6) && volinfo->arbiter_count) {
         ret = snprintf(buf + total_len, sizeof(buf) - total_len, "%s=%d\n",
                        GLUSTERD_STORE_KEY_VOL_ARBITER_CNT,
