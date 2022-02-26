3f2ab8bf81c666aab5cae8e64d19416ab7298ccf
JamesWSWu <wu.shiwei@zte.com.cn>
2022-02-26 11:03:18 +0800

    Fixes:3177
    Change-Id: I7ae0283cfc5d02d7168d750cf6354a6c8e3f94f0
    Signed-off-by: JamesWSWu <wu.shiwei@zte.com.cn>

--- xlators/features/locks/src/clear.h
+++ xlators/features/locks/src/clear.h
@@ -57,7 +57,8 @@ clrlk_parse_args(const char *cmd, clrlk_args *args);
 
 int
 clrlk_clear_posixlk(xlator_t *this, pl_inode_t *pl_inode, clrlk_args *args,
-                    int *blkd, int *granted, int *op_errno);
+                    int *blkd, int *granted, int *op_errno, char *client_uid,
+                    pid_t client_pid, bool setlk_interrupt);
 int
 clrlk_clear_inodelk(xlator_t *this, pl_inode_t *pl_inode, pl_dom_list_t *dom,
                     clrlk_args *args, int *blkd, int *granted, int *op_errno);
