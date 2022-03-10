2cd6f6585aa6445350750700c6e636bcf68f6a83
mohit84 <moagrawa@redhat.com>
2022-03-10 14:48:26 +0530

    nfs: Optimize ctxcount value to reduce memory consumption for nfs-server (#3237)
    In case of gnfs for nfs-server all volumes are attached with a single graph.
    During inode table creation it sets ctxcount same to the total xlators are
    attached with a graph. If a user has created 100 volumes (1x3) so total
    number of xlators are attached with graph is 702 per volumes almost 7
    xlators are attached (io-stat, dht, write-behind, afr, c1, c2, c3) so total
    ctxcount set is 702. During inode creation we do allocate memory based on
    number of ctxcount to save the xlator data on inode. In case of 100 volumes
    memory allocation per inode will be around 32(bytes per ctxcount) * 702 = 22464 Bytes
    for ctx.In caseof gnfs default lru_limit is 90000 so total memory allocation will
    be 22464*90000(almost 2G). Per volume memory consumption is 2G so it will be huge
    for all 100 volumes if every volumes has reached till maximum lru_limit.
    
    Solution: An inode is associated with a specific volume so it is not
              possible multiple volume can save the data on inode_ctx so
              set the ctxcount based on the xlators associated with a volume
              not all the volumes.With this change It will reduce huge
              memory consumption for nfs-server. After set ctxcount (702/100 + 2 = 9)
              total memory allocation will be 32 * 9 * 90000 = 26M per volumes
              it is a huge saving and even for all 100 volumes has reached at
              maximum lru_limit memory consumption will not be more than 2.6G.
    
    Change-Id: If35804d4337723ea902a471ca28842a595030e78
    Fixes: #3236
    Signed-off-by: Mohit Agrawal <moagrawa@redhat.com>

--- libglusterfs/src/inode.c
+++ libglusterfs/src/inode.c
@@ -137,6 +137,25 @@ inode_table_prune(inode_table_t *table);
 void
 fd_dump(struct list_head *head, char *prefix);
 
+/* Calculate index based on the inode_table root_level
+   and xlator_id
+*/
+static int
+inode_get_ctx_index(inode_table_t *table, xlator_t *xlator)
+{
+    int ctx_idx = xlator->level;
+
+    if (ctx_idx > table->root_level) {
+#ifdef DEBUG
+        int32_t idx = xlator->xl_id - table->root_id;
+        GF_ASSERT((idx > 0) && (idx < (table->ctxcount - table->root_level)));
+#endif
+        ctx_idx = (table->root_level + xlator->xl_id - table->root_id);
+    }
+
+    return ctx_idx;
+}
+
 static int
 hash_dentry(inode_t *parent, const char *name, int mod)
 {
@@ -449,13 +468,14 @@ __inode_retire(inode_t *inode)
 static int
 __inode_get_xl_index(inode_t *inode, xlator_t *xlator)
 {
-    int set_idx = -1;
+    int set_idx = inode_get_ctx_index(inode->table, xlator);
 
-    if ((inode->_ctx[xlator->xl_id].xl_key != NULL) &&
-        (inode->_ctx[xlator->xl_id].xl_key != xlator))
+    if ((inode->_ctx[set_idx].xl_key != NULL) &&
+        (inode->_ctx[set_idx].xl_key != xlator)) {
+        set_idx = -1;
         goto out;
+    }
 
-    set_idx = xlator->xl_id;
     inode->_ctx[set_idx].xl_key = xlator;
 
 out:
@@ -1703,7 +1723,18 @@ inode_table_with_invalidator(uint32_t lru_limit, xlator_t *xl,
         return NULL;
 
     new->xl = xl;
-    new->ctxcount = xl->graph->xl_count + 1;
+
+    /* root_id and root_level will be useful to access index of specific
+       xlator
+    */
+    new->root_id = xl->xl_id;
+    new->root_level = xl->level;
+
+    /* The ctxcount value should be equal to the total xlators are associated
+       with specific volume. The sum of xl->level and xl->child_count represents
+       total xlators are associated with specific volume in a graph
+    */
+    new->ctxcount = (xl->level + xl->child_count + 1);
 
     new->lru_limit = lru_limit;
     new->invalidator_fn = invalidator_fn;
@@ -2190,7 +2221,8 @@ __inode_ctx_get2(inode_t *inode, xlator_t *xlator, uint64_t *value1,
     if (!inode || !xlator || !inode->_ctx)
         goto out;
 
-    index = xlator->xl_id;
+    index = inode_get_ctx_index(inode->table, xlator);
+
     if (inode->_ctx[index].xl_key != xlator)
         goto out;
 
@@ -2303,7 +2335,8 @@ inode_ctx_del2(inode_t *inode, xlator_t *xlator, uint64_t *value1,
         if (!inode->_ctx)
             goto unlock;
 
-        index = xlator->xl_id;
+        index = inode_get_ctx_index(inode->table, xlator);
+
         if (inode->_ctx[index].xl_key != xlator) {
             ret = -1;
             goto unlock;
@@ -2344,7 +2377,8 @@ __inode_ctx_reset2(inode_t *inode, xlator_t *xlator, uint64_t *value1,
 
     LOCK(&inode->lock);
     {
-        index = xlator->xl_id;
+        index = inode_get_ctx_index(inode->table, xlator);
+
         if (inode->_ctx[index].xl_key != xlator) {
             ret = -1;
             goto unlock;
