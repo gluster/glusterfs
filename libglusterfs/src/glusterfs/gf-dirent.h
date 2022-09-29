fffd551e22c54c0b5ca96fea62a5b5f4b40b0379
Yaniv Kaul <ykaul@redhat.com>
2022-09-29 21:39:12 +0300

    Fix review comments - use entry->d_len

--- libglusterfs/src/glusterfs/gf-dirent.h
+++ libglusterfs/src/glusterfs/gf-dirent.h
@@ -56,7 +56,7 @@ struct _gf_dirent {
 static inline gf_boolean_t
 inode_dir_or_parentdir(gf_dirent_t *entry)
 {
-    return ((entry->d_name[0] == '.') &&
+    return ((entry->d_len <= 2) && (entry->d_name[0] == '.') &&
             ((entry->d_name[1] == 0) || (entry->d_name[1] == '.')));
 }
 
