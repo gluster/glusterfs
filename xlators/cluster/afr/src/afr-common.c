157ad4212fd0456a05f7d2f0ca32f35968704535
Amar Tumballi <amar@kadalu.tech>
2023-09-23 09:48:49 +0530

    add simple-quota xattr to afr-and-dht ignore list
    Signed-off-by: Amar Tumballi <amar@kadalu.tech>

--- xlators/cluster/afr/src/afr-common.c
+++ xlators/cluster/afr/src/afr-common.c
@@ -2723,7 +2723,7 @@ afr_frame_return(call_frame_t *frame)
     return call_count;
 }
 
-static char *afr_ignore_xattrs[] = {GF_SELINUX_XATTR_KEY, QUOTA_SIZE_KEY, NULL};
+static char *afr_ignore_xattrs[] = {GF_SELINUX_XATTR_KEY, QUOTA_SIZE_KEY, SQUOTA_SIZE_KEY, SQUOTA_SIZE_LIMIT, NULL};
 
 gf_boolean_t
 afr_is_xattr_ignorable(char *key)
