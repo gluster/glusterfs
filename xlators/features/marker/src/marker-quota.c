3a1e02e254c0f8086688894a7f62e85e732c19b0
Juan Calderon-Perez <835733+gaby@users.noreply.github.com>
2024-01-11 06:38:44 -0500

    Marker optimize the size of the stack #3711 (#4297)
    The brick process is getting crashed while quota is enabled
    
    Fixes: #3710 
    > (Reviewed on upstream link https://github.com/gluster/glusterfs/pull/3711)
    Signed-off-by: Juan Calderon-Perez <jgcalderonperez@protonmail.com>

--- xlators/features/marker/src/marker-quota.c
+++ xlators/features/marker/src/marker-quota.c
@@ -1120,8 +1120,8 @@ mq_synctask1(xlator_t *this, synctask_fn_t task, gf_boolean_t spawn, loc_t *loc,
     }
 
     if (spawn) {
-        ret = synctask_new1(this->ctx->env, 1024 * 16, task,
-                            mq_synctask_cleanup, NULL, args);
+        ret = synctask_new1(this->ctx->env, 0, task, mq_synctask_cleanup, NULL,
+                            args);
         if (ret) {
             gf_log(this->name, GF_LOG_ERROR,
                    "Failed to spawn "
